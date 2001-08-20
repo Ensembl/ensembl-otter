28372ac6c3879c54fbb334352c31b068f5662867
jgrg <jgrg>
2001-08-20 19:46:43 +0000

    *** empty log message ***

--- tk/ExonCanvas.pm
+++ tk/ExonCanvas.pm
@@ -23,56 +23,14 @@ sub add_ace_subseq {
     # Get the offset underneath everthing else
     my $y_offset = ($self->canvas->bbox('all'))[3];
     
+    my $strand = $subseq->strand;
     foreach my $ex ($subseq->get_all_Exons) {
-        $y_offset += $self->add_exon_holder($ex, $x_offset, $y_offset);
+        $y_offset += $self->add_exon_holder($ex, $strand, $x_offset, $y_offset);
     }
-}
-
-sub record_exon_start_end {
-    my( $self, $exon_id, $start_id, $end_id ) = @_;
-    
-    $self->{'_exons'}{$exon_id} = [$start_id, $end_id];
-}
-
-sub to_ace_subseq {
-    my( $self ) = @_;
-    
-    my $e = $self->{'_exons'};
-    my $canvas = $self->canvas;
-    
-    my $subseq = Ace::SubSeq->new;
-    $subseq->name($canvas->TopLevel->cget('title'));
-
-    foreach my $exid (keys %$e) {
-        my ($start_id, $end_id) = @{$e->{$exid}};
-        
-        my $start = $canvas->itemcget($start_id, 'text');
-        my $end   = $canvas->itemcget(  $end_id, 'text');
-        
-        my $exon = Hum::Ace::Exon->new;
-        $exon->start($start);
-        $exon->end($end);
-        
-        $subseq->add_Exon($exon);
-    }
-    return $subseq;
-}
-
-sub max_exon_number {
-    my( $self ) = @_;
-    
-    return $self->{'_max_exon_number'} || 0;
-}
-
-sub next_exon_number {
-    my( $self ) = @_;
-    
-    $self->{'_max_exon_number'}++;
-    return $self->{'_max_exon_number'};
 }
 
 sub add_exon_holder {
-    my( $self, $exon, $x_offset, $y_offset ) = @_;
+    my( $self, $exon, $strand, $x_offset, $y_offset ) = @_;
     
     my $canvas  =          $self->canvas;
     my $font    =          $self->font;
@@ -85,6 +43,8 @@ sub add_exon_holder {
     
     my $line_length = $size;
     
+    my $arrow = ($strand == 1) ? 'last' : 'first';
+    
     my $start_text = $canvas->createText(
         $x_offset - $size, $y_offset,
         -anchor     => 'e',
@@ -94,11 +54,11 @@ sub add_exon_holder {
         -tags       => [$exon_id],
         );
     
-    my $centre_arrow = $canvas->createLine(
+    my $strand_arrow = $canvas->createLine(
         $x_offset - $half, $y_offset,
         $x_offset + $half, $y_offset,
         -width      => 1,
-        -arrow      => 'last',
+        -arrow      => $arrow,
         -arrowshape => [$arrow_size, $arrow_size, $arrow_size - $pad],
         -tags       => [$exon_id],
         );
@@ -112,10 +72,71 @@ sub add_exon_holder {
         -tags       => [$exon_id],
         );
     
-    $self->record_exon_start_end($exon_id, $start_text, $end_text);
+    $self->record_exon_inf($exon_id, $start_text, $strand_arrow, $end_text);
     
     # Return how big we were
     return $size + $pad;
+}
+
+sub record_exon_inf {
+    my( $self, $exon_id, @inf ) = @_;
+    
+    $self->{'_exons'}{$exon_id} = [@inf];
+}
+
+sub to_ace_subseq {
+    my( $self ) = @_;
+    
+    my $e = $self->{'_exons'};
+    my $canvas = $self->canvas;
+    
+    my $subseq = Ace::SubSeq->new;
+    $subseq->name($canvas->TopLevel->cget('title'));
+
+    my( $subseq_strand );
+    foreach my $exid (keys %$e) {
+        my ($start_id, $strand_arrow, $end_id) = @{$e->{$exid}};
+        
+        my $start  =  $canvas->itemcget($start_id, 'text');
+        my $strand = ($canvas->itemcget($strand_arrow, 'arrow') eq 'last') ? 1 : -1;
+        my $end    =  $canvas->itemcget(  $end_id, 'text');
+        
+        if ($subseq_strand) {
+            $self->message("inconsistent strands")
+                unless $strand == $subseq_strand;
+        } else {
+            $subseq_strand = $strand;
+        }
+        
+        my $exon = Hum::Ace::Exon->new;
+        $exon->start($start);
+        $exon->end($end);
+        
+        $subseq->add_Exon($exon);
+    }
+    $subseq->strand($subseq_strand);
+    
+    return $subseq;
+}
+
+sub message {
+    my( $self, @message ) = @_;
+    
+    # put in stuff to put message in window
+    print STDERR "\n", @message, "\n";
+}
+
+sub max_exon_number {
+    my( $self ) = @_;
+    
+    return $self->{'_max_exon_number'} || 0;
+}
+
+sub next_exon_number {
+    my( $self ) = @_;
+    
+    $self->{'_max_exon_number'}++;
+    return $self->{'_max_exon_number'};
 }
 
 sub font {
