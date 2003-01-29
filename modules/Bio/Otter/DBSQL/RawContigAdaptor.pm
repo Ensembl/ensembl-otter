17b84e6b74e2b0004d3c2a9672d8c13a194e3b4b
michele <michele>
2003-01-29 12:35:52 +0000

    Small extension of RawContigAdaptor to return annotated clones

---
+++ modules/Bio/Otter/DBSQL/RawContigAdaptor.pm
@@ -0,0 +1,52 @@
+package Bio::Otter::DBSQL::RawContigAdaptor;
+
+use strict;
+
+use Bio::EnsEMBL::DBSQL::RawContigAdaptor;
+
+use vars qw(@ISA);
+
+@ISA = qw ( Bio::EnsEMBL::DBSQL::RawContigAdaptor);
+
+
+# This is assuming the otter info and the ensembl genes are in the same database 
+# and so have the same adaptor
+
+sub new {
+    my ($class,$dbobj) = @_;
+
+    my $self = {};
+    bless $self,$class;
+
+    if( !defined $dbobj || !ref $dbobj ) {
+        $self->throw("Don't have a db [$dbobj] for new adaptor");
+    }
+
+    $self->db($dbobj);
+
+    return $self;
+}
+
+
+sub fetch_filled_by_dbIDs {
+   my ($self,@ids) = @_;
+
+   my $result = $self->SUPER::fetch_filled_by_dbIDs(@ids);
+
+   foreach my $key (keys %$result) {
+      my $clone = $result->{$key}->clone;
+
+      $clone = $self->db->get_AnnotatedCloneAdaptor->annotate_clone($clone);  
+
+   }
+
+   return $result;
+}
+1;
+
+	
+
+
+
+
+
