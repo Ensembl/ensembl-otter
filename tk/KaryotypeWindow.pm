4bdb6b604e9f4448a339367a145751ec7da2377f
keenan <keenan>
2003-03-03 16:15:29 +0000

    *** empty log message ***

--- tk/KaryotypeWindow.pm
+++ tk/KaryotypeWindow.pm
@@ -10,18 +10,14 @@ use KaryotypeWindow::Chromosome;
 
 sub new {
     my $pkg = shift;
-    
+
     my $self = $pkg->SUPER::new(@_);
-    
+
     #$self->canvas->DefineBitmap(
     #    'gvar', 4, 1, pack('b4', '..11')
     #    );
-    $self->canvas->DefineBitmap(
-        'gvar', 2, 2, pack('b2' x 2,
-            '.1',
-            '1.',
-            )
-        );
+    $self->canvas->DefineBitmap( 'gvar', 2, 2, pack( 'b2' x 2, '.1', '1.', ) );
+
     #$self->canvas->DefineBitmap(
     #    'gcen', 2, 1, pack('b2', '.1')
     #    );
@@ -29,108 +25,158 @@ sub new {
 }
 
 sub get_all_Chromosomes {
-    my( $self ) = @_;
-    
-    if (my $lst = $self->{'_Chromosome_list'}) {
+    my ($self) = @_;
+
+    if ( my $lst = $self->{'_Chromosome_list'} ) {
         return @$lst;
-    } else {
+    }
+    else {
         return;
     }
 }
 
 sub chromosomes_per_row {
-    my( $self, $chromosomes_per_row ) = @_;
-    
+    my ( $self, $chromosomes_per_row ) = @_;
+
     if ($chromosomes_per_row) {
         $self->{'_chromosomes_per_row'} = $chromosomes_per_row;
     }
     return $self->{'_chromosomes_per_row'} || 12;
 }
 
-
 sub add_Chromosome {
-    my( $self, $chr ) = @_;
-    
+    my ( $self, $chr ) = @_;
+
     confess "Missing Chromosome argument" unless $chr;
     my $lst = $self->{'_Chromosome_list'} ||= [];
-    push(@$lst, $chr);
+    push ( @$lst, $chr );
 }
 
 sub new_Chromosome {
-    my( $self ) = @_;
-    
+    my ($self) = @_;
+
     my $chr = KaryotypeWindow::Chromosome->new;
     $self->add_Chromosome($chr);
     return $chr;
 }
 
 sub draw {
-    my( $self ) = @_;
-    
-    my $max = $self->chromosomes_per_row;
-    my $set = [];
+    my ($self) = @_;
+
+    my $max     = $self->chromosomes_per_row;
+    my $set     = [];
     my @all_set = ($set);
-    foreach my $chr ($self->get_all_Chromosomes) {
-        push(@$set, $chr);
-        if (@$set >= $max) {
+    foreach my $chr ( $self->get_all_Chromosomes ) {
+        push ( @$set, $chr );
+        if ( @$set >= $max ) {
             $set = [];
-            push(@all_set, $set);
+            push ( @all_set, $set );
         }
     }
-    
+
     my $pad = $self->pad;
-    my ($x, $y) = ($pad, $pad);
+    my ( $x, $y ) = ( $pad, $pad );
+    my $y_pos;
     foreach my $set (@all_set) {
         next unless @$set;
-        $y = $self->draw_chromsome_set($x, $y, $set);
-        $y += $pad * 2;
+        $y = $self->draw_chromsome_set( $x, $y, $set );
+        #$y += $pad;
+        $y_pos += $y;
+       
     }
+    
+    $self->_draw_scale( $x, $y_pos, scalar(@all_set), $max );
+
+}
+
+sub _draw_scale {
+    my ( $self, $x, $y, $row_count, $max ) = @_;
+
+    my $pad     = $self->pad;
+    my $scale_y = $y - 75;
+    my $scale_x = ( $pad * $max ) + $pad + 75;
+
+    $self->{'_canvas'}->createText(
+        $scale_x - 18, $scale_y, -text => 'Scale: ',
+        -font => [ 'Helvetica', '10', 'bold' ],
+    );
+
+    $self->{'_canvas'}->createText(
+        $scale_x + 17, $scale_y + 10, -text => '50 tRNAs',
+        -font => [ 'Helvetica', '8' ],
+    );
+
+    $self->{'_canvas'}->createLine(
+        $scale_x, $scale_y, $scale_x + 25, $scale_y, -fill => '#cc3333',
+    );
+
+    $self->{'_canvas'}->createLine(
+        $scale_x, $scale_y - 2, $scale_x, $scale_y + 3, -fill => 'black',
+    );
+
+    $self->{'_canvas'}->createLine(
+        $scale_x + 25, $scale_y - 2, $scale_x + 25, $scale_y + 3, -fill => 'black',
+    );
+
 }
 
 sub draw_chromsome_set {
-    my( $self, $x, $y, $set ) = @_;
+    my ( $self, $x, $y, $set ) = @_;
 
     warn sprintf "Drawing set of %d chromsomes\n", scalar @$set;
 
-    my $scale = $self->Mb_per_pixel;
-    my $canvas = $self->canvas;
-    my $pad = $self->pad;
+    my $scale          = $self->Mb_per_pixel;
+    my $canvas         = $self->canvas;
+    my $pad            = $self->pad;
     my $max_chr_height = 0;
     foreach my $chr (@$set) {
         $chr->Mb_per_pixel($scale);
         my $h = $chr->height;
         $max_chr_height = $h if $h > $max_chr_height;
     }
-    
+
     foreach my $chr (@$set) {
         $chr->set_initial_and_terminal_bands;
-        $chr->draw($self, $x, $y + $max_chr_height - $chr->height);
+        $chr->draw( $self, $x, $y + $max_chr_height - $chr->height );
         $x += $chr->width + $pad;
+
     }
-    return $max_chr_height + $self->pad + $self->font_size;
+    return $max_chr_height + $self->pad + $self->font_size + 50;
 }
 
 sub Mb_per_pixel {
-    my( $self, $Mb_per_pixel ) = @_;
-    
+    my ( $self, $Mb_per_pixel ) = @_;
+
     if ($Mb_per_pixel) {
         $self->{'_Mb_per_pixel'} = $Mb_per_pixel;
     }
     return $self->{'_Mb_per_pixel'} || 1;
 }
 
-
 sub pad {
-    my( $self, $pad ) = @_;
-    
+    my ( $self, $pad ) = @_;
+
     if ($pad) {
         $self->{'_pad'} = $pad;
     }
-    return $self->{'_pad'} || 40;
+    return $self->{'_pad'} || 70;
 }
 
+sub data {
+    my ( $self, %data ) = @_;
 
+    if (%data) {
+        $self->{'_data'} = {%data};
+
+    }
+
+    return $self->{'_data'};
+
+}
 1;
+
+
+
 
 __END__
 
--- tk/KaryotypeWindow/Chromosome.pm
+++ tk/KaryotypeWindow/Chromosome.pm
@@ -6,16 +6,17 @@ package KaryotypeWindow::Chromosome;
 use strict;
 use Carp;
 use KaryotypeWindow::Band;
+use KaryotypeWindow::Graph;
 
 sub new {
-    my( $pkg ) = @_;
-    
+    my ( $pkg ) = @_;
+
     return bless {}, $pkg;
 }
 
 sub name {
-    my( $self, $name ) = @_;
-    
+    my ( $self, $name ) = @_;
+
     if ($name) {
         $self->{'_name'} = $name;
     }
@@ -23,64 +24,66 @@ sub name {
 }
 
 sub draw {
-    my( $self, $kw, $x, $y ) = @_;
-    
+    my ( $self, $kw, $x, $y ) = @_;
+
     my $save_y = $y;
-    
-    my $scale = $self->Mb_per_pixel;
+
+    my $scale  = $self->Mb_per_pixel;
     my $canvas = $kw->canvas;
-    foreach my $band ($self->get_all_Bands) {
+    foreach my $band ( $self->get_all_Bands ) {
         $band->Mb_per_pixel($scale);
-        $band->draw($self, $canvas, $x, $y);
+        $band->draw( $self, $canvas, $x, $y );
         $y += $band->height;
     }
-    
-    my ($first, $last) = $self->get_first_and_last_Bands;
+
+    my ( $first, $last ) = $self->get_first_and_last_Bands;
     my $outline = $canvas->createPolygon(
-        $self->get_outline_coordinates,
-        -fill       => undef,
-        -outline    => 'black',
-        -smooth     => 1,
-        -joinstyle  => 'round',
-        );
+        $self->get_outline_coordinates, -fill => undef,
+        -outline   => 'black',
+        -smooth    => 1,
+        -joinstyle => 'round',
+    );
+
+    $self->height('10000');
     
     $canvas->createText(
-        $x + $self->width / 2,
-        $save_y + $self->height + $kw->pad / 2,
-        -anchor => 'n',
-        -text   => $self->name,
-        -font   => ['Helvetica', $kw->font_size, 'bold'],
-        );
+        $x + $self->width / 2, $save_y + $self->height + 20, -anchor => 'n',
+        -text => $self->name,
+        -font => [ 'Helvetica', $kw->font_size, 'bold' ],
+    );
+    
+    my $graph = $self->Graph;
+    $graph->chromosome($self); 
+    $graph->draw($kw, $x, $save_y );
+    
 }
 
 sub height {
-    my( $self ) = @_;
-    
-    my ($first, $last) = $self->get_first_and_last_Bands;
-    my $length = ($last->end - $first->start + 1) / 1_000_000;
+    my ($self) = @_;
+
+    my ( $first, $last ) = $self->get_first_and_last_Bands;
+    my $length = ( $last->end - $first->start + 1 ) / 1_000_000;
     return $length / $self->Mb_per_pixel;
 }
 
 sub get_outline_coordinates {
-    my( $self ) = @_;
-    
+    my ($self) = @_;
+
     my @bands = $self->get_all_Bands;
-    my( @coords );
+    my (@coords);
     foreach my $b (@bands) {
-        push(@coords, $b->right_coordinates)
-            unless $b->is_rectangular;
+        push ( @coords, $b->right_coordinates ) unless $b->is_rectangular;
     }
-    foreach my $b (reverse @bands) {
-        push(@coords, $b->left_coordinates)
-            unless $b->is_rectangular;
+    foreach my $b ( reverse @bands ) {
+        push ( @coords, $b->left_coordinates ) unless $b->is_rectangular;
     }
-    
+
     return @coords;
 }
 
 sub set_initial_and_terminal_bands {
-    my( $self ) = @_;
-    
+    my ($self) = @_;
+
     my @bands = $self->get_all_Bands;
     foreach my $band (@bands) {
         $band->is_first(0);
@@ -92,54 +95,68 @@ sub set_initial_and_terminal_bands {
 }
 
 sub get_first_and_last_Bands {
-    my( $self ) = @_;
-    
+    my ($self) = @_;
+
     my @bands = $self->get_all_Bands;
-    return @bands[0, $#bands];
+    return @bands[ 0, $#bands ];
 }
 
 sub width {
-    my( $self ) = @_;
-    
+    my ($self) = @_;
+
     return $self->Mb_per_pixel * 15;
 }
 
 sub get_all_Bands {
-    my( $self ) = @_;
-    
-    if (my $lst = $self->{'_Band_list'}) {
+    my ($self) = @_;
+
+    if ( my $lst = $self->{'_Band_list'} ) {
         return @$lst;
-    } else {
+    }
+    else {
         return;
     }
 }
 
 sub add_Band {
-    my( $self, $band ) = @_;
-    
+    my ( $self, $band ) = @_;
+
     confess "Missing Band argument" unless $band;
     my $lst = $self->{'_Band_list'} ||= [];
-    push(@$lst, $band);
+    push ( @$lst, $band );
 }
 
 sub new_Band {
-    my( $self, $class ) = @_;
-    
+    my ( $self, $class ) = @_;
+
     $class ||= 'KaryotypeWindow::Band';
     my $band = $class->new;
     $self->add_Band($band);
     return $band;
 }
 
-sub Mb_per_pixel {
-    my( $self, $Mb_per_pixel ) = @_;
+
+sub Graph {
+    my ( $self, $graph ) = @_;
     
+    unless ( $self->{'_graph'} ) {
+        $self->{'_graph'} = KaryotypeWindow::Graph->new;
+    
+    }
+    
+    return $self->{'_graph'};
+
+}
+
+
+sub Mb_per_pixel {
+    my ( $self, $Mb_per_pixel ) = @_;
+
     if ($Mb_per_pixel) {
         $self->{'_Mb_per_pixel'} = $Mb_per_pixel;
     }
     return $self->{'_Mb_per_pixel'} || confess "Scale not set";
 }
-
 
 1;
 
