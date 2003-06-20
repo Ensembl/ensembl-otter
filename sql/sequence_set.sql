382164f54ce14998f7443da7a8f5c3712bee8f02
jgrg <jgrg>
2003-06-20 15:45:43 +0000

    removing duplicate sql file

--- sql/sequence_set.sql
+++
@@ -1,7 +0,0 @@
-# SQL to add sequence set table to pipeline and otter databses 
-
-create table sequence_set (
-        assembly_type varchar (20),
-        description TEXT,
-        analysis_priority int
-    );
