f64c973eb8ef149b139ccc122d7b187b46c480ec
jgrg <jgrg>
2003-09-15 13:51:45 +0000

    change to lace_acedb.tar finding

--- modules/Bio/Otter/Lace/AceDatabase.pm
+++ modules/Bio/Otter/Lace/AceDatabase.pm
@@ -72,9 +72,15 @@ sub tar_file {
         $self->{'_tar_file'} = $tar_file;
     }
     elsif (! $self->{'_tar_file'}) {
-        my $root = $ENV{'LACE_LOCAL'} || '/nfs/humace2/hum/data';
-        my $file = 'lace_acedb.tar';
-        $self->{'_tar_file'} = "$root/$file";
+        foreach my $root ($ENV{'OTTER_HOME'}, $ENV{'LACE_LOCAL'}, '/nfs/humace2/hum/data') {
+            next unless $root;
+            my $file = "$root/lace_acedb.tar";
+            if (-e $file) {
+                warn "FOUND '$file'";
+                $self->{'_tar_file'} = $file;
+                last;
+            }
+        }
     }
     return $self->{'_tar_file'};
 }
