ee909de1cec0a4ed3136ce0844edc2d7505d757f
jgrg <jgrg>
2003-11-10 13:56:14 +0000

    tables for contig and vega status tracking

--- sql/lace.sql
+++ sql/lace.sql
@@ -29,3 +29,52 @@ create table sequence_set_access (
     
     PRIMARY KEY(assembly_type, author_id)
 );
+
+create table contig_status (
+    contig_status_id  INT(10) unsigned DEFAULT '0' NOT NULL  auto_increment,
+    contig_id INT(10) unsigned DEFAULT '0' NOT NULL,
+    author_id INT(10) unsigned DEFAULT '0' NOT NULL,
+    status_date DATETIME,
+    status ENUM(
+        'annotated',
+        'marked_for_checking',
+        'checked',
+        'marked_for_submitting',
+        'submitted'
+        ),
+    
+    PRIMARY KEY (contig_status_id),
+    KEY cln_stat_date (contig_id, status, status_date)
+);
+
+create table current_contig_status (
+    contig_status_id  INT(10) unsigned DEFAULT '0' NOT NULL,
+    contig_id INT(10) unsigned DEFAULT '0' NOT NULL,
+    
+    PRIMARY KEY (contig_status_id),
+    KEY (contig_id)
+);
+
+create table contig_vega_status (
+    contig_vega_status_id  INT(10) unsigned DEFAULT '0' NOT NULL  auto_increment,
+    contig_id INT(10) unsigned DEFAULT '0' NOT NULL,
+    author_id INT(10) unsigned DEFAULT '0' NOT NULL,
+    vega_status_date DATETIME,
+    vega_status ENUM(
+        'unpublished',
+        'private',
+        'marked_for_publishing',
+        'vega_published'
+        ),
+    
+    PRIMARY KEY (contig_vega_status_id),
+    KEY cln_stat_date (contig_id, vega_status, status_date)
+);
+
+create table current_contig_vega_status (
+    contig_vega_status_id  INT(10) unsigned DEFAULT '0' NOT NULL,
+    contig_id INT(10) unsigned DEFAULT '0' NOT NULL,
+    
+    PRIMARY KEY (contig_status_id),
+    KEY (contig_id)
+);
