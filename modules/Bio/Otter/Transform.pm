6048e9e1c75270a661e86dd38e4a2c399ec956db
rds <rds>
2005-01-26 18:45:55 +0000

    base class for transformations, needs cvs up -d

---
+++ modules/Bio/Otter/Transform.pm
@@ -0,0 +1,119 @@
+=pod
+
+=head1 Bio::Otter::Transform
+
+=head1 DESCRIPTION
+
+This is a bit of an experiment, and is designed as a base class
+for xml parsing and transformation to otter objects, with the 
+goal of moving it out of Converter and moving to stream based method.
+
+=head1 USAGE
+
+Try this and see first implementation in Bio::Otter::Lace::Client
+which uses the Bio::Otter::Transform::DataSets child.
+
+  my $transform = Bio::Otter::Transform->new();
+  
+  # this returns the XML::Parser obj
+  my $p = $transform->my_parser();
+  $p->parse($xml);
+
+  # get the objects created
+  $transform->objects;
+
+=head1 Note to subclasses
+
+You need to implement {start,end,char}_handler methods to create
+your object list, even if they're just "sub end_handler{ }".
+
+=head1 BUGS
+
+There's probably a better way to do this, but it kinda works at the 
+moment for Datasets at least.
+
+=head1 AUTHOR
+
+Roy Storey rds@sanger.ac.uk
+
+=cut
+
+
+package Bio::Otter::Transform;
+
+use strict;
+use warnings;
+
+use XML::Parser;
+
+sub new{
+    my $pkg = shift;
+
+    my $self = bless({}, ref($pkg) || $pkg);
+
+    return $self;
+}
+
+sub my_parser{
+    my ($self) = @_;
+    my $p1 = new XML::Parser(Style => 'Debug',
+                             Handlers => {
+                                 Start => sub { 
+                                     $self->start_handler(@_);
+                                 },
+                                 End => sub {
+                                     $self->end_handler(@_);
+                                 },
+                                 Char => sub { 
+                                     $self->char_handler(@_);
+                                 }
+                             });
+
+    return $p1;
+}
+
+sub default_handler{
+    my $c = (caller(1))[3];
+    print "$c -> @_\n";
+}
+
+sub start_handler{
+    my $self = shift;
+    $self->default_handler(@_);
+}
+sub end_handler{
+    my $self = shift;
+    $self->default_handler(@_);
+}
+sub char_handler{
+    my $self = shift;
+    $self->default_handler(@_);
+}
+
+sub set_property{
+    my ($self, $prop_name, $value) = @_;
+    return undef unless $prop_name;
+    if($value){
+        $self->{'_properties'}->{$prop_name} = $value;
+    }
+    return $self->{'_properties'}->{$prop_name};
+}
+sub get_property{
+    my $self = shift;
+    return $self->set_property(@_);
+}
+
+sub objects{
+    my $self = shift;
+    return $self->{'_objects'};
+}
+sub add_object{
+    my $self = shift;
+    push(@{$self->{'_objects'}}, shift) if @_;
+    
+}
+
+# END
+1; # return true;
+
+__END__
