cd00252bafcd8e3c6ac873d9b8cbf38b7822efa0
rds <rds>
2005-01-26 18:46:38 +0000

    transformation of datasets xml

---
+++ modules/Bio/Otter/Transform/DataSets.pm
@@ -0,0 +1,56 @@
+package Bio::Otter::Transform::DataSets;
+
+use strict;
+use warnings;
+use Bio::Otter::Transform;
+use Bio::Otter::Lace::DataSet;
+
+our @ISA = qw(Bio::Otter::Transform);
+
+
+# probably these should be global rather than assign EVERY time
+# ones were interested in 
+my $SUB_ELE = { map { $_ => 1 } qw(host port user pass dbname type
+                                   dna_host dna_port dna_user dna_pass dna_dbname)};
+my $SUP_ELE = { map { $_ => 1 } qw(otter datasets) };
+
+# this should be in xsl and use xslt to transform and create the objects
+sub start_handler{
+    my $self = shift;
+    my $xml  = shift;
+    my $ele  = lc shift;
+    my $attr = {@_};
+
+    if($ele eq 'dataset'){
+        my $ds = Bio::Otter::Lace::DataSet->new();
+        $ds->name($attr->{'name'});
+        $ds->author($self->get_property('author'));
+        $self->add_object($ds);
+    }elsif($SUB_ELE->{$ele}){
+     #   print "* Interesting $ele\n";
+    }else{
+      #  print "Uninteresting $ele\n";
+    }
+    
+}
+sub end_handler{ }
+sub char_handler{
+    my $self = shift;
+    my $xml  = shift;
+    my $data = shift;
+    #my $context = ($xml->context)[-1];
+    my $context = $xml->current_element();
+    #print "context '$context'\n";
+    if($SUB_ELE->{$context}){
+        my $context_method = uc $context;
+        my $ds = $self->objects;
+        my $current = $ds->[$#$ds];
+        if($current->can($context_method)){
+            $current->$context_method($data);
+        }
+    }
+}
+
+
+
+1;
