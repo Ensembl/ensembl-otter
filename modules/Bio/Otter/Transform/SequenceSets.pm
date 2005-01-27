b978c934a5f2a32be626d98926f089a6f13fa4f1
rds <rds>
2005-01-27 21:53:12 +0000

    set the dataset name

--- modules/Bio/Otter/Transform/SequenceSets.pm
+++ modules/Bio/Otter/Transform/SequenceSets.pm
@@ -8,9 +8,9 @@ use Bio::Otter::Lace::SequenceSet;
 our @ISA = qw(Bio::Otter::Transform);
 
 
-# probably these should be global rather than assign EVERY time
 # ones were interested in 
 my $SUB_ELE = { map { $_ => 1 } qw(description assembly_type vega_set_id analysis_priority)};
+# super elements to the actual sequence set
 my $SUP_ELE = { map { $_ => 1 } qw(otter sequencesets) };
 
 # this should be in xsl and use xslt to transform and create the objects
@@ -24,6 +24,7 @@ sub start_handler{
         my $ss = Bio::Otter::Lace::SequenceSet->new();
         $ss->name($attr->{'name'});
         $ss->is_hidden($attr->{'hidden'});
+        $ss->dataset_name($self->get_property('dataset_name'));
         $self->add_object($ss);
     }elsif($SUB_ELE->{$ele}){
      #   print "* Interesting $ele\n";
