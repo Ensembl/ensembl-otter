4a0b8c26903909909804d103ae9d25fcdea77524
jgrg <jgrg>
2005-02-03 15:55:31 +0000

    *** empty log message ***

---
+++ tk/MenuCanvasWindow/Example.pm
@@ -0,0 +1,32 @@
+
+### MenuCanvasWindow::Example
+
+package MenuCanvasWindow::Example;
+
+use strict;
+use base 'MenuCanvasWindow';
+
+
+sub initialize {
+    my( $self ) = @_;
+    
+    my $file_menu = $self->make_menu('File');
+    $file_menu->add('command',
+        -label          => 'New',
+        -command        => sub { print "new\n" },
+        -accelerator    => 'Ctrl+N',
+        -underline      => 1,
+        );    
+}
+
+
+1;
+
+__END__
+
+=head1 NAME - MenuCanvasWindow::Example
+
+=head1 AUTHOR
+
+James Gilbert B<email> jgrg@sanger.ac.uk
+
