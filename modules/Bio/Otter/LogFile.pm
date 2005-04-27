fc2b7d7eedf1875d5c023785e446e91902801680
jgrg <jgrg>
2005-04-27 16:48:46 +0000

    *** empty log message ***

--- modules/Bio/Otter/LogFile.pm
+++ modules/Bio/Otter/LogFile.pm
@@ -4,21 +4,8 @@
 package Bio::Otter::LogFile;
 
 use strict;
-{
-    # On perl 5.8.0 this check generates the warning:
-    # 'v-string in use/require non-portable'
-    no warnings;
-    # tie doesn't work properly for filehandles
-    # on earlier versions of perl:
-    use v5.8.0;  
-}
 use Symbol 'gensym';
-
-my $log;    # Variable stores logging filehandle
-
-my $prefix_sub = sub {
-    return scalar(localtime) . " - ";
-    };
+use Carp;
 
 sub import {
     my $class = shift;
@@ -26,59 +13,47 @@ sub import {
     $class->make_log(@_) if @_;
 }
 
-sub make_log {
-    my( $class, $file ) = @_;
-    
-    if ($log) {
-        print_to_log("Switching log to '$file'");
-        close($log);
-        untie *STDERR;
-        untie *STDOUT;
-    }
-    
-    # Create logging filehandle
-    $log = gensym();
-    open($log, '>', $file)
-        or die "Can't write log to '$file' : $!";
-
-    # Unbuffer our logging filehandle
-    my $oldfh = select($log);
-    $| = 1;
-    select($oldfh);
-
-    tie *STDERR, $class;
-    tie *STDOUT, $class;
-}
+my $prefix_sub = sub {
+    return scalar(localtime) . " - ";
+    };
 
 sub set_prefix_sub {
     $prefix_sub = shift;
 }
 
-sub TIEHANDLE {
-    return bless {}, shift;
-}
+sub make_log {
+    my( $class, $file ) = @_;
 
-sub PRINT {
-    shift;  # Don't need $self
-    
-    print_to_log(@_);
-}
+    # Unbuffer STDOUT
+    my $oldsel = select(STDOUT);
+    $| = 1;
+    select($oldsel);
 
-sub PRINTF {
-    shift;  # Don't need $self
-    my $fmt = shift;
-    
-    print_to_log(sprintf($fmt, @_));
+    if (my $pid = open(STDOUT, "|-")) {
+        # Send parent's STDERR to the same place as STDOUT.
+        open STDERR, ">&STDOUT" or confess "Can't redirect STDERR to STDOUT";
+        return $pid;
+    }
+    elsif (defined $pid) {
+        my $log = gensym();
+        open $log, "> $file" or confess "Can't write to logfile '$file': $!";
+        
+        # Unbuffer logfile
+        $oldsel = select($log);
+        $| = 1;
+        select($oldsel);
+        
+        # Child filters output from parent
+        while (<STDIN>) {
+           print $log $prefix_sub->(), $_;
+        }
+        close $log;
+        exit;   # Child must exit here!
+    }
+    else {
+        confess "Can't fork STDOUT filter: $!";
+    }
 }
-
-sub print_to_log {
-    
-    print $log $prefix_sub->(), @_;
-    
-    # Add an extra newline for a neat message
-    print $log "\n" unless $_[$#_] =~ /\n$/;
-}
-
 
 1;
 
