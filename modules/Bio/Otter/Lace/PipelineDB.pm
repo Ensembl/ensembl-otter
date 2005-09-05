efc04a1d4f7ab38ac7e29e0f96724272e3c9d017
lg4 <lg4>
2005-09-05 13:19:29 +0000

    *** empty log message ***

--- modules/Bio/Otter/Lace/PipelineDB.pm
+++ modules/Bio/Otter/Lace/PipelineDB.pm
@@ -23,27 +23,31 @@ sub get_rw_DBAdaptor {
 }
 
 sub get_pipeline_DBAdaptor {
-    my( $otter_db, $rw ) = @_;
+    my( $otter_db, $inherit_assembly_type ) = @_;
 
     require Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor;
-    return Bio::Otter::Lace::SatelliteDB::_get_DBAdaptor(
+    my $pipe_db = Bio::Otter::Lace::SatelliteDB::_get_DBAdaptor(
         $otter_db, 'pipeline_db', 'Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor');
+
+    if($inherit_assembly_type) {
+        $pipe_db->assembly_type($otter_db->assembly_type());
+    }
+
+    return $pipe_db;
 }
 
 sub get_pipeline_rw_DBAdaptor {
-    my( $otter_db ) = @_;
+    my( $otter_db, $inherit_assembly_type ) = @_;
     
     require Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor;
-    return Bio::Otter::Lace::SatelliteDB::_get_DBAdaptor(
+    my $pipe_db =  Bio::Otter::Lace::SatelliteDB::_get_DBAdaptor(
         $otter_db, 'pipeline_db_rw', 'Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor');
-}
 
-sub get_pipeline_v32_DBAdaptor {
-    my( $otter_db, $rw ) = @_;
+    if($inherit_assembly_type) {
+        $pipe_db->assembly_type($otter_db->assembly_type());
+    }
 
-    require Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor;
-    return Bio::Otter::Lace::SatelliteDB::_get_DBAdaptor(
-        $otter_db, 'pipeline_db_v32', 'Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor');
+    return $pipe_db;
 }
 
 1;
