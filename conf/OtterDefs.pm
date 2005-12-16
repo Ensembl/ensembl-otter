270d9ec02c6263ad6b467ae39324bdff5a4f1515
lg4 <lg4>
2005-12-16 17:19:05 +0000

    *** empty log message ***

--- conf/OtterDefs.pm
+++ conf/OtterDefs.pm
@@ -50,15 +50,15 @@ use Exporter();
 
 my $MAIN_DIR;
 
-# $MAIN_DIR               = '/nfs/team71/analysis/lg4/work';
-$MAIN_DIR               = '/humsql/test_otter';
+# $MAIN_DIR             = '/nfs/team71/analysis/lg4/work';
+$OTTER_SERVER_PORT      = 33999;
+$MAIN_DIR               = "/humsql/ottersrv_rel40_port$OTTER_SERVER_PORT";
 
 $OTTER_SERVER_ROOT 	    = $MAIN_DIR.'/ensembl-otter';
 $OTTER_ALTROOT    	    = $MAIN_DIR.'/ensembl-otter/otter_alt';
 $OTTER_SCRIPTDIR        = $OTTER_SERVER_ROOT . '/scripts/server';
 $OTTER_SERVER		    = Sys::Hostname::hostname();  # Local machine name
 $OTTER_MAX_CLIENTS          = 5;
-$OTTER_SERVER_PORT          = 33999;
 $OTTER_SCRIPT_TIMEOUT       = 60;
 $OTTER_SPECIES_FILE         = $OTTER_SERVER_ROOT . "/conf/species.dat";
 $OTTER_SPECIES              = read_species($OTTER_SPECIES_FILE);
