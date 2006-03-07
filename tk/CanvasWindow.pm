1e88fa0b677012224833eb77799e719a1dd4c673
lg4 <lg4>
2006-03-07 15:33:41 +0000

    *** empty log message ***

--- tk/CanvasWindow.pm
+++ tk/CanvasWindow.pm
@@ -964,6 +964,79 @@ sub next_message_id {
     }
 }
 
+
+##### Moved from MenuCanvasWindow - clipboard manipulation:
+#
+
+sub integers_from_clipboard {
+    my( $self ) = @_;
+
+    my $canvas = $self->canvas;
+
+    my( $text );
+    eval {
+        $text = $canvas->SelectionGet;
+    };
+    return if $@;
+    #warn "Trying to parse: [$text]\n";
+    
+    my( @ints );
+    # match fMap "blue box" DNA selection
+    if (@ints = $text =~ /Selection -?(\d+) ---> -?(\d+)/) {
+        if ($ints[0] == $ints[1]) {
+            # user clicked on single base pair
+            @ints = ($ints[0]);
+        }
+    } else {
+        # match general fMap "blue box" pattern
+        unless (@ints = $text =~ /^\S+\s+-?(\d+)\s+-?(\d+)\s+\(\d+\)/) {
+            # or just get all the integers
+            @ints = grep ! /\./, $text =~ /([\.\d]+)/g;
+        }
+    }
+    return @ints;
+}
+
+=head2 hash_from_clipboard
+
+       Args: hash of arrays
+             {key_name => [qr/(reg)(ex)/, array_index]...}
+   Function: Parse the AceDB clipboard
+Description: clipboard is split on space for the processing by supplied
+             regular expressions. Arrays returned will be empty if no
+             matches are found so a quick die unless @{$returned->{key_name}} 
+             will check that a match was found.
+     return: hash of arrays
+             {key_name => [ 'reg', 'ex' ]}
+
+=cut
+
+sub hash_from_clipboard {
+    my( $self, $regex_hash ) = @_;
+    warn "please pass me a nice hash" unless $regex_hash;
+    my $results = { map { $_ => 0 } keys %$regex_hash };
+
+    my $canvas = $self->canvas;
+
+    my( $text );
+    eval {
+        $text = $canvas->SelectionGet;
+    };
+    return if $@;
+#    warn "Trying to parse: [$text]\n";
+
+    my @s = split(/\s+/, $text);
+
+    foreach my $k(keys(%$regex_hash)){
+	my $regex = $regex_hash->{$k}->[0];
+	my $index = $regex_hash->{$k}->[1];
+	$results->{$k} = [ $s[$index] =~ $regex ] || [];
+    }
+
+    return $results;
+}
+
+
 1;
 
 __END__
