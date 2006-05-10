a92ca5d772ca12a2e0123f5d153855777b74406a
sp1 <sp1>
2006-05-10 11:09:46 +0000

    moved character handling to end handling due to internal buffer problem of expat parser

--- modules/Bio/Otter/Transform/SequenceSets.pm
+++ modules/Bio/Otter/Transform/SequenceSets.pm
@@ -8,9 +8,10 @@ use Bio::Otter::Lace::SequenceSet;
 our @ISA = qw(Bio::Otter::Transform);
 
 # ones were interested in 
-my $SUB_ELE = { map { $_ => 1 } qw(description vega_set_id analysis_priority)};
+my $SUB_ELE = { map { $_ => 1 } qw(description vega_set_id priority)};
 # super elements to the actual sequence set
 my $SUP_ELE = { map { $_ => 1 } qw(otter sequencesets) };
+my $value;
 
 # this should be in xsl and use xslt to transform and create the objects
 sub start_handler{
@@ -18,6 +19,7 @@ sub start_handler{
     my $xml  = shift;
     my $ele  = lc shift;
     my $attr = {@_};
+    $value='';
     $self->_check_version(@_) if $ele eq 'otter';
     if($ele eq 'sequenceset'){
         my $ss = Bio::Otter::Lace::SequenceSet->new();
@@ -32,23 +34,31 @@ sub start_handler{
     }
 }
 
-sub end_handler{ }
-
-sub char_handler{
+sub end_handler{ 
     my $self = shift;
     my $xml  = shift;
-    my $data = shift;
-    my $context = $xml->current_element();
+    $value =~ s/^\s*//;
+    $value =~ s/\s*$//;
+    my $context = shift;
     if($SUB_ELE->{$context}){
         my $context_method = $context;
         my $ss = $self->objects;
         my $current = $ss->[$#$ss];
         if($current->can($context_method)){
-            $current->$context_method($data);
+            $current->$context_method($value);
         }else{
-#            print STDERR "$current can't $context_method\n";
+            print STDERR "$current can't $context_method\n";
         }
     }
+}
+
+sub char_handler{
+  my $self = shift;
+  my $xml  = shift;
+  my $data = shift;
+  if ($data ne ""){
+    $value .= $data;
+  }
 }
 
 1;
