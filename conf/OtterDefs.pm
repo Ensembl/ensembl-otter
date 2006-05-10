189318daf8681acc01678f442e0f9f3be215ec5e
lg4 <lg4>
2006-05-10 14:22:28 +0000

    clonesequences

--- conf/OtterDefs.pm
+++ conf/OtterDefs.pm
@@ -83,6 +83,7 @@ $OTTER_GET_SCRIPTS          = {'/perl/get_region'     => 'get_region',
                                '/perl/get_locks'                  => 'get_locks',
                                '/perl/get_sequence_notes'         => 'get_sequence_notes',
                                '/perl/find_clones'                => 'find_clones',
+                               '/perl/get_clonesequences'         => 'get_clonesequences',
                               };
 $OTTER_POST_SCRIPTS         = {'/perl/write_region'   => 'write_region',
                                '/perl/unlock_region'  => 'unlock_region',
