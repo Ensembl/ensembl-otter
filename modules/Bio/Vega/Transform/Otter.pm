282a44ee9cfab6d821e89c504ff7a20c2bfd8ae7
jgrg <jgrg>
2006-05-15 15:02:18 +0000

    *** empty log message ***

---
+++ modules/Bio/Vega/Transform/Otter.pm
@@ -0,0 +1,20 @@
+
+### Bio::Vega::Transform::Otter
+
+package Bio::Vega::Transform::Otter;
+
+use strict;
+
+
+
+
+1;
+
+__END__
+
+=head1 NAME - Bio::Vega::Transform::Otter
+
+=head1 AUTHOR
+
+James Gilbert B<email> jgrg@sanger.ac.uk
+
