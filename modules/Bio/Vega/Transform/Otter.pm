10cfd9f50555e3751b5d29e18a72416056ae43e1
jgrg <jgrg>
2006-05-16 13:23:38 +0000

    *** empty log message ***

--- modules/Bio/Vega/Transform/Otter.pm
+++ modules/Bio/Vega/Transform/Otter.pm
@@ -30,11 +30,55 @@ sub initialize {
 
     $self->object_builders(
         {
-            exon       => 'build_Exon',
-            transcript => 'build_Transcript',
-            locus      => 'build_Locus',
+            exon                => 'build_Exon',
+            transcript          => 'build_Transcript',
+            locus               => 'build_Locus',
+            sequence_fragment   => 'build_Contig',
+            evidence            => 'build_Evidence',
+            feature             => 'build_Feature',
+            assembly_tag        => 'build_AssemblyTag',
+
+            # We don't currently do anything on encountering
+            # these end tags:
+            sequence_set        => 'report_set_end',
+            exon_set            => 'report_set_end',
+            evidence_set        => 'report_set_end',
+            otter               => 'report_set_end',
         }
     );
+    $self->set_multi_value_tags(
+        [
+            [ locus             => qw{ remark synonym } ],
+            [ transcript        => qw{ remark         } ],
+            [ sequence_fragment => qw{ remark keyword } ],
+        ]
+    );
+}
+
+sub report_set_end {
+    my ($self) = @_;
+    
+    # Do nothing
+}
+
+sub build_Contig {
+    my ($self, $data) = @_;
+    
+}
+
+sub build_Evidence {
+    my ($self, $data) = @_;
+    
+}
+
+sub build_Feature {
+    my ($self, $data) = @_;
+    
+}
+
+sub build_AssemblyTag {
+    my ($self, $data) = @_;
+    
 }
 
 sub build_Exon {
@@ -46,15 +90,13 @@ sub build_Exon {
         -strand    => $data->{'strand'},
         -stable_id => $data->{'stable_id'},
     );
+    ### Need to add Slice here.
     my $list = $exon_list{$self} ||= [];
     push @$list, $exon;
 }
 
 sub build_Transcript {
     my ($self, $data) = @_;
-
-    use Data::Dumper;
-    print STDERR Dumper($data);
 
     my $exons = delete $exon_list{$self};
 
@@ -79,7 +121,8 @@ sub build_Locus {
         -stable_id => $data->{'stable_id'},
         );
     foreach my $tsct (@$transcripts) {
-        $gene->add_Transcript($tsct);
+        ### Fails without slice attached
+        #$gene->add_Transcript($tsct);
     }
 
     my $list = $gene_list{$self} ||= [];
