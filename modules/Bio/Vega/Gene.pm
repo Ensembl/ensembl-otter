214f27065ade58284dbadec77db3d1895bec820b
sp1 <sp1>
2006-06-26 09:00:01 +0000

    more subs to suit vega

--- modules/Bio/Vega/Gene.pm
+++ modules/Bio/Vega/Gene.pm
@@ -1,27 +1,56 @@
 package Bio::Vega::Gene;
 
 use strict;
+use Bio::EnsEMBL::Utils::Argument qw ( rearrange );
+
 use base 'Bio::EnsEMBL::Gene';
+
 
 sub new {
   my($class,@args) = @_;
   my $self = $class->SUPER::new(@args);
-  my ($gene_info)  = $self->_rearrange([qw(INFO)],@args);
-  $self->gene_info($gene_info);
+  my ($gene_author)  = rearrange([qw(AUTHOR)],@args);
+  $self->gene_author($gene_author);
   return $self;
 }
 
-sub gene_info {
-   my ($obj,$value) = @_;
-   if( defined $value) {
-       if ($value->isa("Bio::Vega::GeneInfo")) {
-	   $obj->{'gene_info'} = $value;
-       } else {
-	   $obj->throw("Argument to gene_info must be a Bio::Vega::GeneInfo object.  Currently is [$value]");
-       }
-    }
-    return $obj->{'gene_info'};
+sub gene_author {
+  my ($obj,$value) = @_;
+  if( defined $value) {
+	 if ($value->isa("Bio::Vega::Author")) {
+		$obj->{'gene_author'} = $value;
+	 } else {
+		$obj->throw("Argument to gene_author must be a Bio::Vega::Author object.  Currently is [$value]");
+	 }
+  }
+  return $obj->{'gene_author'};
 }
+
+sub source  {
+
+  my $self = shift;
+  $self->{'source'} = shift if( @_ );
+  return ( $self->{'source'} || "havana" );
+
+}
+
+=head2 truncated_flag
+
+Either TRUE or FALSE (1 or 0), it flags whether
+the gene contains all its components that are
+stored in the database, and hence whether it is
+editable in the client.  Defaults to 0.
+
+=cut
+
+sub truncated_flag {
+  my( $self, $flag ) = @_;
+  if (defined $flag) {
+	 $self->{'truncated_flag'} = $flag ? 1 : 0;
+  }
+  return $self->{'truncated_flag'} || 0;
+}
+
 
 1;
 
