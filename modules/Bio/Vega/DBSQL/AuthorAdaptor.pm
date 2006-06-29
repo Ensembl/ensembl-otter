3f0eb24098392e28d558a8839db7050d69340f1b
sp1 <sp1>
2006-06-29 11:27:24 +0000

    added more fetch methods

--- modules/Bio/Vega/DBSQL/AuthorAdaptor.pm
+++ modules/Bio/Vega/DBSQL/AuthorAdaptor.pm
@@ -1,5 +1,3 @@
-### Bio::Vega::DBSQL::AuthorAdaptor.pm
-
 package Bio::Vega::DBSQL::AuthorAdaptor;
 
 use strict;
@@ -8,14 +6,146 @@ use Bio::EnsEMBL::Utils::Exception qw ( throw warning );
 
 use base 'Bio::EnsEMBL::DBSQL::BaseAdaptor';
 
+
+=head2 _generic_sql_fetch
+
+ Title   : _generic_sql_fetch
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub _generic_sql_fetch {
+  my( $self, $where_clause,$author ) = @_;
+  if (! defined $author ){
+	 $author=new Bio::Vega::Author;
+  }
+  my $sql = "
+	     SELECT a.author_id,
+             a.author_email,
+             a.author_name,
+             g.group_id,
+             g.group_name
+	     FROM author a,author_group g "
+	 .$where_clause.
+		" AND a.group_id=g.group_id ";
+  my $sth = $self->prepare($sql);
+  $sth->execute();
+  if (my $ref = $sth->fetchrow_hashref) {
+	 $author->dbID($ref->{author_id});
+	 $author->email($ref->{author_email});
+	 $author->name($ref->{author_name});
+	 my $group=new Bio::Vega::AuthorGroup;
+	 $group->dbID($ref->{group_id});
+	 $group->name($ref->{name});
+	 $author->group($group);
+	 return $author;
+  } else {
+	 return;
+  }
+
+}
+
+=head2 fetch_by_dbID
+
+ Title   : fetch_by_dbID
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub fetch_by_dbID {
+	my ($self,$id) = @_;
+
+	if (!defined($id)) {
+		$self->throw("Id must be entered to fetch an author object");
+	}
+
+	my $author = $self->_generic_sql_fetch(" where author_id = $id ");
+
+	return $author;
+}
+
+=head2 fetch_by_name
+
+ Title   : fetch_by_name
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+
+sub fetch_by_name {
+	my ($self,$name) = @_;
+
+	if (!defined($name)) {
+		$self->throw("Name must be entered to fetch an author object");
+	}
+
+	my $author = $self->_generic_sql_fetch("where author_name = \'$name\'");
+
+	return $author;
+}
+
+=head2 fetch_by_email
+
+ Title   : fetch_by_email
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub fetch_by_email {
+	my ($self,$email) = @_;
+
+	if (!defined($email)) {
+		$self->throw("Email address must be entered to fetch an author object");
+	}
+
+	my $author = $self->_generic_sql_fetch("where author_email= \'$email\'");
+
+	return $author;
+}
+	
+sub exists_in_db {
+  my ($self,$author) = @_;
+  if (!defined($author->name)) {
+	 throw("Name is empty in the author object - should be set to check for exsistence");
+  }
+  if (!defined($author->email)) {
+	 throw("email is empty in the author object - should be set to check for exsistence");
+  }
+  my $author_email=$author->email;
+  my $author_name=$author->name;
+  $author = $self->_generic_sql_fetch("where author_email= \'$author_email\' and author_name=\'$author_name\' ",$author);
+  return $author;
+}
+
 =head2 store
 
  Title   : store
  Usage   :
  Function:
  Example :
- Returns :
+ Returns : 
  Args    :
+
 
 =cut
 
@@ -29,20 +159,17 @@ sub store {
   my $author_name  = $author->name  || throw "Author does not have a name";
   my $author_email = $author->email || throw "Author does not have an email address";
   # Is this author already in the database?
-  if ($self->fetch_by_name_email($author)){
+
+  if ( $self->exists_in_db($author)){
 	 return 1;
   }
-
-
   my $group=$author->group;
-
   my $group_id=$group->dbID;
   my $group_name=$group->name;
   my $ga=$self->db->get_AuthorGroupAdaptor;
   if (!defined $group_id){
 	 $group_id=$ga->fetch_id_by_name($group_name);
   }
-
   if (!defined $group_id){
 	 warning("about to store new author-group $group_name");
 	 $ga->store($group);
@@ -55,45 +182,12 @@ sub store {
   $sth->execute($author_email, $author_name,$group_id);
   my $db_id = $sth->{'mysql_insertid'} || throw('Failed to get autoincremented ID from statement handle');
   $author->dbID($db_id);
-}
 
-sub fetch_by_name_email {
-  my ($self,$author) = @_;
-  if (!defined($author)) {
-	 $self->throw("Name must be entered to fetch an author object");
-  }
-  my $author_name=$author->name;
-  my $author_email=$author->email;
-  my $sql = q{
-		SELECT a.author_id,
-             g.group_id,
-             g.name
-		FROM author a,author_group g
-      WHERE a.author_name = ? AND a.author_email = ? AND a.group_id=g.group_id};
-  my $sth = $self->prepare($sql);
-  $sth->execute($author_name,$author_email);
-  if (my $ref = $sth->fetchrow_hashref) {
-	 $author->dbID($ref->{author_id});
-	 my $group=new Bio::Vega::AuthorGroup;
-	 $group->dbID($ref->{group_id});
-	 $group->name($ref->{name});
-	 $author->group($group);
-	 return $author;
-  } else {
-	 return;
-  }
 }
 
 1;
 
-__END__
-
-=head1 NAME - Bio::Vega::DBSQL::AuthorAdaptor.pm
-
-=head1 AUTHOR
-
-Sindhu K. Pillai B<email> sp1@sanger.ac.uk
-
+	
 
 
 
