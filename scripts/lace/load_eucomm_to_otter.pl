faeb4b722f7feea1ab8796108c4e9ae1a3370e12
ck1 <ck1>
2006-09-07 14:36:59 +0000

    *** empty log message ***

--- scripts/lace/load_eucomm_to_otter.pl
+++ scripts/lace/load_eucomm_to_otter.pl
@@ -1,4 +1,5 @@
 #!/usr/local/bin/perl -w
+# contact: ck1@sanger.ac.uk
 
 use strict;
 use Getopt::Long 'GetOptions';
@@ -7,171 +8,189 @@ use DBI;
 use Bio::EnsEMBL::SimpleFeature;
 use Bio::EnsEMBL::AssemblyMapper;
 use Bio::EnsEMBL::Analysis;
+
+# typical params:  perl load_eucomm_to_otter.pl -ds mouse -eucomm your_eucommfile -verbose > ! load_eucomm.log
+# the script no longer requires vega_db
+# but requires original dataset format like:
+# similarity     OTTMUSE00000134685      U1R     U1R     chr2-03 25200208        25200257        +       .       .
+# where column 5 has the sequence set info and can be queried from vega_db like
+#                                SELECT es.stable_id, s.name
+#								 FROM exon_stable_id es, exon e, seq_region s
+#								 WHERE es.stable_id = 'OTTMUSE00000000701'
+#								 AND es.exon_id=e.exon_id
+#								 AND e.seq_region_id=s.seq_region_id
+#								 AND s.coord_system_id=1
+
+
+my ($dataset, $eucommFile, $verbose);
+my $help = sub { exec('perldoc', $0) };
+
+Bio::Otter::Lace::Defaults::do_getopt('ds|dataset=s' => \$dataset, # eg, human or mouse or zebrafish
+									  'h|help'       => $help,
+									  'eucomm=s'     => \$eucommFile,
+									  'verbose'      => \$verbose
+									 ) or $help->(); # plus default options
+$help->() unless ( $dataset && $eucommFile );
+
+my $client      = Bio::Otter::Lace::Defaults::make_Client();
+my $dset        = $client->get_DataSet_by_name($dataset);	
+my $otter_db    = $dset->get_cached_DBAdaptor;
+my $mapper_ad   = $otter_db->get_AssemblyMapperAdaptor();
+my $sf_ad       = $otter_db->get_SimpleFeatureAdaptor();
+my $rca         = $otter_db->get_RawContigAdaptor;
+
 {
 
-  my ($dataset, @sets, $vega_db, $eucommFile, $verbose);
+  my $atype_feats = parse_eucomm_oligos($eucommFile);
+  load_simple_features($atype_feats, $otter_db);
+  check_duplicates($otter_db);
 
-  my $help = sub { exec('perldoc', $0) };
+}
 
-  Bio::Otter::Lace::Defaults::do_getopt('ds|dataset=s' => \$dataset,    # eg, human or mouse or zebrafish
-										'h|help'       => $help,
-										'vegadb=s'     => \$vega_db,
-										'eucomm=s'     => \$eucommFile,
-										'verbose'      => \$verbose
-									   ) or $help->();                  # plus default options
-  $help->() unless ( $dataset );
+sub parse_eucomm_oligos {
 
-  my $client      = Bio::Otter::Lace::Defaults::make_Client();	        # Bio::Otter::Lace::Client
-  my $dset        = $client->get_DataSet_by_name($dataset);             # Bio::Otter::Lace::DataSet
-  my $otter_db    = $dset->get_cached_DBAdaptor;                        # Bio::EnsEMBL::Containerr
-  my $mapper_ad   = $otter_db->get_AssemblyMapperAdaptor();
-  my $sf_ad       = $otter_db->get_SimpleFeatureAdaptor();
-  my $rca         = $otter_db->get_RawContigAdaptor;
-  my $vega        = connect_vega_db($vega_db);
-  my $atype_feats = parse_and_verify_eucomm_data($eucommFile, $vega);
+  my $eucommFile = shift;
+
+  my $atype_feats = {};
+
+  open(DATA, "$eucommFile") or die $!;
+  while ( <DATA> ) {
+	my @data = split(/\t/, $_);
+	# similarity     OTTMUSE00000134685      U1R     U1R     chr2-03 25200208        25200257        +       .       .
+	push(@{$atype_feats->{$data[4]}}, [$data[1], $data[2], $data[5], $data[6]]);
+  }
+
+  if ( $verbose ){
+	my $count;
+	foreach ( sort keys %$atype_feats ){
+	  $count += scalar @{$atype_feats->{$_}};
+	  warn sprintf("%-8s: %d\n", $_, scalar @{$atype_feats->{$_}});
+	}
+	warn "Total features from original eucomm oligo dataset: $count\n\n";
+  }
+  return $atype_feats;
+
+}
+
+sub load_simple_features {
+
+  my ($atype_feats, $otter_db) = @_;
+
+  # processing parsed dataset
 
   my @simpleFeatures;
 
-  foreach my $atype ( keys %$atype_feats ){
+  foreach my $atype ( sort keys %$atype_feats ) {
 
 	print "Working on $atype\n";
 
+	my $fcounter = 0;
+
 	$otter_db->assembly_type($atype);
+	$atype =~ /chr(\d+)-.+/i;
+	my $chr_digit = $1;
 
-	foreach my $chr_name ( keys %{$atype_feats->{$atype}} ){
+	foreach my $feat (  @{$atype_feats->{$atype}} ) {
 
-	  foreach my $feat (@{$atype_feats->{$atype}->{$chr_name}} ){
+	  my $exonSid    = $feat->[0];
+	  my $feat_lbl   = $exonSid."_$feat->[1]"."_$atype"; # eg, OTTMUSE00000180348_VR_chr2-03
+	  my $feat_start = $feat->[2];
+	  my $feat_end   = $feat->[3];
+	  my $strand     = 1;		# default by Eucomm
+	  my $chr_start  = 1;
+	  my $chr_end    = $otter_db->get_ChromosomeAdaptor()->fetch_by_chr_name($chr_digit)->length();
 
-		my $exonSid    = $feat->[0];
-		my $feat_start = $feat->[1];
-		my $feat_end   = $feat->[2];
-		my $feat_lbl   = $exonSid."_$feat->[3]";
-		my $strand     = 1; # default by Eucomm
-		my $chr_start  = 1;
-		my $chr_end    = $otter_db->get_ChromosomeAdaptor()->fetch_by_chr_name($chr_name)->length();
+	  # preparing coord mapper for whole chr of an assembly_type
+	  my $mapper     = $mapper_ad->fetch_by_type($atype);
+	  $mapper_ad->register_region($mapper, $atype, $chr_digit, $chr_start, $chr_end);
 
-		# preparing coord mapper for whole chr of an assembly_type
-		my $mapper     = $mapper_ad->fetch_by_type($atype);
-		$mapper_ad->register_region($mapper, $atype, $chr_name, $chr_start, $chr_end);
+	  print "\t$feat_lbl: start=$feat_start, end=$feat_end\n" if $verbose;
 
-		print "Feat_start: $feat_start, Feat_end: $feat_end, Label: $feat_lbl\n" if $verbose;
+	  # transforming chrom. coord to contig coord
+	  my @raw_coordlist = $mapper->map_coordinates_to_rawcontig( $chr_digit, $feat_start, $feat_end, $strand );
 
-		# transforming chrom. coord to contig coord
-		my @raw_coordlist = $mapper->map_coordinates_to_rawcontig( $chr_name, $feat_start, $feat_end, $strand );
-		for my $coord (@raw_coordlist) {
+	  if (scalar @raw_coordlist > 1 and $verbose) {
+		warn "\t$feat_lbl returns ", scalar @raw_coordlist, " features by feature mapper\n";
+	  }
 
-		  # make simplefeature obj
-		  print "mapped to: contig_id=".$coord->id().", start=".$coord->start().", end=".$coord->end().", strand=".$coord->strand().".\n" if $verbose;
+	  for my $coord (@raw_coordlist) {
 
-		  my $analysis = new Bio::EnsEMBL::Analysis;
+		# make simplefeature obj
+		print "\tmapped to: contig_id=".$coord->id().", start=".$coord->start().", end=".$coord->end().", strand=".$coord->strand().".\n\n" if $verbose;
 
-          $analysis->dbID(9);
-          $analysis->logic_name("EUCOMM_AUTO");
+		my $analysis = new Bio::EnsEMBL::Analysis;
 
-		  my $sf = new Bio::EnsEMBL::SimpleFeature;
-		  $sf->dbID($coord->id);
-		  $sf->start($coord->start);
-		  $sf->end($coord->end);
-		  $sf->strand($coord->strand);
-		  $sf->analysis($analysis);
-		  $sf->display_label($feat_lbl);
-		  $sf->score(1);
+		$analysis->dbID(9);
+		$analysis->logic_name("EUCOMM_AUTO");
 
-          # now attach a rawContig object to simplefeature
-          my $contig = $rca->fetch_by_dbID($coord->id);
-          $sf->attach_seq($contig);
+		my $sf = new Bio::EnsEMBL::SimpleFeature;
+		$sf->dbID($coord->id);
+		$sf->start($coord->start);
+		$sf->end($coord->end);
+		$sf->strand($coord->strand);
+		$sf->analysis($analysis);
+		$sf->display_label($feat_lbl);
+		$sf->score(1);
 
-		  push(@simpleFeatures, $sf);
-		}
+		# now attach a rawContig object to simplefeature
+		my $contig = $rca->fetch_by_dbID($coord->id);
+		$sf->attach_seq($contig);
+
+		$fcounter++;
+		push(@simpleFeatures, $sf);
 	  }
 	}
+	warn "$atype: $fcounter features returned by feature mapper\n" if $verbose;
   }
 
   # now store simple features
-  foreach my $sf (@simpleFeatures ){
+  my $counter = 0;
+  foreach my $sf (@simpleFeatures ) {
 	eval {
-      $sf_ad->store($sf);
-    };
+	  #$sf_ad->store($sf);
+	};
+	$counter++ unless $@;
   }
-  print "Losding successful!\n\n" unless $@;
+  print "Losding $counter EUCOMM OLIGO simple_features successfully!\n\n" unless $@;
 }
 
-sub parse_and_verify_eucomm_data {
+sub check_duplicates {
 
-  my ($eucommFile, $vega_db) = @_;
+  my $dbh = shift;
+  my $sth = $dbh->prepare(qq{
+							 SELECT contig_id, contig_start, contig_end, display_label, simple_feature_id
+                             FROM simple_feature
+							 where analysis_id = (SELECT analysis_id FROM analysis WHERE logic_name= 'EUCOMM_AUTO')
+							}
+						 );
+  $sth->execute;
 
-  open(EU, "<$eucommFile") or die $1;
+  my %simple_feature = ();
+  my @to_delete;
 
-  my $exonID_data = {};
-
-  while ( <EU> ){
-
-	# similarity      OTTMUSE00000020324      D1R     D1R     1       60710211        60710260        +       .       .
-	my @cols = split(/\t/, $_);
-	my $exonSid    = $cols[1];
-	my $label      = $cols[2];
-	my $chr        = $cols[4];
-	my $feat_start = $cols[5];
-	my $feat_end   = $cols[6];
-
-	push(@{$exonID_data->{$exonSid}->{$chr}}, [$exonSid, $feat_start, $feat_end, $label]);
+  while (my $row = $sth->fetchrow_arrayref) {
+    my $dupl = $row->[0].$row->[1].$row->[2].$row->[3];
+    push(@{$simple_feature{$dupl}},$row->[4]);
   }
-  close EU;
 
-  my @exonSIds;
-  map {push(@exonSIds, "'".$_."'")} keys %$exonID_data;
-  my $exonSIDs = join(',', @exonSIds);
-
-  # make sure that assembly_type that an exon_stable_id maps to is
-  # on the same chr as col. 5 in eucomm data
-
-  # s.name returns name of assembly_type, eg, chr1-02, in the vega database
-  my $sql = $vega_db->prepare(qq{
-								 SELECT es.stable_id, s.name
-								 FROM exon_stable_id es, exon e, seq_region s
-								 WHERE es.stable_id in ($exonSIDs)
-								 AND es.exon_id=e.exon_id
-								 AND e.seq_region_id=s.seq_region_id
-								 AND s.coord_system_id=1
-								}
-							 );
-  $sql->execute();
-
-  my $ok;
-  my $atype_feats = {};
-
-  while ( my ($exSID, $atype) = $sql->fetchrow() ) {
-	$atype =~ /chr(.+)-\d+/;
-	my $chr_a = $1;
-
-	foreach my $chr_b (keys %{$exonID_data->{$exSID}} ){
-	  if ($chr_b ne $chr_a){
-		warn "Features: $exSID\n";
-		map {warn "@$_\n"} @{$exonID_data->{$exSID}->{$chr_b}};
-		warn "mapped to different chromosomes: [$chr_a vs $chr_b]\n";
-	  }
-	  else {
-		$ok = 1;
-		push(@{$atype_feats->{$atype}->{$chr_b}}, @{$exonID_data->{$exSID}->{$chr_b}});
-	  }
+  if ( !%simple_feature ) {
+    print "Not getting any data! Somethins is wrong\n";
+    $sth->finish;
+    $dbh->disconnect;
+    exit(0)
+  }
+  foreach ( keys %simple_feature ) {
+	if ( scalar @{$simple_feature{$_}} > 1) {
+	  my @dupl = sort { $a<=>$b } @{$simple_feature{$_}};
+	  shift @dupl;				# keep the oldest one
+	  push(@to_delete, @dupl);  # kick the rest for trashing
 	}
   }
-  if ( $ok ){
-	$exonID_data = {}; # discard
-	return $atype_feats;
-  }
-  else {
-	die "Discrepancies in Eucomm mapping!";
-  }
+
+  warn "Found ", scalar @to_delete, " eucomm oligo simple_feature_id duplicates.\n";
+  warn "@to_delete" if $verbose;
 }
 
-sub connect_vega_db {
 
-  my $dbname = shift;
-
-  my $dbh = DBI->connect("DBI:mysql:$dbname:vegabuild:3304", "ottro", "", {RaiseError => 1})
-        || die "cannot connect to $dbname, $DBI::errstr";
-  return $dbh;
-}
 
 __END__
