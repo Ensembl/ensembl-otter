9229fac0423ec4cd6e7ac6bd23ec55db885250e7
sp1 <sp1>
2006-09-11 12:45:26 +0000

    contig lock module

---
+++ modules/Bio/Vega/ContigLock.pm
@@ -0,0 +1,67 @@
+package Bio::Vega::ContigLock;
+
+### Maybe simpler for each Lock to have-a Clone and an Author
+### (instead of a clone_id)?
+
+use strict;
+use Bio::EnsEMBL::Utils::Argument qw ( rearrange );
+use base qw(Bio::EnsEMBL::Storable);
+
+sub new {
+  my($class,@args) = @_;
+
+  my $self = bless {}, $class;
+
+  my ($dbID,$contig_id,$author,$timestamp,$hostname)  = 
+      rearrange([qw(DBID CONTIG_ID AUTHOR TIMESTAMP HOSTNAME
+                            )],@args);
+
+  $self->contig_id($contig_id);
+  $self->author($author);
+  $self->timestamp($timestamp);
+  $self->hostname($hostname);
+
+  return $self;
+}
+
+sub contig_id {
+    my( $self, $contig_id ) = @_;
+    if ($contig_id) {
+        $self->{'contig_id'} = $contig_id;
+    }
+    return $self->{'contig_id'};
+}
+
+sub author{
+   my ($self,$value) = @_;
+   if( defined $value) {
+		 if ($value->isa("Bio::Vega::Author")) {
+			 $self->{'author'} = $value;
+		 } else {
+			 $self->throw("Argument [$value] is not a Bio::Vega::Author");
+		 }
+	 }
+    return $self->{'author'};
+}
+
+sub timestamp{
+   my ($obj,$value) = @_;
+   if( defined $value) {
+      $obj->{'timestamp'} = $value;
+    }
+    return $obj->{'timestamp'};
+}
+
+sub hostname {
+    my( $self, $hostname ) = @_;
+    if ($hostname) {
+        $self->{'hostname'} = $hostname;
+    }
+    return $self->{'hostname'};
+}
+
+
+
+1;
+
+
