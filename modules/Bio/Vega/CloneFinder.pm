125a64ffdc0bae2da67eeb3e59e6a5058a7a6db1
lg4 <lg4>
2006-10-18 16:13:02 +0000

    *** empty log message ***

--- modules/Bio/Vega/CloneFinder.pm
+++ modules/Bio/Vega/CloneFinder.pm
@@ -68,12 +68,6 @@ sub register_feature {
                     @{ $feature->project($component) } ]
     );
 
-    if($cs_name ne $component) {
-        warn "LIST in its order is: ".join(', ',  map { $_->to_Slice()->seq_region_name() }
-                        sort { ($a->from_start() <=> $b->from_start())*$feature->strand() }
-                                            @{ $feature->project($component) } )."\n";
-    }
-
     push @{ $self->qnames_locators()->{$qname} }, $loc;
 }
 
