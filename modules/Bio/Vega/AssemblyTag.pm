c31976208acbfc6c816e1b58d15bf9b068c2df49
sp1 <sp1>
2007-01-04 12:40:37 +0000

    revised schema and API

--- modules/Bio/Vega/AssemblyTag.pm
+++ modules/Bio/Vega/AssemblyTag.pm
@@ -8,37 +8,14 @@ sub new {
 
   my($class,@args) = @_;
   my $self = $class->SUPER::new(@args);
-  my ($contig_id,$contig_name,$tag_type,$tag_info)  = rearrange([qw(CONTIG_ID CONTIG_NAME TAG_TYPE TAG_INFO)],@args);
-  $self->contig_id($contig_id);
-  $self->contig_name($contig_name);
+  my ($tag_type,$tag_info)  = rearrange([qw(TAG_TYPE TAG_INFO)],@args);
   $self->tag_type($tag_type);
   $self->tag_info($tag_info);
   return $self;
 
 }
 
-# start, end, strand, dbID(tag_id) methods are inherited
-
-##future this method can go away as we need only the contig_name method and create a contig_slice to which
-##the assembly tag object can be attached
-
-sub contig_id {
-  my ($self, $val) = @_;
-  if ($val){
-    $self->{contig_id} = $val;
-  }
-
-  return $self->{contig_id};
-}
-
-sub contig_name {
-  my ($self, $val) = @_;
-  if ($val){
-    $self->{contig_name} = $val;
-  }
-
-  return $self->{contig_name};
-}
+# start, end, strand, dbID(tag_id), slice methods are inherited
 
 sub tag_type {
  my ($self, $val) = @_;
@@ -56,26 +33,6 @@ sub tag_info {
   }
 
   return $self->{tag_info};
-}
-
-sub slice_by_contig_id {
-
-  my ($self,$contig_id)=@_;
-  my $sa=$self->db->get_SliceAdaptor;
-  my $slice=$sa->fetch_by_seq_region_id($contig_id);
-  $self->{'slice'}=$slice;
-  return $slice;
-
-}
-
-sub slice_by_contig_name {
-
-  my ($self,$contig_name)=@_;
-  my $sa=$self->db->get_SliceAdaptor;
-  my $slice=$sa->fetch_by_region('contig',$contig_name);
-  $self->{'slice'}=$slice;
-  return $slice;
-
 }
 
 1;
