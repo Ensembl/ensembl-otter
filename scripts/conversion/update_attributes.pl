718fbb2bea23b5672a9045b12dbae79dd15b4a27
st3 <st3>
2007-03-16 17:48:53 +0000

    patch from branch-new-seqstore

--- scripts/conversion/update_attributes.pl
+++ scripts/conversion/update_attributes.pl
@@ -59,7 +59,6 @@ use vars qw($SERVERROOT);
 
 BEGIN {
     $SERVERROOT = "$Bin/../../..";
-    unshift(@INC, "$SERVERROOT/ensembl-otter/modules");
     unshift(@INC, "$SERVERROOT/ensembl/modules");
     unshift(@INC, "$SERVERROOT/bioperl-live");
 }
@@ -70,7 +69,7 @@ use Bio::EnsEMBL::Utils::ConversionSupport;
 
 $| = 1;
 
-my $support = new Bio::EnsEMBL::Utils::ConversionSupport($SERVERROOT);
+our $support = new Bio::EnsEMBL::Utils::ConversionSupport($SERVERROOT);
 
 # parse options
 $support->parse_common_options(@_);
@@ -124,130 +123,192 @@ while (my $row = <$fh>) {
 close($fh);
 $support->log("Done reading ".scalar(@rows)." entries.\n");
 
-# if any attrib_types are loaded that are different from 
-# the file, a consistency problem is reported and the 
-# upload is not done.
+# check for consistency between database and file
 if (check_consistency($dbh, \@rows)) {
     # consistent
+    $support->log("Deleting from attrib_type...\n");
     $dbh->do("DELETE FROM attrib_type");
+    $support->log("Done.\n");
 
+    # load attributes from file
+    $support->log("Loading attributes from file...\n");
     load_attribs($dbh, \@rows);
+    $support->log("Done.\n");
+
 } else {
-    print STDERR "Repairing database, not consistent!\n";
+    # inconsistent, try to fix
+    $support->log("Database and definition file not consistent, repairing database...\n");
     repair($dbh, \@rows);
+    $support->log("Done.\n");
 }
 
+1;
+
+=head2 repair
+
+  Arg[1]      : DBI $dbh - a database handle
+  Arg[2]      : Hashref $attribs - hashref containing the attribute_types loaded
+                from the definition file
+  Description : 
+  Return type : none
+  Exceptions  : none
+  Caller      : internal
+
+=cut
 
 # move attrib types wih the same code to the common attrib_type table
 # ones that are not in the table move to an attrib_type_id that is not used
 sub repair {
-  my ($dbh, $attribs) = @_;
-  
-  my @tables = qw( seq_region_attrib misc_attrib translation_attrib transcript_attrib );
-  my $ref = $dbh->selectall_arrayref( "show create table attrib_type" );
-  my $create_table = $ref->[0]->[1];
+    my ($dbh, $attribs) = @_;
 
-  $dbh->do( "alter table attrib_type rename old_attrib_type" );
-  $dbh->do( $create_table );
+    my @tables = qw(seq_region_attrib misc_attrib translation_attrib transcript_attrib);
 
+    # create backup of attrib_type table
+    $support->log("Creating backup of attrib_type table...\n", 1);
+    my $ref = $dbh->selectall_arrayref("SHOW CREATE TABLE attrib_type");
+    my $create_table = $ref->[0]->[1];
+    $dbh->do("ALTER TABLE attrib_type RENAME old_attrib_type");
+    $dbh->do($create_table);
+    $support->log("Done.\n", 1);
 
-  load_attribs($dbh, $attribs);
+    # load attributes from file
+    $support->log("Loading attributes from file...\n", 1);
+    load_attribs($dbh, $attribs);
+    $support->log("Done.\n", 1);
 
-  $dbh->do( "delete oat " .
-	   "from old_attrib_type oat, attrib_type at " .
-	   "where oat.attrib_type_id = at.attrib_type_id " .
-	   "and oat.code = at.code" );
+    $support->log("Resolving inconsistencies...\n", 1);
+    $dbh->do(qq(
+        DELETE  oat
+        FROM    old_attrib_type oat, attrib_type at
+        WHERE   oat.attrib_type_id = at.attrib_type_id
+        AND     oat.code = at.code
+    ));
 
-  # what remains in old attrib type ?
-  #  Entries with a code that is unknown in general file 
-  #  and that shouldnt really happen. If it happens, the code
-  #  needs to ne appended to attrib_type table and the attrib
-  #  type_ids will be updated in the feature tables.
+    # what remains in old attrib type ?
+    #
+    # 1. Entries with a code that is unknown in general file and that shouldnt
+    # really happen. If it happens, the code needs to be appended to attrib_type
+    # table and the attrib type_ids will be updated in the feature tables.
+    #
+    # 2. Entries with a code that is known, but has different attrib_type_id.
+    # Feature tables will be updated.
 
-  #  Entries with a code that is known, but has different 
-  #  attrib_type_id. Feature tables will be updated.
+    $dbh->do(qq(
+        CREATE TABLE tmp_attrib_types
+        SELECT  oat.attrib_type_id, oat.code, oat.name, oat.description
+        FROM    old_attrib_type oat
+        LEFT JOIN attrib_type at
+                ON oat.code = at.code
+        WHERE   at.code IS NULL
+    ));
+    $dbh->do(qq(
+        INSERT INTO attrib_type (code, name, description)
+        SELECT  code, name, description
+        FROM    tmp_attrib_types
+    ));
 
-  $dbh->do( "create table tmp_attrib_types ".
-	   "select oat.attrib_type_id, oat.code, oat.name, oat.description " .
-	   "from old_attrib_type oat " .
-	   "left join attrib_type at " .
-	   "on oat.code = at.code " .
-	   "where at.code is null" );
-  $dbh->do( "insert into attrib_type( code, name, description) ".
-	   "select code, name, description ". 
-	   "from tmp_attrib_types" );
+    $ref = $dbh->selectall_arrayref("SELECT code FROM tmp_attrib_types");
+    $dbh->do("DROP TABLE tmp_attrib_types");
 
-  $ref = $dbh->selectall_arrayref( "select code from tmp_attrib_types" );
-  $dbh->do( "drop table tmp_attrib_types" );
-
-  print STDERR "Todo:\n";
-  if( @$ref ) {
-    print STDERR "  Missing codes ",join( ", ", map { $_->[0] } @$ref ),"\n";
-  }
-
-  my %missing_codes = map { $_->[0], 1 } @$ref;
-
-  $ref = $dbh->selectall_arrayref( "select code from old_attrib_type oat " );
-
-  my @updated_codes;
-  for my $code_ref ( @$ref ) {
-    if( ! exists $missing_codes{ $code_ref->[0] } ) {
-      push( @updated_codes, $code_ref->[0] );
+    if (@{ $ref }) {
+        $support->log_warning("Missing codes: ".join(", ", map { $_->[0] } @{ $ref })."\n", 2);
     }
-  }
 
-  print STDERR "  Updated codes ", join( ", ", @updated_codes ), "\n";
+    my %missing_codes = map { $_->[0], 1 } @{ $ref };
 
-  # now do multi table updates on all tables
-  for my $up_table ( @tables ) {
-    $dbh->do( "update $up_table tb, attrib_type at, old_attrib_type oat ".
-	     "set tb.attrib_type_id = at.attrib_type_id ".
-	     "where tb.attrib_type_id = oat.attrib_type_id ".
-	     "and oat.code = at.code " );
-  }
+    $ref = $dbh->selectall_arrayref("SELECT code FROM old_attrib_type oat");
 
-  $dbh->do( "drop table old_attrib_type" );
+    my @updated_codes;
+    for my $code_ref (@{ $ref }) {
+        if (!exists $missing_codes{$code_ref->[0]}) {
+            push @updated_codes, $code_ref->[0];
+        }
+    }
+
+    $support->log("Updated codes: ".join(", ", @updated_codes)."\n", 2);
+
+    # now do multi table updates on all tables
+    for my $up_table (@tables) {
+        $dbh->do(qq(
+            UPDATE  $up_table tb, attrib_type at, old_attrib_type oat
+            SET     tb.attrib_type_id = at.attrib_type_id
+            WHERE   tb.attrib_type_id = oat.attrib_type_id
+            AND     oat.code = at.code
+        ));
+    }
+    $support->log("Done.\n", 1);
+
+    $support->log("Dropping backup of attrib_type table...\n", 1);
+    $dbh->do("DROP TABLE old_attrib_type");
+    $support->log("Done.\n", 1);
 }
 
+=head2 load_attribs
+
+  Arg[1]      : DBI $dbh - a database handle
+  Arg[2]      : Hashref $attribs - hashref containing the attribute_types loaded
+                from the definition file
+  Description : 
+  Return type : none
+  Exceptions  : none
+  Caller      : internal
+
+=cut
 
 sub load_attribs {
-  my ($dbh, $attribs) = @_;
+    my ($dbh, $attribs) = @_;
     my $sth;
-    $sth = $dbh->prepare( "insert into attrib_type( attrib_type_id, code, name, description) ".
-			 "values(?,?,?,?)" ); 
-    for my $attrib ( @$attribs ) {
-      $sth->execute( $attrib->{'attrib_type_id'}, $attrib->{'code'},
-		     $attrib->{'name'}, $attrib->{'description'} );
+    $sth = $dbh->prepare(qq(
+        INSERT INTO attrib_type (attrib_type_id, code, name, description)
+        VALUES (?,?,?,?) 
+    )); 
+    foreach my $attrib (@{ $attribs }) {
+        $sth->execute(
+            $attrib->{'attrib_type_id'},
+            $attrib->{'code'},
+            $attrib->{'name'},
+            $attrib->{'description'}
+        );
     }
 }
-  
 
+=head2 check_consistency
 
-# alternatively consistency can be enforceed to a certain degree 
+  Arg[1]      : DBI $dbh - a database handle
+  Arg[2]      : Hashref $attribs - hashref containing the attribute_types loaded
+                from the definition file
+  Description : 
+  Return type : 1 if consistent, 0 if not
+  Exceptions  : none
+  Caller      : internal
+
+=cut
+
 sub check_consistency {
-  my $dbh = shift;
-  my $attribs = shift;
+    my $dbh = shift;
+    my $attribs = shift;
 
-  my ( %db_codes, %file_codes );
-  map { $file_codes{$_->{'attrib_type_id'}} = $_->{'code'}} @$attribs;
-  
-  my $sth = $dbh->prepare( "SELECT attrib_type_id, code, name, description ".
-			  "FROM attrib_type" );
-  $sth->execute();
-  while( my $arr = $sth->fetchrow_arrayref() ) {
-    $db_codes{ $arr->[0] } = $arr->[1];
-  }
+    my (%db_codes, %file_codes);
+    map { $file_codes{$_->{'attrib_type_id'}} = $_->{'code'} } @$attribs;
 
-  # check if any ids in the database colide with the file
-  my $consistent = 1;
-  for my $dbid ( keys %db_codes ) {
-    if(! exists $file_codes{ $dbid } || 
-       $file_codes{$dbid} ne $db_codes{ $dbid } ) {
-      $consistent = 0;
+    my $sth = $dbh->prepare(qq(
+        SELECT attrib_type_id, code, name, description
+        FROM attrib_type
+    ));
+    $sth->execute();
+    while (my $arr = $sth->fetchrow_arrayref) {
+        $db_codes{$arr->[0]} = $arr->[1];
     }
-  }
 
-  return $consistent;
+    # check if any ids in the database colide with the file
+    my $consistent = 1;
+    for my $dbid (keys %db_codes) {
+        if (!exists $file_codes{$dbid} || $file_codes{$dbid} ne $db_codes{$dbid}) {
+            $consistent = 0;
+        }
+    }
+
+    return $consistent;
 }
 
 
