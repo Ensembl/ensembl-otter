d840633ae3a469113c57e587d079433a742a51b0
lg4 <lg4>
2007-05-23 15:14:41 +0000

    more diag

--- modules/Bio/Vega/Writer.pm
+++ modules/Bio/Vega/Writer.pm
@@ -22,6 +22,8 @@ sub formatclosetag{
 sub formatopenendtag{
   my ($self,$name,$indent,$value)=@_;
   $indent=$indent+2;
+  if(!defined($name)) { warn "NAME is not defined"; }
+  if(!defined($value)) { warn "VALUE is not defined"; }
   return ((' ' x $indent).'<'.$name.'>'.$value.'</'.$name.">\n")
 }
 
