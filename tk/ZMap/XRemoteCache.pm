89be41a21cddab9e3ed88bc9501537c72bc20098
rds <rds>
2007-07-23 15:05:37 +0000

    improved version of the cache. - clients are available to find zmap's from other xaceseqchooser instances - clients know about which actions they support

--- tk/ZMap/XRemoteCache.pm
+++ tk/ZMap/XRemoteCache.pm
@@ -5,116 +5,260 @@ use strict;
 use warnings;
 
 use X11::XRemote;
+use Data::Dumper;
 
-# self = { 
-#     _clients => {
-#         '<WindowID>' => <X11::XRemote>instance
-#     },
-#     _name2id => {
-#         '<Name>' => '<WindowID>',
-#     }
-# }
+my $CLIENT_DEBUG = 1;
+my $CACHE_DEBUG  = 1;
+
+my $object_cache = {
+    # 'window_id' => { object  => X11::XRemote, 
+    #                  pid     => $process_id, 
+    #                  actions => [ qw(actions accepted by window) ],
+    # },
+};
+
+# A window has  1 pid, 1 pid  has many windows. A window  has a single
+# list of  actions, each  list of actions  has many  windows, possibly
+# spread over multiple pids.
 
 sub new{
     my $pkg = shift;
     my $self = {};
-    $self->{'_clients'} ||= {};
-    $self->{'_name2id'} ||= {};
-    $self->{'_id2name'} ||= {};
-    return bless($self, $pkg);
+    $self->{'_clients'} = \$object_cache;
+    $self->{'_user_keys'} = {};
+    $self->{'_self_windows'} = {};
+    bless($self, $pkg);
+
+    return $self;
 }
 
-sub add_client_with_name{
-    my ($self, $client, $name) = @_;
+sub insert_lookup{
+    my ($self, $key, $value) = @_;
+    $self->{'_user_keys'}->{$key} = $value;
+    warn "Inserting $key - $value" if $CACHE_DEBUG;
+    return 1;
+}
+sub lookup_value{
+    my ($self, $key, $value) = @_;
+    $value = $self->{'_user_keys'}->{$key};
+    return $value;
+}
+# access the class data
+sub _get_clients_cache{
+    my ($self) = @_;
 
-    warn "name is required"     if(!defined($name));
+    if(ref $self){
+        return ${ $self->{'_clients'} };
+    }else{
+        return $object_cache;
+    }
+
+    return {};
+}
+
+sub get_pid_list{
+    my ($self) = @_;
+    my ($list, $cache, $pid_hash);
+    
+    $list ||= [];
+
+    $cache = $self->_get_clients_cache();
+
+    foreach my $xwid(keys(%$cache)){
+        my $pid = $cache->{$xwid}->{'pid'};
+        if(defined $pid){ $pid_hash->{$pid} = 1; }
+    }
+
+    push(@$list, keys(%$pid_hash));
+
+    return $list;
+}
+
+sub add_client_with_pid_actions{
+    my ($self, $client, $pid, @actions) = @_;
+
     warn "client is required"    if(!defined($client));
+    warn "pid is required"       if(!defined($pid));
     
     my $id = $client->window_id();
 
-    warn "Are you _really_ sure? (client with id '$id' already exists)"    if($self->get_client_with_id($id));
+    warn "Are you _really_ sure? (client with id '$id' already exists)" if($self->get_client_with_id($id));
     
-    $self->{'_clients'}->{$id}   = $client;
-    $self->{'_name2id'}->{$name} = $id;
-    $self->{'_id2name'}->{$id}   = $name;
+    my $cache = $self->_get_clients_cache();
+
+    warn "Adding client $id" if $CACHE_DEBUG;
+    $cache->{$id} = {
+        'object'  => $client,
+        'pid'     => $pid,
+        'actions' => [ @actions ],
+    };
+    $self->{'_self_windows'}->{$id} = $id;
 
     return 1;
 }
 
+sub _internal_get_cache_with_id{
+    my ($self, $id, $entry) = @_;
+
+    my $cache = $self->_get_clients_cache();
+
+    if($id){
+        $entry = $cache->{$id};
+    }else{
+        $entry = undef;
+    }
+
+    return $entry;
+}
+
 sub get_client_with_id{
     my ($self, $id, $client) = @_;
-    if($id){
-        $client = $self->{'_clients'}->{$id};
+
+    if(my $entry = $self->_internal_get_cache_with_id($id)){
+        $client = $entry->{'object'};
     }else{
         $client = undef;
     }
     return $client;
 }
 
-sub get_client_with_name{
-    my ($self, $name, $id) = @_;
-    $id = $self->{'_name2id'}->{$name};
-    return $self->get_client_with_id($id);
+sub get_clients_with_pid{
+    my ($self, $pid) = @_;
+
+    my $client_list = [];
+
+    my $cache = $self->_get_clients_cache();
+
+    foreach my $id(keys(%$cache)){
+        if ($pid eq $cache->{$id}->{'pid'}){
+            push(@$client_list, $cache->{$id}->{'object'});
+        }
+    }
+
+    return $client_list;
+}
+
+sub _internal_get_client_for_action_pid{
+    my ($self, $cache, $requested_action, $pid) = @_;
+
+    my $client = undef;
+
+    foreach my $id(keys(%$cache)){
+        my ($tmp_client, $tmp_pid, $tmp_actions) = ($cache->{$id}->{'object'}, 
+                                                    $cache->{$id}->{'pid'}, 
+                                                    $cache->{$id}->{'actions'});
+        if(defined($pid)){
+            next unless $tmp_pid eq $pid;
+        }
+        $tmp_actions ||= [];
+        foreach my $action(@$tmp_actions){
+            if($requested_action eq $action){
+                $client = $tmp_client;
+            }
+        }
+    }
+
+    return $client;
+
+}
+
+sub get_client_for_action_pid{
+    my ($self, $requested_action, $pid) = @_;
+
+    my $cache  = $self->_get_clients_cache();
+
+    my $client = $self->_internal_get_client_for_action_pid($cache, $requested_action, $pid);
+
+    return $client;
+}
+
+sub get_own_client_for_action_pid{
+    my ($self, $requested_action, $pid) = @_;
+
+    my $cache;
+
+    foreach my $id(keys(%{$self->{'_self_windows'}})){
+        $cache->{$id} = $self->_internal_get_cache_with_id($id);
+    }
+
+    my $client = $self->_internal_get_client_for_action_pid($cache, $requested_action, $pid);
+
+    return $client;
 }
 
 sub remove_client_with_id{
     my ($self, $id, $name) = @_;
 
-    $name = $self->{'_id2name'}->{$id};
+    my $client = $self->get_client_with_id($id);
 
-    $self->remove_client_with_name($name);
+    if($client){
+        my $wid   = $client->window_id();
+        my $cache = $self->_get_clients_cache();
+        warn "Client Window id != hash key id" if ($id ne $wid);
+        delete $cache->{$id};
+        delete $self->{'_self_windows'}->{$id};
+    }
 
     return undef;
 }
 
-sub remove_client_with_name{
-    my ($self, $name, $client) = @_;
+sub remove_clients_to_bad_windows{
+    my ($self) = @_;
+    my $cache = $self->_get_clients_cache();
 
-    $client = $self->get_client_with_name($name);
-
-    if($client){
-        my $id = $client->window_id();
-        delete $self->{'_id2name'}->{$id};
-        delete $self->{'_name2id'}->{$name};
-        delete $self->{'_clients'}->{$id};
+    warn "bad window cleaning" if $CACHE_DEBUG;
+    foreach my $id(keys(%$cache)){
+        if(my $xr = $cache->{$id}->{'object'}){
+            if(!$xr->ping()){
+                warn sprintf "client %s bad removing", $id if $CACHE_DEBUG;
+                delete $cache->{$id};
+                delete $self->{'_self_windows'}->{$id};
+            }else{
+                warn sprintf "client %s not bad", $id if $CACHE_DEBUG;
+            }
+        }else{
+            delete $cache->{$id};
+            delete $self->{'_self_windows'}->{$id};
+        }
+            
     }
-
-    return ;
+    return undef;
 }
 
-sub create_client_with_name_id{
-    my ($self, $requested_name, $requested_id) = @_;
+sub create_client_with_pid_id_actions{
+    my ($self, $requested_pid, $requested_id, @actions) = @_;
     my ($cached_name, $cached_id);
 
-    my $client = $self->get_client_with_name($requested_name);
+    my $client = $self->get_client_with_id($requested_id);
+
+    my $allow_overwrite = 0;
 
     if($client){
-        warn "Are you sure? (client with name '$requested_name' already exists)";
-        $cached_id = $client->window_id();
-        if($cached_id eq $requested_id){
-            warn "identical";
-        }else{
-            warn "requested id != cached id so creating new X11::Xremote instance";
+        warn "Are you sure? (client with window id '$requested_id' already exists)";
+        if($allow_overwrite){
+            $cached_id = $client->window_id();
             $client = X11::XRemote->new(-id     => $requested_id, 
                                         -server => 0,
-                                        -_DEBUG => 1);
-            $self->add_client_with_name($client, $requested_name);            
+                                        -_DEBUG => $CLIENT_DEBUG);
+            $self->add_client_with_pid_actions($client, $requested_pid, @actions);            
         }
     }else{
-        $client = $self->get_client_with_id($requested_id);
-            
-        if($client){
-            warn "Are you sure? (client with id '$requested_id' already exists)";
-            # cached_name cannot eq requested_name
-        }else{
-            $client = X11::XRemote->new(-id     => $requested_id, 
-                                        -server => 0,
-                                        -_DEBUG => 1);
-            $self->add_client_with_name($client, $requested_name);
-        }
+        $client = X11::XRemote->new(-id     => $requested_id, 
+                                    -server => 0,
+                                    -_DEBUG => $CLIENT_DEBUG);
+        $self->add_client_with_pid_actions($client, $requested_pid, @actions);
     }
 
     return $client;
+}
+
+
+sub DESTROY{
+    my ($self) = @_;
+
+    my $cache = $self->_get_clients_cache();
+
+    print Dumper $cache if $CACHE_DEBUG;
 }
 
 1;
