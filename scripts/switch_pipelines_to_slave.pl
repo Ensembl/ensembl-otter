1c549a3e773514568406c4f73e064e17bc7d18e9
lg4 <lg4>
2007-09-18 13:35:11 +0000

    loutre databases are also included

--- scripts/switch_pipelines_to_slave.pl
+++ scripts/switch_pipelines_to_slave.pl
@@ -25,6 +25,29 @@ sub connect_with_params {
 	return DBI->connect ($datasource, $username, $password, { RaiseError => 1 });
 }
 
+sub switch_over {
+    my ($dbh, $dbname) = @_;
+
+    my $sthm = $dbh->prepare("SELECT meta_key, meta_value FROM $dbname.meta where meta_key in ($affected_list)");
+    $sthm->execute();
+    while(my ($meta_key, $meta_value) = $sthm->fetchrow()) {
+        $meta_value=~s/\n/ /g;
+        if($meta_value=~/otterpipe\d/) {
+            print "$dbname : $meta_key\n\t$meta_value\n-------------\n";
+            if($meta_value=~s/3302/3312/) {
+                $meta_value=~s/otterpipe1/otterslave/;
+            } elsif($meta_value=~s/3303/3313/) {
+                $meta_value=~s/otterpipe2/otterslave/;
+            }
+            $meta_value=~s/'/"/g;
+
+            print "\t$meta_value\n===============\n\n\n";
+
+            $dbh->do("UPDATE $dbname.meta set meta_value='$meta_value' where meta_key='$meta_key'");
+        }
+    }
+}
+
 sub main {
     my $dbh = connect_with_params(
         '-HOST'   => 'otterlive',
@@ -33,29 +56,18 @@ sub main {
         '-PASS'   => 'wibble',
     );
 
-    my $sth = $dbh->prepare("SHOW DATABASES LIKE 'otter_%'");
-    $sth->execute();
-
-    while(my($dbname) = $sth->fetchrow()) {
-        my $sthm = $dbh->prepare("SELECT meta_key, meta_value FROM $dbname.meta where meta_key in ($affected_list)");
-        $sthm->execute();
-        while(my ($meta_key, $meta_value) = $sthm->fetchrow()) {
-            $meta_value=~s/\n/ /g;
-            if($meta_value=~/otterpipe\d/) {
-                print "$dbname : $meta_key\n\t$meta_value\n-------------\n";
-                if($meta_value=~s/3302/3312/) {
-                    $meta_value=~s/otterpipe1/otterslave/;
-                } elsif($meta_value=~s/3303/3313/) {
-                    $meta_value=~s/otterpipe2/otterslave/;
-                }
-                $meta_value=~s/'/"/g;
-
-                print "\t$meta_value\n===============\n\n\n";
-
-                $dbh->do("UPDATE $dbname.meta set meta_value='$meta_value' where meta_key='$meta_key'");
-            }
-        }
+    my $sth1 = $dbh->prepare("SHOW DATABASES LIKE 'otter_%'");
+    $sth1->execute();
+    while(my($dbname) = $sth1->fetchrow()) {
+        switch_over($dbh, $dbname);
     }
+
+    my $sth2 = $dbh->prepare("SHOW DATABASES LIKE 'loutre_%'");
+    $sth2->execute();
+    while(my($dbname) = $sth2->fetchrow()) {
+        switch_over($dbh, $dbname);
+    }
+
     $dbh->disconnect ();
 }
 
