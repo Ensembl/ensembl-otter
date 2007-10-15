02301e10a78c6ce5ef53396a89f4ce132406e76d
st3 <st3>
2007-10-15 15:53:53 +0000

    modified for loutre

--- scripts/conversion/xref/add_vega_xrefs.pl
+++ scripts/conversion/xref/add_vega_xrefs.pl
@@ -1,282 +1,312 @@
 #!/usr/local/bin/perl
 
+=head1 NAME
+
+add_vega_xrefs.pl - add xrefs to display gene, transcript and translation names
+
+=head1 SYNOPSIS
+
+add_vega_xrefs.pl [options]
+
+General options:
+    --conffile, --conf=FILE             read parameters from FILE
+                                        (default: conf/Conversion.ini)
+
+    --dbname, db_name=NAME              use database NAME
+    --host, --dbhost, --db_host=HOST    use database host HOST
+    --port, --dbport, --db_port=PORT    use database port PORT
+    --user, --dbuser, --db_user=USER    use database username USER
+    --pass, --dbpass, --db_pass=PASS    use database passwort PASS
+    --logfile, --log=FILE               log to FILE (default: *STDOUT)
+    --logpath=PATH                      write logfile to PATH (default: .)
+    --logappend, --log_append           append to logfile (default: truncate)
+    -v, --verbose                       verbose logging (default: false)
+    -i, --interactive=0|1               run script interactively (default: true)
+    -n, --dry_run, --dry=0|1            don't write results to database
+    -h, --help, -?                      print help (this message)
+
+Specific options:
+    --chromosomes, --chr=LIST           only process LIST chromosomes
+    --gene_stable_id, --gsi=LIST|FILE   only process LIST gene_stable_ids
+                                        (or read list from FILE)
+    --gene_type=TYPE                    only process genes of type TYPE
+    --start_gid=NUM                     start at gene with gene_id NUM
+    --prune                             delete all xrefs (except Interpro) and
+                                        gene/transcript.display_xref_ids before
+                                        running the script
+
+=head1 DESCRIPTION
+
+This script retrieves annotated gene/transcript names and adds them as xrefs
+to genes/transcripts/translations, respectively, setting them as display_xrefs.
+
+=head1 LICENCE
+
+This code is distributed under an Apache style licence:
+Please see http://www.ensembl.org/code_licence.html for details
+
+=head1 AUTHOR
+
+Steve Trevanion <st3@sanger.ac.uk>
+Patrick Meidl <meidl@ebi.ac.uk>
+
+=head1 CONTACT
+
+Post questions to the EnsEMBL development list ensembl-dev@ebi.ac.uk
+
+=cut
+
 use strict;
+use warnings;
+no warnings 'uninitialized';
 
-use Bio::Otter::DBSQL::DBAdaptor;
+use FindBin qw($Bin);
+use vars qw($SERVERROOT);
+
+BEGIN {
+    $SERVERROOT = "$Bin/../../../..";
+    unshift(@INC, "$SERVERROOT/ensembl/modules");
+    unshift(@INC, "$SERVERROOT/bioperl-live");
+}
+
 use Getopt::Long;
-
-my $host   = 'ecs2a';
-my $user   = 'ensadmin';
-my $pass   = 'ensembl';
-my $port   = 3306;
-my $dbname = 'otter_merged_chrs_with_anal';
-
-my @chromosomes;
-my $path = 'VEGA';
-my $do_store = 0;
-my $gene_type;
-my $start_gid;
-my @gene_stable_ids;
-
-my $opt_h;
+use Pod::Usage;
+use Bio::EnsEMBL::Utils::ConversionSupport;
 
 $| = 1;
 
-&GetOptions(
-	    'host:s'              => \$host,
-	    'dbname:s'            => \$dbname,
-	    'port:n'              => \$port,
-	    'user:s'              => \$user,
-	    'pass:s'              => \$pass,
-	    'path:s'              => \$path,
-	    'chromosomes:s'       => \@chromosomes,
-	    'gene_stable_id:s'    => \@gene_stable_ids,
-	    'store'               => \$do_store,
+my $support = new Bio::EnsEMBL::Utils::ConversionSupport($SERVERROOT);
 
-	    'h'                   => \$opt_h,
-
-	    'gene_type:s'         => \$gene_type,
-	    'start_gid:s'         => \$start_gid,
+# parse options
+$support->parse_common_options(@_);
+$support->parse_extra_options(
+    'chromosomes|chr=s@',
+    'gene_stable_id|gsi=s@',
+    'gene_type=s',
+    'start_gid=s',
+    'prune=s',
+);
+$support->allowed_params(
+    $support->get_common_params,
+    'chromosomes',
+    'gene_stable_id',
+    'gene_type',
+    'start_gid',
+    'prune',
 );
 
-if($opt_h){
-  print<<ENDOFTEXT;
-add_vega_xrefs.pl
-
-  -host           host    host of mysql instance ($host)
-  -dbname         dbname  database ($dbname)
-  -port           port    port ($port)
-  -user           user    user ($user)
-  -pass           pass    password 
-
-  -path           path    path ($path)
-  -chromosomes    chr,[chr]
-  -store                  write xrefs to database
-
-  -gene_stable_id gsi[,gsi] (list or file containing list)
-
-  -h                      this help
-ENDOFTEXT
-  exit 0;
+if ($support->param('help') or $support->error) {
+    warn $support->error if $support->error;
+    pod2usage(1);
 }
 
-if (scalar(@chromosomes)) {
-  @chromosomes = split (/,/, join (',', @chromosomes));
+$support->comma_to_list('chromosomes');
+$support->list_or_file('gene_stable_id');
+
+# ask user to confirm parameters to proceed
+$support->confirm_params;
+
+# get log filehandle and print heading and parameters to logfile
+$support->init_log;
+
+# connect to database and get adaptors (caching features on one slice only)
+my $dba = $support->get_database('ensembl');
+my $sa = $dba->get_SliceAdaptor();
+my $ga = $dba->get_GeneAdaptor();
+my $ea = $dba->get_DBEntryAdaptor();
+
+# statement handles for display_xref_id updates
+my $sth_gene = $dba->dbc->prepare("update gene set display_xref_id=? where gene_id=?");
+my $sth_trans = $dba->dbc->prepare("update transcript set display_xref_id=? where transcript_id=?");
+
+# delete all xrefs if --prune option is used
+if ($support->param('prune') and $support->user_proceed('Would you really like to delete all xrefs (except Interpro) before running this script?')) {
+
+    my $num;
+    # xrefs
+    $support->log("Deleting all xrefs (except Interpro)...\n");
+    $num = $dba->dbc->do(qq(
+        DELETE x
+        FROM xref x, external_db ed
+        WHERE x.external_db_id = ed.external_db_id
+        AND ed.db_name <> 'Interpro'
+    ));
+    $support->log("Done deleting $num entries.\n");
+
+    # object_xrefs
+    $support->log("Deleting all object_xrefs...\n");
+    $num = $dba->dbc->do(qq(DELETE FROM object_xref));
+    $support->log("Done deleting $num entries.\n");
+
+    # gene.display_xref_id
+    $support->log("Resetting gene.display_xref_id...\n");
+    $num = $dba->dbc->do(qq(UPDATE gene set display_xref_id = 0));
+    $support->log("Done resetting $num genes.\n");
+    
+    # transcript.display_xref_id
+    $support->log("Resetting transcript.display_xref_id...\n");
+    $num = $dba->dbc->do(qq(UPDATE transcript set display_xref_id = 0));
+    $support->log("Done resetting $num transcripts.\n");
 }
 
-my %gene_stable_ids;
-if (scalar(@gene_stable_ids)) {
-  my $gene_stable_id=$gene_stable_ids[0];
-  if(scalar(@gene_stable_ids)==1 && -e $gene_stable_id){
-    # 'gene' is a file
-    @gene_stable_ids=();
-    open(IN,$gene_stable_id) || die "cannot open $gene_stable_id";
-    while(<IN>){
-      chomp;
-      push(@gene_stable_ids,$_);
-    }
-    close(IN);
-  }else{
-    @gene_stable_ids = split (/,/, join (',', @gene_stable_ids));
-  }
-  print "Using list of ".scalar(@gene_stable_ids)." gene stable ids\n";
-  %gene_stable_ids = map {$_,1} @gene_stable_ids;
-}
+my $found = 0;
+my @gene_stable_ids = $support->param('gene_stable_id');
+my %gene_stable_ids = map { $_, 1 } @gene_stable_ids;
+my $chr_length = $support->get_chrlength($dba);
+my @chr_sorted = $support->sort_chromosomes($chr_length);
 
-my $db = new Bio::Otter::DBSQL::DBAdaptor(
-  -host   => $host,
-  -user   => $user,
-  -port   => $port,
-  -pass   => $pass,
-  -dbname => $dbname
-);
-$db->assembly_type($path);
+# loop over chromosomes
+$support->log("Looping over chromosomes: @chr_sorted\n\n");
+foreach my $chr (@chr_sorted) {
+    $support->log_stamped("> Chromosome $chr (".$chr_length->{$chr}."bp).\n\n");
 
-my $sa  = $db->get_SliceAdaptor();
-my $aga = $db->get_GeneAdaptor();
-my $adx = $db->get_DBEntryAdaptor();
+    # fetch genes from db
+    $support->log("Fetching genes...\n");
+    my $slice = $sa->fetch_by_region('toplevel', $chr);
+    my $genes = $slice->get_all_Genes;
+    $support->log("Done fetching ".scalar @$genes." genes.\n\n");
 
-my $chrhash = get_chrlengths($db,$path);
+    # loop over genes
+    my ($gnum, $tnum, $tlnum);
+ GENE:
+    foreach my $gene (@$genes) {
+        my $gsi = $gene->stable_id;
+        my $gid = $gene->dbID;
 
-#filter to specified chromosome names only
-if (scalar(@chromosomes)) {
-  foreach my $chr (@chromosomes) {
-    my $found = 0;
-    foreach my $chr_from_hash (keys %$chrhash) {
-      if ($chr_from_hash =~ /^${chr}$/) {
-        $found = 1;
-        last;
-      }
-    }
-    if (!$found) {
-      print "Didn't find chromosome named $chr in database $dbname\n";
-    }
-  }
- HASH: 
-  foreach my $chr_from_hash (keys %$chrhash) {
-    foreach my $chr (@chromosomes) {
-      if ($chr_from_hash =~ /^${chr}$/) { next HASH; }
-    }
-    delete($chrhash->{$chr_from_hash});
-  }
-}
+        # filter to user-specified gene_stable_ids
+        if (scalar(@gene_stable_ids)){
+            next unless $gene_stable_ids{$gsi};
+        }
 
-my $first=0;
-foreach my $chr (reverse sort bychrnum keys %$chrhash) {
-  print STDERR "Chr $chr from 1 to " . $chrhash->{$chr} . "\n";
-  my $chrstart = 1;
-  my $chrend   = $chrhash->{$chr};
-
-  my $slice = $sa->fetch_by_chr_start_end($chr, 1, $chrhash->{$chr});
-
-  print "Fetching genes\n";
-  my $genes = $aga->fetch_by_Slice($slice);
-  print "Done fetching genes\n";
-
-  foreach my $gene (@$genes) {
-    my $gsi=$gene->stable_id;
-    if(scalar(@gene_stable_ids)){
-      next unless $gene_stable_ids{$gsi};
-    }
-    my $gene_name;
-    if ($gene->gene_info->name && $gene->gene_info->name->name) {
-      $gene_name = $gene->gene_info->name->name;
-    } else {
-      $gene_name = $gene->stable_id;
-    }
-    if($gene_type){
-      if($gene_type ne $gene->type){
-	print "Gene $gene_name skipped - not of type $gene_type\n";
-	next;
-      }
-    }
-    # hack to allow a restart
-    if($start_gid && !$first){
-      my $gid=$gene->dbID;
-      if($gid==$start_gid){
-	$first=1;
-	print "Found $gid\n";
-      }
-      print "Skipping $gene_name $gid - waiting for start\n";
-      next;
-    }
-
-    my $dbentry=Bio::EnsEMBL::DBEntry->new(-primary_id=>$gene->stable_id,
-                                           -display_id=>$gene_name, 
-                                           -version=>1,
-                                           -release=>1,
-                                           -dbname=>"Vega_gene",
-                                          );
-    $dbentry->status('KNOWN');
-    $gene->add_DBEntry($dbentry);
-    $adx->store($dbentry,$gene->dbID,'Gene') if $do_store;
-
-    my $sth = $db->prepare("update gene set display_xref_id=" .      
-                             $dbentry->dbID .
-                           " where gene_id=" . $gene->dbID);
-    print $sth->{Statement} . ";\n";
-    $sth->execute;
-
-
-    foreach my $trans (@{$gene->get_all_Transcripts}){
-      my $tran_name;
-      if ($trans->transcript_info->name) {
-        $tran_name = $trans->transcript_info->name;
-      } else {
-        $tran_name = $trans->stable_id;
-      }
-      my $dbentry=Bio::EnsEMBL::DBEntry->new(-primary_id=>$trans->stable_id,
-                                             -display_id=>$tran_name, 
-                                             -version=>1,
-                                             -release=>1,
-                                             -dbname=>"Vega_transcript",
-                                            );
-      $dbentry->status('KNOWN');
-      $adx->store($dbentry,$trans->dbID,'Transcript') if $do_store;
-      my $sth = $db->prepare("update transcript set display_xref_id=" .      
-                               $dbentry->dbID .
-                             " where transcript_id=" . $trans->dbID);
-      $sth->execute;
-      print $sth->{Statement} . ";\n";
-
-      print "processing $tran_name\n";
-
-      my $translation = $trans->translation;
-      if (defined($translation)) {
-        my $dbentry=Bio::EnsEMBL::DBEntry->new(-primary_id=>$translation->stable_id,
-                                               -display_id=>$translation->stable_id, 
-                                               -version=>1,
-                                               -release=>1,
-                                               -dbname=>"Vega_translation",
-                                              );
+		#get (unique) name from gene attrib, or use stable ID
+        my $gene_name = &get_name($gene);
+		next GENE unless ($gene_name);
+		
+        $support->log("Gene $gene_name ($gid, $gsi)...\n");
+	
+		# filter to user-specified gene_type
+		my $gene_type = $support->param('gene_type');
+		if ($gene_type and ($gene_type ne $gene->type)){
+            $support->log("Skipping gene - not of type $gene_type.\n", 1);
+            next;
+        }
+		
+        # allow a restart of script at specified gene_stable_id
+        if($support->param('start_gid') && !$found){
+            if($gid == $support->param('start_gid')){
+                $found = 1;
+                $support->log("Found $gid - starting at next gene.\n", 1);
+            } else {
+                $support->log("Skipping gene - waiting for start gene_id.\n", 1);
+            }
+            next;
+        }
+		
+        # add the gene name as an xref to db
+        $gnum++;
+        my $dbentry = Bio::EnsEMBL::DBEntry->new(
+			-primary_id => $gene->stable_id,
+			-display_id => $gene_name, 
+			-version    => 1,
+			-release    => 1,
+			-dbname     => "Vega_gene",
+        );
         $dbentry->status('KNOWN');
-        $adx->store($dbentry,$trans->translation->dbID,'Translation') if $do_store;
-      }
+        $gene->add_DBEntry($dbentry);
+        if (! $support->param('dry_run')) {
+            my $dbID = $ea->store($dbentry, $gid, 'Gene');
+            $sth_gene->execute($dbID, $gid);
+            $support->log_verbose("Stored xref $dbID for gene $gsi ($gid).\n", 1);
+        }
+		
+        # loop over transcripts
+	TRANS:
+        foreach my $trans (@{$gene->get_all_Transcripts}){
+            my $tid = $trans->dbID;
+            my $tsi = $trans->stable_id;
+            my $trans_name = &get_name($trans);
+			next TRANS unless ($trans_name);
+
+            $support->log("Transcript $trans_name ($tid, $tsi)...\n", 1);
+			
+            # add transcript name as an xref to db
+            $tnum++;
+            my $dbentry = Bio::EnsEMBL::DBEntry->new(
+				-primary_id=>$trans->stable_id,
+				-display_id=>$trans_name, 
+				-version=>1,
+				-release=>1,
+				-dbname=>"Vega_transcript",
+			);
+            $dbentry->status('KNOWN');
+            if (! $support->param('dry_run')) {
+                my $dbID = $ea->store($dbentry, $tid, 'Transcript');
+                $sth_trans->execute($dbID, $tid);
+                $support->log_verbose("Stored xref $dbID for transcript $tsi ($tid).\n", 2);
+            }
+			
+            # translations
+            my $translation = $trans->translation;
+            if ($translation) {
+                # add translation name as xref to db
+                $tlnum++;
+                my $tlsi = $translation->stable_id;
+				my $tlid =  $translation->dbID;
+                my $dbentry = Bio::EnsEMBL::DBEntry->new(
+					-primary_id=>$tlid,
+					-display_id=>$tlid, 
+					-version=>1,
+					-release=>1,
+					-dbname=>"Vega_translation",
+				);
+                $dbentry->status('KNOWN');
+                if (! $support->param('dry_run')) {
+                    $ea->store($dbentry, $trans->translation->dbID, 'Translation');
+                    $support->log_verbose("Stored xref ".$dbentry->dbID." for translation $tlsi ($tlid).\n", 2);
+                }
+            }
+        }
     }
-  }
+	
+    $support->log("\nAdded xrefs for $gnum (of ".scalar @$genes.") genes, $tnum transcripts, $tlnum translations.\n");
+    $support->log_stamped("Done with chromosome $chr.\n\n");
 }
 
-sub get_chrlengths{
-  my $db = shift;
-  my $type = shift;
+# finish log
+$support->finish_log;
 
-  if (!$db->isa('Bio::EnsEMBL::DBSQL::DBAdaptor')) {
-    die "get_chrlengths should be passed a Bio::EnsEMBL::DBSQL::DBAdaptor\n";
-  }
 
-  my %chrhash;
-
-  my $q = qq( SELECT chrom.name,max(chr_end) FROM assembly as ass, chromosome as chrom
-              WHERE ass.type = '$type' and ass.chromosome_id = chrom.chromosome_id
-              GROUP BY chrom.name
-            );
-
-  my $sth = $db->prepare($q) || $db->throw("can't prepare: $q");
-  my $res = $sth->execute || $db->throw("can't execute: $q");
-
-  while( my ($chr, $length) = $sth->fetchrow_array) {
-    $chrhash{$chr} = $length;
-  }
-  return \%chrhash;
+sub get_name {
+	my $obj = shift;
+	my $sid = $obj->stable_id;
+	my $id  = $obj->dbID;
+	my $type = ($obj->isa("Bio::EnsEMBL::Gene"))       ? 'Gene'
+		     : ($obj->isa("Bio::EnsEMBL::Transcript")) ? 'Transcript'
+			 : undef;
+	if (! $type) {
+		$support->log_warning("Don't know how to deal with an object of type ",ref($obj),". Skipping\n");
+		return undef;
+	}
+	my $name;
+	my @names = map {$_->value} @{$obj->get_all_Attributes('name')};
+	if (! @names ) {
+		$support->log_warning("$type $id ($sid) has no name attrib, using stable ID.\n");
+		$name = $sid;
+	}
+	elsif (scalar (@names > 1)) {
+		my %unique_names;
+		map {$unique_names{$_}++} @names;
+		if (scalar(keys %unique_names) > 1 ) {
+			$support->log_warning("$type $id ($sid) has multiple different name attribs, using stable ID.\n");
+			$name = $sid;
+		}
+		else {
+			$name = $names[0];
+		}	
+	}
+	else {
+		$name =  $names[0];
+	}
+	return $name;
 }
-
-
-sub bychrnum {
-
-  my @awords = split /_/, $a;
-  my @bwords = split /_/, $b;
-
-  my $anum = $awords[0];
-  my $bnum = $bwords[0];
-
-  #  if ($anum !~ /^chr/ || $bnum !~ /^chr/) {
-  #    die "Chr name doesn't begin with chr for $a or $b";
-  #  }
-
-  $anum =~ s/chr//;
-  $bnum =~ s/chr//;
-
-  if ($anum !~ /^[0-9]*$/) {
-    if ($bnum !~ /^[0-9]*$/) {
-      return $anum cmp $bnum;
-    } else {
-      return 1;
-    }
-  }
-  if ($bnum !~ /^[0-9]*$/) {
-    return -1;
-  }
-
-  if ($anum <=> $bnum) {
-    return $anum <=> $bnum;
-  } else {
-    if ($#awords == 0) {
-      return -1;
-    } elsif ($#bwords == 0) {
-      return 1;
-    } else {
-      return $awords[1] cmp $bwords[1];
-    }
-  }
-}
-
+	
