b3179474bb59d182afae85b45c265417666dacae
jgrg <jgrg>
2007-10-16 15:28:34 +0000

    *** empty log message ***

---
+++ scripts/loutre/patch_gene_biotype_status.sql
@@ -0,0 +1,44 @@
+
+
+    UPDATE gene
+    SET biotype = 'protein_coding'
+      , status = 'KNOWN'
+    WHERE biotype = 'known'
+      AND status = 'KNOWN';
+
+    UPDATE gene
+    SET biotype = 'protein_coding'
+      , status = 'KNOWN'
+    WHERE biotype = 'known'
+      AND status = 'UNKNOWN';
+
+    UPDATE gene
+    SET biotype = 'processed_transcript'
+      , status = 'NOVEL'
+    WHERE biotype = 'novel_transcript'
+      AND status = 'UNKNOWN';
+
+    UPDATE gene
+    SET biotype = 'processed_transcript'
+      , status = 'KNOWN'
+    WHERE biotype = 'novel_transcript'
+      AND status = 'KNOWN';
+
+    UPDATE gene
+    SET biotype = 'processed_transcript'
+      , status = 'PUTATIVE'
+    WHERE biotype = 'putative'
+      AND status = 'UNKNOWN';
+
+    UPDATE gene
+    SET biotype = 'protein_coding'
+      , status = 'PREDICTED'
+    WHERE biotype = 'processed_transcript'
+      AND status = 'PREDICTED';
+
+    UPDATE gene
+    SET biotype = 'protein_coding'
+      , status = 'PREDICTED'
+    WHERE biotype = 'predicted_gene'
+      AND status = 'UNKNOWN';
+
