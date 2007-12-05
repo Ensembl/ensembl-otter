b98684b1ec1d1029d7907ee13e1e8e431492e737
st3 <st3>
2007-12-05 14:27:23 +0000

    as the names says - converts old vega biotype to new ones

---
+++ scripts/loutre/patch_old_vega_databases.sql
@@ -0,0 +1,60 @@
+
+update transcript
+   set biotype = lcase(biotype);
+
+update transcript 
+   set biotype = 'protein_coding' 
+ where biotype = 'coding';
+
+update transcript 
+   set biotype = 'processed_transcript' 
+ where biotype = 'transcript';
+
+UPDATE transcript 
+   SET biotype = 'processed_transcript',
+       status  = 'PUTATIVE'
+ WHERE biotype = 'putative'
+   AND status  = 'UNKNOWN';
+
+UPDATE transcript
+   SET biotype = 'transcribed_pseudogene'
+ WHERE biotype = 'expressed_pseudogene';
+
+UPDATE transcript
+   SET biotype = 'protein_coding',
+	   status  = 'KNOWN'
+ WHERE biotype = 'known_cds';
+
+UPDATE transcript
+   SET biotype = 'protein_coding',
+	   status  = 'PUTATIVE'
+ WHERE biotype = 'putative_cds';
+
+UPDATE transcript
+   SET biotype = 'protein_coding',
+	   status  = 'NOVEL'
+ WHERE biotype = 'novel_cds';
+
+UPDATE gene
+   SET biotype = lcase(biotype)
+ WHERE biotype like 'Ig%';
+
+update gene 
+   set status = 'NOVEL'
+ where status = 'UNKNOWN';
+
+
+delete at, sra
+from attrib_type at, seq_region_attrib sra
+where at.attrib_type_id = sra.attrib_type_id
+and at.code in
+('UnprocPsCount','ProcPsCount','IgPsSegCount','IgSegCount','NovelPTCount','NovelPCCount','KnownPTCount','KnownPCCount','PTCount')
+
+
+
+
+delete 
+select * 
+from attrib_type at, seq_region_attrib sra
+where at.attrib_type_id = sra.attrib_type_id
+and at.code in ('KnownPCCount','NovelPCCount','NovelPTCount','IgSegCount','IgPsSegCount, ProcPsCount','UnprocPsCount','KnownPTCount','TotAssemblyLeng');
