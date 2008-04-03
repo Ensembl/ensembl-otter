a25626112c3de277e5fafbc0bed7d8e27d1ed29e
st3 <st3>
2008-04-03 14:01:23 +0000

    patches for vega website

---
+++ scripts/loutre/patch_vega_biotype_status.sql
@@ -0,0 +1,73 @@
+
+
+    UPDATE gene
+    SET biotype = 'protein_coding'
+      , status = 'KNOWN'
+    WHERE biotype = 'known'
+      AND status = 'KNOWN';
+
+    UPDATE gene
+    SET biotype = 'protein_coding'
+      , status = 'KNOWN'
+    WHERE biotype = 'known'
+      AND status = 'UNKNOWN';
+
+    UPDATE gene
+    SET biotype = 'processed_transcript'
+      , status = 'NOVEL'
+    WHERE biotype = 'novel_transcript'
+      AND status = 'UNKNOWN';
+
+    UPDATE gene
+    SET biotype = 'processed_transcript'
+      , status = 'KNOWN'
+    WHERE biotype = 'novel_transcript'
+      AND status = 'KNOWN';
+
+    UPDATE gene
+    SET biotype = 'processed_transcript'
+      , status = 'PUTATIVE'
+    WHERE biotype = 'putative'
+      AND status = 'UNKNOWN';
+
+    UPDATE gene
+    SET biotype = 'processed_transcript'
+      , status = 'PUTATIVE'
+    WHERE biotype = 'putative'
+      AND status = 'KNOWN';
+
+    UPDATE gene
+    SET biotype = 'protein_coding'
+      , status = 'PREDICTED'
+    WHERE biotype = 'processed_transcript'
+      AND status = 'PREDICTED';
+
+    UPDATE gene
+    SET biotype = 'protein_coding'
+      , status = 'PREDICTED'
+    WHERE biotype = 'predicted_gene'
+      AND status = 'UNKNOWN';
+
+   UPDATE gene
+    SET biotype = 'IG_gene'
+    WHERE biotype = 'ig_gene';
+
+   UPDATE gene
+    SET biotype = 'IG_pseudogene'
+    WHERE biotype = 'ig_pseudogene';
+
+   UPDATE gene
+    SET biotype = 'transcribed_pseudogene'
+    WHERE biotype = 'expressed_pseudogene';
+
+   UPDATE transcript
+    SET biotype = 'IG_gene'
+    WHERE biotype = 'ig_gene';
+
+   UPDATE transcript
+    SET biotype = 'IG_pseudogene'
+    WHERE biotype = 'ig_pseudogene';
+
+   UPDATE transcript
+    SET biotype = 'transcribed_pseudogene'
+    WHERE biotype = 'expressed_pseudogene';
