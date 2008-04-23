8395b8d49dac9154b5112d29fcd9c790d959791f
jgrg <jgrg>
2008-04-23 17:50:06 +0000

    *** empty log message ***

--- modules/Bio/Otter/SpeciesDat.pm
+++ modules/Bio/Otter/SpeciesDat.pm
@@ -88,7 +88,7 @@ sub remove_restricted_datasets {
     my $sp = $self->{'_species_dat_hash'};
 
     foreach my $dataset_name (keys %$sp) {
-        next unless $sp->{$dataset_name}{'restricted'};
+        next unless $sp->{$dataset_name}{'RESTRICTED'};
         $self->log(sprintf("Dataset %s is %srestricted", $dataset_name, $allowed_hash->{$dataset_name} ? '' : 'not '));
         delete $sp->{$dataset_name} unless $allowed_hash->{$dataset_name};
     }
