1b1d950ce8651bd757aca005b2824ae6d8decf35
jgrg <jgrg>
2008-06-06 13:56:08 +0000

    *** empty log message ***

--- tk/EditWindow/LocusName.pm
+++ tk/EditWindow/LocusName.pm
@@ -80,14 +80,9 @@ sub do_rename {
     if ($old_name eq $new_name) {
         return;
     }
-    my $xc = $self->XaceSeqChooser;
-    my $xr = $xc->xace_remote;
-    unless ($xr) {
-        $xc->message('No xace attached');
-        return;
-    }
     warn "Renaming Locus '$old_name' to '$new_name'\n";
     
+    my $xc = $self->XaceSeqChooser;
 
     eval {
         my @xml;
@@ -126,9 +121,7 @@ sub do_rename {
         }
         $xc->send_zmap_commands(@xml);    
 
-        # Send the rename command to xace
-        $xr->load_ace($ace);
-        $xr->save;
+        $xc->save_ace($ace);
     };
     if ($@) {
         $xc->exception_message("Error renaming locus '$old_name' to '$new_name'; ". $@);
