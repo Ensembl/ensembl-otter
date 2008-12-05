2f4f150676b2fabf88573cbe9e6d73f45994542f
ml6 <ml6>
2008-12-05 17:21:56 +0000

    *** empty log message ***

--- modules/Bio/Otter/Lace/Pfam.pm
+++ modules/Bio/Otter/Lace/Pfam.pm
@@ -17,9 +17,13 @@ use LWP::UserAgent;
 use HTTP::Request;
 use XML::LibXML;
 use XML::LibXML::XPathContext;
+use Bio::Otter::Lace::TempFile;
 my $SEARCH_URL = 'http://pfam.sanger.ac.uk/search/sequence';
 my $HMM_URL    = 'http://pfam.sanger.ac.uk/family/hmm';
 my $SEED_URL   = 'http://pfam.sanger.ac.uk/family/alignment/download/format';
+
+# full path for hmmalign
+my $HMMALIGN = '/software/pfam/bin/hmmalign';
 
 sub new {
 	my ($self) = @_;
@@ -118,7 +122,6 @@ sub poll_results {
 # parses the results XML and return a hash containing the hits and locations
 sub parse_results {
 	my ( $self, $results_xml ) = @_;
-	my $log = Log::Log4perl->get_logger;
 	print STDOUT "parsing XML search results\n";
 	my $parser = XML::LibXML->new();
 	my $dom;
@@ -152,18 +155,203 @@ sub parse_results {
 		  . scalar @locations
 		  . ' location(s) for '
 		  . $match_node->getAttribute('accession') . "\n";
+		$results->{ $match_node->getAttribute('accession') }->{id} = $match_node->getAttribute('id');
+		$results->{ $match_node->getAttribute('accession') }->{class} = $match_node->getAttribute('class');
 		foreach my $location_node (@locations) {
 			my $location = {
 							 start => $location_node->getAttribute('start'),
 							 end   => $location_node->getAttribute('end')
 			};
-			push @{ $results->{ $match_node->getAttribute('accession') }
-				  ->{locations} }, $location;
+			push @{ $results->{ $match_node->getAttribute('accession') }->{locations} }, $location;
 		}
 	}
 	print "done parsing search results\n";
-	return { hits => $results };
+	return $results;
 }
+
+#-------------------------------------------------------------------------------
+# retrieves the HMM and seed alignment for each hit and drops them into the
+# results hash
+
+sub retrieve_pfam_hmm {
+  my ( $self, $domains ) = @_;
+
+  # create a user agent
+  my $ua = LWP::UserAgent->new;
+  $ua->env_proxy;
+
+  my %data;
+
+  foreach my $domain (@$domains) {
+
+    #----------------------------------------
+    # get the HMM
+
+    print STDOUT "retrieving HMM for $domain";
+
+    # set up the request
+    my $req = HTTP::Request->new( POST => $HMM_URL );
+    $req->content_type( 'application/x-www-form-urlencoded' );
+
+    # add the query parameters onto the URL
+    my $uri = URI->new;
+    $uri->query_form( mode => 'ls',
+                      entry => $domain );
+    $req->content( $uri->query );
+
+    # submit the request
+    my $res = $ua->request( $req );
+    print STDOUT "HMM request submitted";
+
+    print "HMM retrieval failed: ". $res->status_line
+      unless $res->is_success;
+
+    $data{$domain} = $res->content;
+
+    print STDOUT "retrieved HMM for $domain\n";
+
+
+
+  } # end of "foreach domain"
+
+  return \%data;
+
+}
+
+sub retrieve_pfam_seed {
+  my ( $self, $domains ) = @_;
+
+  # create a user agent
+  my $ua = LWP::UserAgent->new;
+  $ua->env_proxy;
+
+  my %data;
+
+  foreach my $domain (@$domains) {
+
+    #----------------------------------------
+    # get the HMM
+
+    print STDOUT "retrieving seed alignments for $domain";
+
+    # set up the request
+    my $req = HTTP::Request->new( POST => $SEED_URL );
+    $req->content_type( 'application/x-www-form-urlencoded' );
+
+    # add the query parameters onto the URL
+    my $uri = URI->new;
+    $uri->query_form( alnType  => 'seed',
+                      download => 1,
+                      entry    => $domain );
+    $req->content( $uri->query );
+
+    # submit the request
+    my $res = $ua->request( $req );
+    print STDOUT "seed request submitted";
+
+    print "seed alignment retrieval failed: ". $res->status_line
+      unless $res->is_success;
+
+    $data{$domain} = $res->content;
+    # (need to strip out secondary structure and consensus lines,
+    # otherwise hmmalign seg faults...)
+    $data{$domain} =~ s/^\#=G[CR].*?\n//mg;
+
+    print STDOUT "retrieved seed for $domain\n";
+
+
+
+  } # end of "foreach domain"
+
+  return \%data;
+
+}
+
+sub get_seq_snippets {
+	my ( $self, $seq_name, $seq_string, $hit_locations) = @_;
+
+	my $s;
+
+    foreach my $location ( @$hit_locations ) {
+
+	    	my $subseq = substr( $seq_string,
+                           $location->{start} - 1,
+                           $location->{end} - $location->{start} + 1 );
+
+			$s .= ">${seq_name}_$location->{start}-$location->{end}\n$subseq\n";
+    }
+
+	return $s;
+}
+
+
+#-------------------------------------------------------------------------------
+# builds a sequence file containing the sequence snippets that match Pfam
+# hits and aligns them to the seed alignment using hmmalign. Writes the
+# the resulting alignments to the working directory
+
+sub align_to_seed {
+  my ( $self, $seq, $domain, $hmm, $seed) = @_;
+
+  # the sequence that we'll be aligning
+  my $seq_file = $self->create_filename($domain,"seq");
+  open(my $seq_fh, ">$seq_file")
+            || die "Error creating '$seq_file' : $!";
+
+  print $seq_fh $seq; close $seq_fh;
+
+
+    # the seed alignment
+    my $seed_file = $self->create_filename($domain,"seed");
+    open(my $seed_fh, ">$seed_file")
+            || die "Error creating '$seed_file' : $!";
+
+    print $seed_fh $seed; close $seed_fh;
+
+    # the HMM
+    my $hmm_file = $self->create_filename($domain,"ls");
+    open(my $hmm_fh, ">$hmm_file")
+            || die "Error creating '$hmm_file' : $!";
+
+    print $hmm_fh $hmm; close $hmm_fh;
+
+    # the hmmalign output
+    my $output_filename = $self->create_filename($domain,"aln");
+
+
+    # build the hmmalign command
+    my $cmd = $HMMALIGN . ' --withali ' . $seed_file .
+                          ' -q '        . $hmm_file .
+                          ' '           . $seq_file .
+                          ' > '         . $output_filename;
+
+    print STDOUT "hmmalign command: |$cmd|";
+
+    system( $cmd ) == 0
+      or print "couldn't run hmmalign for $domain hits: $!";
+
+      return $output_filename;
+
+}
+
+sub create_filename{
+  my ($self, $stem, $ext, $dir) = @_;
+  if(!$dir){
+    $dir = '/var/tmp';
+  }
+  $stem = '' if(!$stem);
+  $ext = '' if(!$ext);
+  die $dir." doesn't exist Runnable:create_filename" unless(-d $dir);
+  my $num = int(rand(100000));
+  my $file = $dir."/".$stem.".".$$.".".$num.".".$ext;
+  while(-e $file){
+    $num = int(rand(100000));
+    $file = $dir."/".$stem.".".$$.".".$num.".".$ext;
+  }
+  return $file;
+}
+
+
 1;
 __END__
 
