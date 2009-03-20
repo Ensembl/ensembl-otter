a5ae1bcfe26d88aa0da95138140173c1e11a247d
st3 <st3>
2009-03-20 14:48:08 +0000

    whitespace

--- scripts/conversion/xref/add_havana_xrefs.pl
+++ scripts/conversion/xref/add_havana_xrefs.pl
@@ -60,9 +60,9 @@ use vars qw($SERVERROOT);
 use Storable;
 
 BEGIN {
-    $SERVERROOT = "$Bin/../../../..";
-    unshift(@INC, "$SERVERROOT/ensembl/modules");
-    unshift(@INC, "$SERVERROOT/bioperl-live");
+  $SERVERROOT = "$Bin/../../../..";
+  unshift(@INC, "$SERVERROOT/ensembl/modules");
+  unshift(@INC, "$SERVERROOT/bioperl-live");
 }
 
 use Getopt::Long;
@@ -78,20 +78,20 @@ our $support = new Bio::EnsEMBL::Utils::ConversionSupport($SERVERROOT);
 # parse options
 $support->parse_common_options(@_);
 $support->parse_extra_options(
-    'chromosomes|chr=s@',
-    'gene_stable_id|gsi=s@',
-    'prune',
+  'chromosomes|chr=s@',
+  'gene_stable_id|gsi=s@',
+  'prune',
 );
 $support->allowed_params(
-    $support->get_common_params,
-    'chromosomes',
-    'gene_stable_id',
-    'prune',
+  $support->get_common_params,
+  'chromosomes',
+  'gene_stable_id',
+  'prune',
 );
 
 if ($support->param('help') or $support->error) {
-    warn $support->error if $support->error;
-    pod2usage(1);
+  warn $support->error if $support->error;
+  pod2usage(1);
 }
 
 $support->comma_to_list('chromosomes');
@@ -115,26 +115,26 @@ my $ea = $dba->get_DBEntryAdaptor();
 # delete all existing Havana xrefs if --prune option is used
 
 if ($support->param('prune') and $support->user_proceed('Would you really like to delete all previously generated Havana xrefs before running this script?')) {
-	my $num;
-	# xrefs
-	$support->log("Deleting all Havana xrefs...\n");
-	$num = $dba->dbc->do(qq(
+  my $num;
+  # xrefs
+  $support->log("Deleting all Havana xrefs...\n");
+  $num = $dba->dbc->do(qq(
            DELETE x
            FROM xref x, external_db ed
            WHERE x.external_db_id = ed.external_db_id
            AND ed.db_name = 'Havana_gene';
         ));
-	$support->log("Done deleting $num entries.\n");
+  $support->log("Done deleting $num entries.\n");
 
-	# object_xrefs
-	$support->log("Deleting orphan object_xrefs...\n");
-	$num = $dba->dbc->do(qq(
+  # object_xrefs
+  $support->log("Deleting orphan object_xrefs...\n");
+  $num = $dba->dbc->do(qq(
            DELETE ox
            FROM object_xref ox
            LEFT JOIN xref x ON ox.xref_id = x.xref_id
            WHERE x.xref_id IS NULL
         ));
-	$support->log("Done deleting $num entries.\n");
+  $support->log("Done deleting $num entries.\n");
 }
 
 my @gene_stable_ids = $support->param('gene_stable_id');
@@ -147,107 +147,107 @@ my ($total_found,$total_not_found,$total_external) = (0,0,0);
 # loop over chromosomes
 $support->log("Looping over chromosomes: @chr_sorted\n\n");
 foreach my $chr (@chr_sorted) {
-    $support->log_stamped("> Chromosome $chr (".$chr_length->{$chr}."bp).\n\n");
+  $support->log_stamped("> Chromosome $chr (".$chr_length->{$chr}."bp).\n\n");
 
-    # fetch genes from db
-    $support->log("Fetching genes...\n");
-    my $slice = $sa->fetch_by_region('chromosome', $chr);
-    my $genes = $slice->get_all_Genes();
-    $support->log_stamped("Done fetching ".scalar @$genes." genes.\n\n");
+  # fetch genes from db
+  $support->log("Fetching genes...\n");
+  my $slice = $sa->fetch_by_region('chromosome', $chr);
+  my $genes = $slice->get_all_Genes();
+  $support->log_stamped("Done fetching ".scalar @$genes." genes.\n\n");
 
-	my $havana_genes;
-	my ($havana_c,$external_c,$found,$not_found) = (0,0,0,0);
-    $support->log("Analysing Havana genes...\n");
-	foreach my $gene (@{$genes}) {
-		my $analysis_name = $gene->analysis->logic_name;
-		next unless ($analysis_name eq 'otter');
-		$havana_c++;
-		my $gsi = $gene->stable_id;
-        my $gene_name = $gene->display_xref->display_id;
-		# filter to user-specified gene_stable_ids
-        if (scalar(@gene_stable_ids)){
-            next unless $gene_stable_ids{$gsi};
-        }
-		#add display_xref name
-		if ($havana_genes->{$gene_name} ) {
-			$support->log_warning("Name $gene_name attached to genes $gsi and ".$havana_genes->{$gene_name}."\n");
-		}
-		else {
-			$havana_genes->{$gene_name} = $gsi;
-		}
+  my $havana_genes;
+  my ($havana_c,$external_c,$found,$not_found) = (0,0,0,0);
+  $support->log("Analysing Havana genes...\n");
+  foreach my $gene (@{$genes}) {
+    my $analysis_name = $gene->analysis->logic_name;
+    next unless ($analysis_name eq 'otter');
+    $havana_c++;
+    my $gsi = $gene->stable_id;
+    my $gene_name = $gene->display_xref->display_id;
+    # filter to user-specified gene_stable_ids
+    if (scalar(@gene_stable_ids)){
+      next unless $gene_stable_ids{$gsi};
+    }
+    #add display_xref name
+    if ($havana_genes->{$gene_name} ) {
+      $support->log_warning("Name $gene_name attached to genes $gsi and ".$havana_genes->{$gene_name}."\n");
+    }
+    else {
+      $havana_genes->{$gene_name} = $gsi;
+    }
+  }
+
+  # loop over genes again and store xrefs to Havana ones for external genes
+  $support->log("\nAnalysing External genes...\n");
+  foreach my $gene (@$genes) {
+    my $analysis_name = $gene->analysis->logic_name;
+    next if ($analysis_name eq 'otter');
+    $external_c++;
+
+    my $gene_name = $gene->display_xref->display_id;
+    my $gid = $gene->dbID;
+    my $gsi = $gene->stable_id;
+    # filter to user-specified gene_stable_ids
+    if (scalar(@gene_stable_ids)){
+      next unless $gene_stable_ids{$gsi};
+    }
+    my ($stripped_name,$prefix);	
+    # see if the gene_name has a prefix
+    ($prefix,$stripped_name) = $gene_name  =~ /(.*?):(.*)/;
+    unless ($stripped_name) {
+      $stripped_name = $prefix;
+      $prefix = 0;
+    }
+
+    $support->log_verbose("Checking gene $gene_name ($gid, $gsi) using $stripped_name...\n",1);
+    #store xref if there is one
+    if (my $pid = $havana_genes->{$stripped_name} ) {
+      $support->log("Gene $gene_name ($gsi) matches Havana gene $stripped_name ($pid).\n",1);
+      $found++;
+      #check if it's there already...
+      my ($existing_xref,$dbID);		
+      if ($existing_xref = $ea->fetch_by_db_accession('Havana_gene',$pid)) {
+	$support->log_verbose("Using previous xref for gene $gsi (Havana_gene display_id $pid).\n", 1);
+	$gene->add_DBEntry($existing_xref);
+	$dbID = $ea->store($existing_xref, $gid, 'gene') unless $support->param('dry_run');
+      } else {
+	my $dbentry = Bio::EnsEMBL::DBEntry->new(
+	  -primary_id => $pid,
+	  -display_id => $pid,
+	  -version    => 1,
+	  -release    => 1,
+	  -dbname     => 'Havana_gene',
+	  -status     => 'XREF',
+	);
+	$gene->add_DBEntry($dbentry);
+	if ($support->param('dry_run')) {
+	  $support->log_verbose("Would store xref ($pid) for gene $gene_name ($gsi, $gid)\n", 1);
 	}
-
-	# loop over genes again and store xrefs to Havana ones for external genes
-	$support->log("\nAnalysing External genes...\n");
-	foreach my $gene (@$genes) {
-		my $analysis_name = $gene->analysis->logic_name;
-		next if ($analysis_name eq 'otter');
-		$external_c++;
-
-		my $gene_name = $gene->display_xref->display_id;
-        my $gid = $gene->dbID;
- 		my $gsi = $gene->stable_id;
-		# filter to user-specified gene_stable_ids
-        if (scalar(@gene_stable_ids)){
-            next unless $gene_stable_ids{$gsi};
-        }
-		my ($stripped_name,$prefix);	
-        # see if the gene_name has a prefix
-        ($prefix,$stripped_name) = $gene_name  =~ /(.*?):(.*)/;
-		unless ($stripped_name) {
-			$stripped_name = $prefix;
-			$prefix = 0;
-		}
-
-        $support->log_verbose("Checking gene $gene_name ($gid, $gsi) using $stripped_name...\n",1);
-		#store xref if there is one
-		if (my $pid = $havana_genes->{$stripped_name} ) {
-			$support->log("Gene $gene_name ($gsi) matches Havana gene $stripped_name ($pid).\n",1);
-			$found++;
-			#check if it's there already...
-			my ($existing_xref,$dbID);		
-			if ($existing_xref = $ea->fetch_by_db_accession('Havana_gene',$pid)) {
-				$support->log_verbose("Using previous xref for gene $gsi (Havana_gene display_id $pid).\n", 1);
-				$gene->add_DBEntry($existing_xref);
-				$dbID = $ea->store($existing_xref, $gid, 'gene') unless $support->param('dry_run');
-			} else {
-				my $dbentry = Bio::EnsEMBL::DBEntry->new(
-														 -primary_id => $pid,
-														 -display_id => $pid,
-														 -version    => 1,
-														 -release    => 1,
-														 -dbname     => 'Havana_gene',
-														 -status     => 'XREF',
-														);
-				$gene->add_DBEntry($dbentry);
-				if ($support->param('dry_run')) {
-					$support->log_verbose("Would store xref ($pid) for gene $gene_name ($gsi, $gid)\n", 1);
-				}
-				else {
-					$dbID = $ea->store($dbentry, $gid, 'gene') unless $support->param('dry_run');
-					if ($dbID) {
-						$support->log_verbose("Stored xref: display_id $pid (dbID $dbID) for gene $gene_name ($gsi, $gid)\n", 1);
-					}
-					else {
-						$support->log_warning("Problem storing xref ($pid) for gene $gene_name ($gsi, $gid)\n");
-					}
-				}
-			}
-		}
-		else {
-			$support->log_verbose("No match found for gene $gene_name ($gsi, $gid)\n", 1); 
-			$not_found++;
-		}
+	else {
+	  $dbID = $ea->store($dbentry, $gid, 'gene') unless $support->param('dry_run');
+	  if ($dbID) {
+	    $support->log_verbose("Stored xref: display_id $pid (dbID $dbID) for gene $gene_name ($gsi, $gid)\n", 1);
+	  }
+	  else {
+	    $support->log_warning("Problem storing xref ($pid) for gene $gene_name ($gsi, $gid)\n");
+	  }
 	}
+      }
+    }
+    else {
+      $support->log_verbose("No match found for gene $gene_name ($gsi, $gid)\n", 1); 
+      $not_found++;
+    }
+  }
 
-	# log stats
-	$support->log("\nFound $havana_c havana genes and $external_c external genes (of ".scalar @$genes." on this chromosome).\n");
-	$support->log("External genes with a match = $found, without = $not_found\n");
-    $support->log_stamped("Done with chromosome $chr.\n\n");
+  # log stats
+  $support->log("\nFound $havana_c havana genes and $external_c external genes (of ".scalar @$genes." on this chromosome).\n");
+  $support->log("External genes with a match = $found, without = $not_found\n");
+  $support->log_stamped("Done with chromosome $chr.\n\n");
 
-	$total_found += $found;
-	$total_not_found += $not_found;
-	$total_external += $external_c
+  $total_found += $found;
+  $total_not_found += $not_found;
+  $total_external += $external_c
 }
 
 #create a summary
