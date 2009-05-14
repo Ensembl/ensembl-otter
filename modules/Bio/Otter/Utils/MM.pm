c04492baf552a87c802ad4a2e5ab6ede42de2e82
ml6 <ml6>
2009-05-14 15:28:51 +0000

    add code to deal with the EMBL non-mRNA/EST entries.

--- modules/Bio/Otter/Utils/MM.pm
+++ modules/Bio/Otter/Utils/MM.pm
@@ -12,8 +12,8 @@ use Data::Dumper;
 use CGI::Carp qw(fatalsToBrowser);
 
 # NB: databases will be searched in the order in which they appear in this list
-my @DB_CATEGORIES = (	'emblrelease', 
-						'uniprot', 
+my @DB_CATEGORIES = (	'emblrelease',
+						'uniprot',
 						'emblnew',
 						'uniprot_archive',
 						#'refseq'
@@ -29,16 +29,16 @@ my $magic_evi_name_matcher = qr{
         [A-Z]\d[A-Z\d]{4} # a capital letter, a digit, then 4 letters or digits.
     )
     (\-\d+)?              # Optional VARSPLICE suffix
-    (\.\d+)?              # Optional .SV            
+    (\.\d+)?              # Optional .SV
 }x;
 
 sub new {
     my ( $class, @args ) = @_;
-    
+
     my $self = bless {}, $class;
 
 	my ( $host , $port , $user, $name ) = @args;
-	
+
     $self->host($host || 'cbi3');
     $self->port($port || 3306);
     $self->user($user || 'genero');
@@ -49,9 +49,9 @@ sub new {
 
 sub _get_connection {
 	my ( $self, $category ) = @_;
-	
+
 	my $dbh;
-	
+
 	if ($category eq 'uniprot_archive') {
 		my $dsn = "DBI:mysql:host=".$self->host.":uniprot_archive";
 		$dbh = DBI->connect( $dsn, $self->user, '',{ 'RaiseError' => 1 } );
@@ -79,80 +79,80 @@ sub _get_connection {
 
 sub dbh {
 	my ( $self, $category, $dbh ) = @_;
-	
+
 	die "DB category required" unless $category;
-	
-	die "Invalid DB category: $category" unless 
+
+	die "Invalid DB category: $category" unless
 		grep {/^$category$/} @DB_CATEGORIES;
-	
+
 	if ($dbh) {
 		eval { $dbh->isa('DBI::db') };
 		die "Not a valid DB handle: ".(ref $dbh) if $@;
 		$self->{_dbhs}->{$category} = $dbh;
 	}
-	
+
 	unless ($self->{_dbhs}->{$category}) {
         $self->{_dbhs}->{$category} = $self->_get_connection($category);
     }
-    
+
     return $self->{_dbhs}->{$category};
 }
 
 sub get_accession_types {
 	my $self = shift;
-	
+
 	my $accs = shift;
-	
-	my $sql = '	SELECT molecule_type, data_class, accession_version 
+
+	my $sql = '	SELECT molecule_type, data_class, accession_version
 				FROM entry e, accession a
 				WHERE e.entry_id = a.entry_id
 				AND a.accession = ? ';
-				
+
 	my $uniprot_archive_sql = ' SELECT molecule_type, data_class, accession_version
         						FROM entry
-        						WHERE accession_version LIKE ?';	
-        						
+        						WHERE accession_version LIKE ?';
+
 	my %acc_hash = ();
-	
+
 	for my $text (@$accs) {
        	if ($text =~ /$magic_evi_name_matcher/g) {
        		my $prefix = $1 || '*';
         	my $acc    = $2;
             $acc      .= $3 if $3;
        		my $sv     = $4 || '*';
-       		
+
        		$acc_hash{$text} = [$acc, $sv];
 	   	}
 	}
-	
+
 	my %res = map {$_ => []} @$accs;
-	
+
 	return \%res unless %acc_hash;
 
 	for my $db (@DB_CATEGORIES) {
-	    
+
         my $archive = $db eq 'uniprot_archive';
-        
+
        	my $query = $archive ? $uniprot_archive_sql : $sql;
-        
+
 		my $sth = $self->dbh($db)->prepare($query);
-			
+
 		for my $key (keys %acc_hash) {
-				
+
 			my ($acc, $sv) = @{ $acc_hash{$key} };
-			
+
 			$acc .= '%' if $archive; # uniprot_archive accessions have versions appended
-			
+
 			$sth->execute($acc) or die "Couldn't execute statement: " . $sth->errstr;
-			
+
 			if (my ($type, $class, $version) = $sth->fetchrow_array()) {
-			
+
 				my ($db_sv) = $version =~ /.+(\.\d+)/;
-				
-				next unless ($sv eq '*') || ($sv eq $db_sv); 
-				
+
+				next unless ($sv eq '*') || ($sv eq $db_sv);
+
 				# found an entry, so establish if the type is one we're expecting
-						
+
 				if ($class eq 'EST') {
 					$res{$key} = ['EST', "Em:$version"];
 				}
@@ -160,9 +160,9 @@ sub get_accession_types {
 					$res{$key} = ['mRNA', "Em:$version"];
 				}
 				elsif ($type eq 'protein') {
-					
+
 					my $prefix;
-					
+
 					if ($class eq 'STD') {
 						$prefix = 'Sw';
 					}
@@ -175,18 +175,21 @@ sub get_accession_types {
 					else {
 						die "Unexpected data class for uniprot entry: $class";
 					}
-					
+
 					$res{$key} = ['Protein', "$prefix:$version"];
+				} elsif($type && $class) {
+					# deal here with the remaining EMBL molecule type (tRNA, rRNA, other DNA, etc...)
+					$res{$key} = ['Other', "Em:$version"];
 				}
-				
+
 				delete $acc_hash{$key};
 			}
 		}
-		
+
 		last unless %acc_hash;
 	}
-	
-	return \%res;	
+
+	return \%res;
 }
 
 sub name {
