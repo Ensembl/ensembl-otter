36a54dcea94e0ecab680f2453338317ef8cf02b3
gr5 <gr5>
2009-07-15 10:06:09 +0000

    moved the gff constructing code largely into the slice to_gff method

--- modules/Bio/Vega/Utils/EnsEMBL2GFF.pm
+++ modules/Bio/Vega/Utils/EnsEMBL2GFF.pm
@@ -6,42 +6,113 @@ use strict;
 use warnings;
 
 # This module allows conversion of ensembl/otter objects to GFF by inserting
-# _gff_hash and to_gff methods into the necessary feature classes
+# to_gff (and supporting _gff_hash) methods into the necessary feature classes
 
-sub gff_header {
+{
+	package Bio::EnsEMBL::Slice;
 	
-	# create a generic GFF header in the format expected by ZMap
-	
-	my %args = @_;
-	
-	my $slice = $args{slice} || die "Must provide a slice to gff_header";
-	
-	my $include_dna = $args{include_dna};
-	
-	# build up a date string in the format specified by the GFF spec
-	
-	my ( $sec, $min, $hr, $mday, $mon, $year ) = localtime;
-	$year += 1900;    # correct the year
-	$mon++;           # months start at 0
-	my $date = "$year-$mon-$mday";
-
-	my $hdr = 
-		  "##gff-version 2\n"
-		. "##source-version EnsEMBL2GFF 1.0\n"
-		. "##date $date\n"
-		. "##sequence-region "
-		. $slice->seq_region_name . " "
-		. $slice->start . " "
-		. $slice->end . "\n";
+	sub gff_header {
 		
-	if ($include_dna) {	
-		$hdr .=
-			  "##DNA\n"
-			. "##".$slice->seq."\n"
-			. "##end-DNA\n";
+		my $self = shift;
+		my %args = @_;
+		my $include_dna = $args{include_dna};
+		
+		# build up a date string in the format specified by the GFF spec
+	
+		my ( $sec, $min, $hr, $mday, $mon, $year ) = localtime;
+		$year += 1900;    # correct the year
+		$mon++;           # correct the month
+		my $date = "$year-$mon-$mday";
+
+		my $hdr = 
+		  	  "##gff-version 2\n"
+			. "##source-version EnsEMBL2GFF 1.0\n"
+			. "##date $date\n"
+			. "##sequence-region "
+			. $self->seq_region_name . " "
+			. $self->start . " "
+			. $self->end . "\n";
+		
+		if ($include_dna) {	
+			$hdr .=
+				  "##DNA\n"
+				. "##".$self->seq."\n"
+				. "##end-DNA\n";
+		}
+		
+		return $hdr;
 	}
+	
+	sub to_gff {
 		
-	return $hdr;
+		my $self = shift;
+		
+		my %args = @_;
+		
+		my $include_header 	= $args{include_header};
+		my $include_dna 	= $args{include_dna};
+		my $feature_types	= $args{feature_types};
+		my $analyses 		= $args{analyses};
+		my $verbose			= $args{verbose};
+		
+		my $sources_to_types =	$args{sources_to_types};
+		
+		my $gff = $include_header ? $self->gff_header(include_dna => $include_dna) : '';
+		
+		# grab features of each type we're interested in
+			
+		for my $feature_type (@$feature_types) {
+			
+			my $method = 'get_all_'.$feature_type;
+			
+			unless ($self->can($method)) {
+				warn "There is no method to retrieve $feature_type from a slice";
+				next; 
+			}
+			
+			for my $analysis (@$analyses) {
+				
+				my $features = $self->$method($analysis);
+				
+				if ($verbose && scalar(@$features)) {
+					print "Found ".scalar(@$features)." $feature_type from $analysis on slice ".$self->seq_region_name."\n";
+				}
+				
+				for my $feature (@$features) {
+					
+					if ( $feature->can('to_gff') ) {
+						
+						$gff .= $feature->to_gff . "\n";
+
+						if ($sources_to_types) {
+							
+							# If we are passed a sources_to_types hashref, then as we identify the types of 
+							# features each analysis can create, fill in the hash for the caller. This is
+							# required by enzembl, but other users can just ignore this parameter.
+							
+							my $source = $feature->_gff_hash->{source};
+						
+							if ($sources_to_types->{$source}) {
+								unless ($sources_to_types->{$source} eq $feature_type) {
+									die "Can't have multiple gff sources from one analysis:\n".
+										"('$analysis' seems to have both '".$sources_to_types->{$source}.
+										"' and '$feature_type')\n";
+								}
+							}
+							else { 
+								$sources_to_types->{$source} = $feature_type;
+							}
+						}
+					}
+					else {
+						warn "no to_gff method in $feature_type";
+					}
+				}
+			}
+		}
+		
+		return $gff;	
+	}
 }
 
 {
@@ -169,12 +240,12 @@ sub gff_header {
 				$gff .= "\n".join("\t",
 					$tsct->_gff_hash->{seqname},
 					$tsct->_gff_hash->{source},
-					'CDS',
+					'CDS', # feature
 					$tsct->coding_region_start,
 					$tsct->coding_region_end,
 					'.', # score
 					$tsct->_gff_hash->{strand},
-					'0', # strand - not really sure what we should put here, but giface always seems to use 0, so we will too!
+					'0', # frame - not really sure what we should put here, but giface always seems to use 0, so we will too!
 					'Sequence '.$tsct->_gff_hash->{attributes}->{Sequence}
 				);
 			}
@@ -185,8 +256,12 @@ sub gff_header {
 				# exons and introns don't have analyses attached, so give them the transcript's one
 				$feat->analysis($tsct->analysis);
 				
-				# and add the features gff line to our string, appending the sequence information
+				# and add the feature's gff line to our string, appending the sequence information
 				$gff .= "\n".$feat->to_gff . "\tSequence ".'"'.$tsct->display_id.'"';
+				
+				# to be on the safe side, get rid of the analysis we temporarily attached 
+				# (someone might rely on there not being one later)
+				$feat->analysis(undef);
 			}
 		}
 	
