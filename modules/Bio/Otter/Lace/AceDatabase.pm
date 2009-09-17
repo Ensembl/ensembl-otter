738168248aab6ab8ff2fff0bc306fb7f0398e40f
gr5 <gr5>
2009-09-17 16:06:08 +0000

    the LoadColumns window now displays a progress bar giving feedback about which filters have loaded and failed etc.

--- modules/Bio/Otter/Lace/AceDatabase.pm
+++ modules/Bio/Otter/Lace/AceDatabase.pm
@@ -555,7 +555,7 @@ sub write_dna_data {
 }
 
 sub topup_pipeline_data_into_ace_server {
-    my( $self ) = @_;
+    my( $self, $pipeline_progress_window ) = @_;
 
     my $factory = $self->pipeline_DataFactory
         or confess "No pipeline_DataFactory";
@@ -564,8 +564,12 @@ sub topup_pipeline_data_into_ace_server {
     my $ace_server = $self->ace_server();
 
     $factory->ace_string_callback( sub{ $ace_server->save_ace(@_); } );
+    
+    $factory->pipeline_progress_window($pipeline_progress_window);
+    
     my $filters_fetched_data = $factory->topup_pipeline();
     $factory->ace_string_callback( undef );
+    $factory->pipeline_progress_window( undef );
 
     return $filters_fetched_data;
 }
