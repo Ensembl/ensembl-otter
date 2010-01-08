7e10cfcbff59c0b6da2d2a0e445c3890ff3bcca1
ml6 <ml6>
2010-01-08 11:01:37 +0000

    *** empty log message ***

--- scripts/loutre/patch_transcript_biotype_status.sql
+++ scripts/loutre/patch_transcript_biotype_status.sql
@@ -5,3 +5,8 @@
       , status = 'UNKNOWN'
     WHERE biotype = 'processed_transcript'
       AND status = 'NOVEL';
+
+
+UPDATE transcript
+SET biotype = 'processed_transcript', status = 'UNKNOWN'
+WHERE biotype = 'novel_transcript';
