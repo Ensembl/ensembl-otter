4f16fb3f2e1b3dfb3ba5c891a9634c7878ed13fa
gr5 <gr5>
2010-02-26 15:25:55 +0000

    *** empty log message ***

--- modules/Bio/Vega/PredictionTranscript.pm
+++ modules/Bio/Vega/PredictionTranscript.pm
@@ -3,7 +3,25 @@ package Bio::Vega::PredictionTranscript;
 use strict;
 use warnings;
 
+use Bio::EnsEMBL::Utils::Argument qw( rearrange );
+
 use base 'Bio::EnsEMBL::PredictionTranscript';
+
+sub new {
+    my $class = shift;
+
+    my $self = $class->SUPER::new(@_);
+
+    my ($truncated_5_prime, $truncated_3_prime) = rearrange([
+        'TRUNCATED_5_PRIME',
+        'TRUNCATED_3_PRIME'
+    ], @_);
+    
+    $self->{truncated_5_prime} = $truncated_5_prime;
+    $self->{truncated_3_prime} = $truncated_3_prime;
+
+    return $self;
+}
 
 sub truncated_5_prime {
     my $self = shift;
