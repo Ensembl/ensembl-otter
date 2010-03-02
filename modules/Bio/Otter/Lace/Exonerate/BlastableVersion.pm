8dfd85282b7f7af39f075cb4d689fa856fd50e02
jh13 <jh13>
2010-03-02 17:41:36 +0000

    removed unused lexical variables

--- modules/Bio/Otter/Lace/Exonerate/BlastableVersion.pm
+++ modules/Bio/Otter/Lace/Exonerate/BlastableVersion.pm
@@ -9,19 +9,10 @@ use warnings;
 #### Stop anyone else loading their own BlastableVersion
 $INC{q(BlastableVersion.pm)}++;
 
-#### Configurable variables
-
-my $cache_file = '/var/tmp/blast_versions';
-my $tracking_db = 'mysql:blastdb;cbi2.internal.sanger.ac.uk';
-my $tracking_user = 'blastdbro';
-my $tracking_pass = '';
-
-#### No more configurable variables
-
 use vars qw(%versions $debug $revision);
 
 $debug = 0;
-$revision='$Revision: 1.3 $ ';
+$revision='$Revision: 1.4 $ ';
 $revision =~ s/\$.evision: (\S+).*/$1/;
 
 #### CONSTRUCTORS
