5dea7734269a2fa9806199267173398110c1dd0f
jh13 <jh13>
2010-03-02 10:47:54 +0000

    moved the BlastableVersion package from Exonerate.pm to its own module

---
+++ modules/Bio/Otter/Lace/Exonerate/BlastableVersion.pm
@@ -0,0 +1,47 @@
+
+package BlastableVersion;
+
+use strict;
+use warnings;
+
+#### Stop anyone else loading their own BlastableVersion
+$INC{q(BlastableVersion.pm)}++;
+
+#### Configurable variables
+
+my $cache_file = '/var/tmp/blast_versions';
+my $tracking_db = 'mysql:blastdb;cbi2.internal.sanger.ac.uk';
+my $tracking_user = 'blastdbro';
+my $tracking_pass = '';
+
+#### No more configurable variables
+
+use vars qw(%versions $debug $revision);
+
+$debug = 0;
+$revision='$Revision: 1.1 $ ';
+$revision =~ s/\$.evision: (\S+).*/$1/;
+
+#### CONSTRUCTORS
+
+sub new {
+    my $proto = shift;
+    my $self = { };
+    bless ($self, ref($proto) || $proto);
+    return $self;
+}
+
+#### ACCESSOR METHODS
+
+sub date { localtime; }
+sub name { $0; }
+sub version { 1.0.0; }
+sub sanger_version { 1.0.0; }
+
+#### PUBLIC METHODS
+
+sub force_dbi { }
+sub set_hostname { }
+sub get_version { }
+
+1;
