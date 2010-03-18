3409ea7e412f32d82b8fd29f21864a3ddcb7f8a0
jgrg <jgrg>
2010-03-18 11:03:16 +0000

    *** empty log message ***

--- tk/CanvasWindow.pm
+++ tk/CanvasWindow.pm
@@ -262,6 +262,9 @@ sub bind_scroll_commands {
     $canvas->configure(
         -takefocus => 1,
         );
+    $canvas->Tk::bind('<Enter>', sub{
+        $canvas->Tk::focus;
+        });
     
     # Home and End keys
     $canvas->Tk::bind('<Home>', sub{
