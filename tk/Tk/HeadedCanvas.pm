d3780a6531adae631fc5c0c0519544b7a311e419
jgrg <jgrg>
2010-03-18 14:01:16 +0000

    improved dullen_colors script and renamded fade_colors

--- tk/Tk/HeadedCanvas.pm
+++ tk/Tk/HeadedCanvas.pm
@@ -7,199 +7,201 @@ package Tk::HeadedCanvas;
 use strict;
 use Tk;
 
-use base ('Evi::DestroyReporter','Tk::Frame');
+use base ('Evi::DestroyReporter', 'Tk::Frame');
 
 Construct Tk::Widget 'HeadedCanvas';
 
-use Tk::Submethods ( # propagate xviewMoveto(@_) --> xview('moveto',@_); etc
-			'xview'  => [qw(moveto scroll)],
-			'yview'  => [qw(moveto scroll)],
+use Tk::Submethods (    # propagate xviewMoveto(@_) --> xview('moveto',@_); etc
+    'xview' => [qw(moveto scroll)],
+    'yview' => [qw(moveto scroll)],
 );
 
 sub Populate {
-	my ($self,$args) = @_;
+    my ($self, $args) = @_;
 
-	$self->SUPER::Populate($args);
-	
-		# creation and packing of auxiliary frames:
-	my $botframe = $self->Frame()->pack(-side=>'bottom',-fill=>'x');
-	my $lframe = $self->Frame()->pack(-side=>'left',-fill=>'y');
-	my $cframe = $self->Frame()->pack(-side=>'left',-fill=>'both',-expand=>1);
+    $self->SUPER::Populate($args);
 
-		# creation and packing of subwidgets:
-	my $sh = $botframe->Scrollbar(-orient=>'horizontal');
-	my $sqsize = $sh->reqheight();
-	my $minisq = $botframe->Frame(-width=>$sqsize,-height=>$sqsize)
-			->pack(-in=>$botframe,-side=>'right');
-	$sh->pack(-side=>'left',-fill=>'x',-expand=>1);
+    # creation and packing of auxiliary frames:
+    my $botframe = $self->Frame()->pack(-side => 'bottom', -fill => 'x');
+    my $lframe   = $self->Frame()->pack(-side => 'left',   -fill => 'y');
+    my $cframe   = $self->Frame()->pack(-side => 'left',   -fill => 'both', -expand => 1);
 
-	my $sv = $self->Scrollbar(-orient=>'vertical')
-		 ->pack(-side=>'right',-fill=>'y');
+    # creation and packing of subwidgets:
+    my $sh     = $botframe->Scrollbar(-orient => 'horizontal');
+    my $sqsize = $sh->reqheight();
+    my $minisq = $botframe->Frame(-width => $sqsize, -height => $sqsize)->pack(-in => $botframe, -side => 'right');
+    $sh->pack(-side => 'left', -fill => 'x', -expand => 1);
 
-	my $topleft_canvas = $lframe->Canvas(-width =>0,-height=>0)->pack(-side=>'top');
-	my $left_canvas  = $lframe->Canvas(-width =>0)->pack(-side=>'left',-fill=>'y',-expand=>1);
-	my $top_canvas  = $cframe->Canvas(-height=>0)->pack(-side=>'top',-fill=>'x');
-	my $main_canvas = $cframe->Canvas(
-	)->pack(-side=>'bottom',-fill=>'both',-expand=>1);
+    my $sv = $self->Scrollbar(-orient => 'vertical')->pack(-side => 'right', -fill => 'y');
 
-		# scrolls' binding:
-	$sh->configure( -command => [$self => 'xview']);
-	$top_canvas->configure(-xscrollcommand => [$sh => 'set']);
-	$main_canvas->configure(-xscrollcommand => [$sh => 'set']);
-	 
-	$sv->configure( -command => [$self => 'yview']);
-	$left_canvas->configure(-yscrollcommand => [$sv => 'set']);
-	$main_canvas->configure(-yscrollcommand => [$sv => 'set']);
+    my $topleft_canvas = $lframe->Canvas(-width => 0, -height => 0)->pack(-side => 'top');
+    my $left_canvas = $lframe->Canvas(-width => 0)->pack(-side => 'left', -fill => 'y', -expand => 1);
+    my $top_canvas = $cframe->Canvas(-height => 0)->pack(-side => 'top', -fill => 'x');
+    my $main_canvas = $cframe->Canvas()->pack(-side => 'bottom', -fill => 'both', -expand => 1);
 
-		# advertisement:
-	$self->Advertise('main_canvas'     => $main_canvas);
-	$self->Advertise('left_canvas'     => $left_canvas);
-	$self->Advertise('top_canvas'      => $top_canvas);
-	$self->Advertise('topleft_canvas'  => $topleft_canvas);
-	$self->Advertise('xscrollbar'      => $sh);
-	$self->Advertise('yscrollbar'      => $sv);
+    # scrolls' binding:
+    $sh->configure(-command => [ $self => 'xview' ]);
+    $top_canvas->configure(-xscrollcommand => [ $sh => 'set' ]);
+    $main_canvas->configure(-xscrollcommand => [ $sh => 'set' ]);
 
-		# delegate configuration to the main canvas:
-	$self->ConfigSpecs(
-		-background => [['DESCENDANTS','SELF'],'background','Background','white'],
-		-foreground => [['DESCENDANTS','SELF'],'foreground','Foreground','black'],
-		-scrollregion => ['METHOD','scrollregion','Scrollregion',[0,0,0,0]],
-		'DEFAULT' => [ 'main_canvas' ],
-	);
+    $sv->configure(-command => [ $self => 'yview' ]);
+    $left_canvas->configure(-yscrollcommand => [ $sv => 'set' ]);
+    $main_canvas->configure(-yscrollcommand => [ $sv => 'set' ]);
 
-		# delegate methods to the main canvas:
-	$self->Delegates(
-		'DEFAULT' => 'main_canvas',
-	);
+    # advertisement:
+    $self->Advertise('main_canvas'    => $main_canvas);
+    $self->Advertise('left_canvas'    => $left_canvas);
+    $self->Advertise('top_canvas'     => $top_canvas);
+    $self->Advertise('topleft_canvas' => $topleft_canvas);
+    $self->Advertise('xscrollbar'     => $sh);
+    $self->Advertise('yscrollbar'     => $sv);
+
+    # delegate configuration to the main canvas:
+    $self->ConfigSpecs(
+        -background => [ [ 'DESCENDANTS', 'SELF' ], 'background', 'Background', 'white' ],
+        -foreground => [ [ 'DESCENDANTS', 'SELF' ], 'foreground', 'Foreground', 'black' ],
+        -scrollregion => [ 'METHOD', 'scrollregion', 'Scrollregion', [ 0, 0, 0, 0 ] ],
+        'DEFAULT' => ['main_canvas'],
+    );
+
+    # delegate methods to the main canvas:
+    $self->Delegates('DEFAULT' => 'main_canvas',);
 }
 
 sub canvases {
-	my $self = shift @_;
+    my $self = shift @_;
 
-	return [ map { $self->Subwidget($_); }
-				('main_canvas','left_canvas','top_canvas','topleft_canvas')
-	];
+    return [ map { $self->Subwidget($_); } ('main_canvas', 'left_canvas', 'top_canvas', 'topleft_canvas') ];
 }
 
 # overrriding the existing canvas methods:
 
-sub delete { # override the standard method
-	my $self = shift @_;
+sub delete {    # override the standard method
+    my $self = shift @_;
 
-	for my $c (@{$self->canvases()}) {
-		$c->delete(@_);
-	}
+    for my $c (@{ $self->canvases() }) {
+        $c->delete(@_);
+    }
 }
 
 sub scrollregion {
-	my $self = shift @_;
-	if(scalar(@_)) { # configure request
-		$self->fit_everything();
-	} else { # cget request
-		return $self->Subwidget('main_canvas')->cget(-scrollregion);
-	}
+    my $self = shift @_;
+    if (scalar(@_)) {    # configure request
+        $self->fit_everything();
+    }
+    else {               # cget request
+        return $self->Subwidget('main_canvas')->cget(-scrollregion);
+    }
 }
 
 # calls to 'xview()' and 'yview()' should get propagated to the correct subcanvases:
 
 sub xview {
-	my $self = shift @_;
+    my $self = shift @_;
 
-	if(!scalar(@_)) {
-		return $self->Subwidget('main_canvas')->xview();
-	} else {
-		if(($_[0] eq 'moveto')&&($_[1]<0)) { # don't let it become negative
-			$_[1] = 0;
-		}
-		foreach my $c ('main_canvas','top_canvas') {
-			$self->Subwidget($c)->xview(@_);
-		}
-	}
+    if (!scalar(@_)) {
+        return $self->Subwidget('main_canvas')->xview();
+    }
+    else {
+        if (($_[0] eq 'moveto') && ($_[1] < 0)) {    # don't let it become negative
+            $_[1] = 0;
+        }
+        foreach my $c ('main_canvas', 'top_canvas') {
+            $self->Subwidget($c)->xview(@_);
+        }
+    }
 }
 
 sub yview {
-	my $self = shift @_;
+    my $self = shift @_;
 
-	if(!scalar(@_)) {
-		return $self->Subwidget('main_canvas')->yview();
-	} else {
-		if(($_[0] eq 'moveto')&&($_[1]<0)) { # don't let it become negative
-			$_[1] = 0;
-		}
-		foreach my $c ('main_canvas','left_canvas') {
-			$self->Subwidget($c)->yview(@_);
-		}
-	}
+    if (!scalar(@_)) {
+        return $self->Subwidget('main_canvas')->yview();
+    }
+    else {
+        if (($_[0] eq 'moveto') && ($_[1] < 0)) {    # don't let it become negative
+            $_[1] = 0;
+        }
+        foreach my $c ('main_canvas', 'left_canvas') {
+            $self->Subwidget($c)->yview(@_);
+        }
+    }
 }
 
-sub defmin { # not a method
-	my ($def,$a,$b) = @_;
+sub defmin {    # not a method
+    my ($def, $a, $b) = @_;
 
-	return defined($a)
-			? (defined($b)
-				? (($a<$b)?$a:$b)
-				: $a)
-			: (defined($b)
-				? $b
-				: $def);
+    return defined($a)
+      ? (
+        defined($b)
+        ? (($a < $b) ? $a : $b)
+        : $a
+      )
+      : (
+        defined($b)
+        ? $b
+        : $def
+      );
 }
 
-sub defmax { # not a method
-	my ($def,$a,$b) = @_;
+sub defmax {    # not a method
+    my ($def, $a, $b) = @_;
 
-	return defined($a)
-			? (defined($b)
-				? (($a>$b)?$a:$b)
-				: $a)
-			: (defined($b)
-				? $b
-				: $def);
+    return defined($a)
+      ? (
+        defined($b)
+        ? (($a > $b) ? $a : $b)
+        : $a
+      )
+      : (
+        defined($b)
+        ? $b
+        : $def
+      );
 }
 
 sub fit_everything {
-	my $self = shift @_;
+    my $self = shift @_;
 
-	my ($m_x1,$m_y1,$m_x2,$m_y2) = $self->Subwidget('main_canvas')->bbox('all');
-	my ($t_x1,$t_y1,$t_x2,$t_y2) = $self->Subwidget('top_canvas')->bbox('all');
-	my ($l_x1,$l_y1,$l_x2,$l_y2) = $self->Subwidget('left_canvas')->bbox('all');
-	my ($tl_x1,$tl_y1,$tl_x2,$tl_y2) = $self->Subwidget('topleft_canvas')->bbox('all');
+    my ($m_x1,  $m_y1,  $m_x2,  $m_y2)  = $self->Subwidget('main_canvas')->bbox('all');
+    my ($t_x1,  $t_y1,  $t_x2,  $t_y2)  = $self->Subwidget('top_canvas')->bbox('all');
+    my ($l_x1,  $l_y1,  $l_x2,  $l_y2)  = $self->Subwidget('left_canvas')->bbox('all');
+    my ($tl_x1, $tl_y1, $tl_x2, $tl_y2) = $self->Subwidget('topleft_canvas')->bbox('all');
 
-	my $w_x1 = defmin(0,$tl_x1,$l_x1);
-	my $w_x2 = defmax(0,$tl_x2,$l_x2);
+    my $w_x1 = defmin(0, $tl_x1, $l_x1);
+    my $w_x2 = defmax(0, $tl_x2, $l_x2);
 
-	my $n_y1 = defmin(0,$tl_y1,$t_y1);
-	my $n_y2 = defmax(0,$tl_y2,$t_y2);
+    my $n_y1 = defmin(0, $tl_y1, $t_y1);
+    my $n_y2 = defmax(0, $tl_y2, $t_y2);
 
-	my $e_x1 = defmin(0,$t_x1,$m_x1);
-	my $e_x2 = defmax(0,$t_x2,$m_x2);
+    my $e_x1 = defmin(0, $t_x1, $m_x1);
+    my $e_x2 = defmax(0, $t_x2, $m_x2);
 
-	my $s_y1 = defmin(0,$l_y1,$m_y1);
-	my $s_y2 = defmax(0,$l_y2,$m_y2);
+    my $s_y1 = defmin(0, $l_y1, $m_y1);
+    my $s_y2 = defmax(0, $l_y2, $m_y2);
 
-	$self->Subwidget('topleft_canvas')->configure(
-		-scrollregion =>[$w_x1,$n_y1,$w_x2,$n_y2],
-		-width  => ($w_x2-$w_x1),
-		-height => ($n_y2-$n_y1),
-	);
+    $self->Subwidget('topleft_canvas')->configure(
+        -scrollregion => [ $w_x1, $n_y1, $w_x2, $n_y2 ],
+        -width        => ($w_x2 - $w_x1),
+        -height       => ($n_y2 - $n_y1),
+    );
 
-	$self->Subwidget('left_canvas')->configure(
-		-scrollregion =>[$w_x1,$s_y1,$w_x2,$s_y2],
-		-width  => ($w_x2-$w_x1),
-	);
+    $self->Subwidget('left_canvas')->configure(
+        -scrollregion => [ $w_x1, $s_y1, $w_x2, $s_y2 ],
+        -width        => ($w_x2 - $w_x1),
+    );
 
-	$self->Subwidget('top_canvas')->configure(
-		-scrollregion =>[$e_x1,$n_y1,$e_x2,$n_y2],
-		-height => ($n_y2-$n_y1),
-	);
+    $self->Subwidget('top_canvas')->configure(
+        -scrollregion => [ $e_x1, $n_y1, $e_x2, $n_y2 ],
+        -height       => ($n_y2 - $n_y1),
+    );
 
-	$self->Subwidget('main_canvas')->configure(
-		-scrollregion =>[$e_x1,$s_y1,$e_x2,$s_y2],
-	);
+    $self->Subwidget('main_canvas')->configure(-scrollregion => [ $e_x1, $s_y1, $e_x2, $s_y2 ],);
 
-	for my $c (@{ $self->canvases() }) {
-		$c->xviewMoveto(0);
-		$c->yviewMoveto(0);
-	}
+    for my $c (@{ $self->canvases() }) {
+        $c->xviewMoveto(0);
+        $c->yviewMoveto(0);
+    }
 }
 
 1;
