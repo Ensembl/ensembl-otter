eac89c6b60f39c751fddbd9b99f53bbb1859df1d
gr5 <gr5>
2010-05-06 09:22:25 +0000

    adding strand info to coverage filter

--- modules/Bio/Vega/ServerAnalysis/SolexaDepthBins.pm
+++ modules/Bio/Vega/ServerAnalysis/SolexaDepthBins.pm
@@ -21,62 +21,67 @@ sub run {
     
     my @depth_features;
     
-    my %depth;
+    my $depth_forward;
+    my $depth_reverse;
     
     my $slice;
     
     for my $af (@$features) {
         $slice ||= $af->slice;
+        my $depth = $af->strand == 1 ? $depth_forward : $depth_reverse; 
         my @fs = $af->ungapped_features;
         for my $f (@fs) {
             for (my $i = $f->start; $i < $f->end; $i++) {
-                $depth{$i}++;    
+                $depth->{$i}++;    
             }
         }
     }
     
-    my ($start, $end, $tot_depth);
-    
-    my $i = 0;
-    
-    my $bin_cnt = 0;
-    
-    for my $b (sort { $a <=> $b } keys %depth) {
+    for my $depth ($depth_forward, $depth_reverse) {
         
-        my $depth = $depth{$b};
+        my $strand = ($depth == $depth_forward ? 1 : -1);
         
-        $bin_cnt++;
+        my ($start, $end, $tot_depth);
         
-        if (!$start) {
-            $start = $b;
-            $end = $b;
-            $tot_depth = $depth;
-        }
-        elsif ( ($bin_cnt == $BIN_SIZE) || ($b != $end+1)) {
+        my $bin_cnt = 0;
+        
+        for my $b (sort { $a <=> $b } keys %depth) {
             
-            # end this feature
-                            
-            my $score = $tot_depth / ($end - $start +1);
-      
-            my $sf = Bio::EnsEMBL::SimpleFeature->new(
-                -start         => $start,
-                -end           => $end,
-                -strand        => 1,
-                -slice         => $slice,
-                -score         => $score,
-                -display_label => sprintf("Average depth: %.2f", $score),
-            );
-        
-            push @depth_features, $sf;
-        
-            $start = $b;
-            $end = $b;
-            $tot_depth = $depth;
-            $bin_cnt = 0;
-        }
-        else {
-            $end++;
-            $tot_depth += $depth;
+            my $depth = $depth->{$b};
+            
+            $bin_cnt++;
+            
+            if (!$start) {
+                $start = $b;
+                $end = $b;
+                $tot_depth = $depth;
+            }
+            elsif ( ($bin_cnt == $BIN_SIZE) || ($b != $end+1)) {
+                
+                # end this feature
+                                
+                my $score = $tot_depth / ($end - $start +1);
+          
+                my $sf = Bio::EnsEMBL::SimpleFeature->new(
+                    -start         => $start,
+                    -end           => $end,
+                    -strand        => $strand,
+                    -slice         => $slice,
+                    -score         => $score,
+                    -display_label => sprintf("Average depth: %.2f", $score),
+                );
+            
+                push @depth_features, $sf;
+            
+                $start = $b;
+                $end = $b;
+                $tot_depth = $depth;
+                $bin_cnt = 0;
+            }
+            else {
+                $end++;
+                $tot_depth += $depth;
+            }
         }
     }
     
