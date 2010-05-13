365fad3e5d05f38b151d91cf8995fac1522b543e
gr5 <gr5>
2010-05-13 13:26:03 +0000

    several improvements: we no longer use the Client module and just create our own LWP user agent, we only require packages that are only used on a cache miss if we need them, and we print data to zmap before cacheing to improve performance

--- scripts/gff_http.pl
+++ scripts/gff_http.pl
@@ -2,24 +2,18 @@
 
 use Digest::MD5 qw(md5_hex);
 use URI::Escape qw(uri_escape);
-use HTTP::Request;
-
-use Bio::Otter::Lace::Defaults;
 
 my $DEBUG   = 1;
 my $LOG     = 1;
-
-my $client = Bio::Otter::Lace::Defaults::make_Client();
 
 my %args = map {split /=/} @ARGV;
 
 # pull off arguments meant for us
 
-my $url_root = delete $args{url_root};
-
-my $server_script = delete $args{server_script};
-
-my $session_dir = delete $args{session_dir};
+my $url_root        = delete $args{url_root};
+my $server_script   = delete $args{server_script};
+my $session_dir     = delete $args{session_dir};
+my $cookie_jar      = delete $args{cookie_jar};
 
 if ($LOG) {
     my $log_file_name = $session_dir.'/gff_log.txt';
@@ -40,7 +34,7 @@ if ($DEBUG) {
     $gff_filename = $args{gff_source}.'.gff';
 }
 else {
-    $gff_filename = md5_hex($params).'gff';
+    $gff_filename = md5_hex($params).'.gff';
 }
 
 my $top_dir = $session_dir.'/gff_cache';
@@ -76,6 +70,12 @@ if (-e $cache_file) {
 else {
     # cache miss
     
+    # only require these packages now, so we don't take the import hit on a cache hit
+    
+    require LWP::UserAgent;
+    require HTTP::Request;
+    require HTTP::Cookies::Netscape;
+    
     print LOG_FILE "cache miss for $gff_filename\n" if $LOG;
     
     my $request = HTTP::Request->new;
@@ -87,23 +87,26 @@ else {
     my $url = $url_root . '/' . $server_script . '?' . $params;
 
     print LOG_FILE "URL: $url\n" if $LOG && $DEBUG;
-
+    
     $request->uri($url);
+    
+    # create a user agent to send the request
 
-    my $response = $client->request($request);
+    my $ua = LWP::UserAgent->new(
+        timeout             => 9000,
+        env_proxy           => 1,
+        agent               => $0,
+        cookie_jar          => HTTP::Cookies::Netscape->new(file => $cookie_jar),
+        protocols_allowed   => [qw(http https)],
+    );
+
+    my $response = $ua->request($request);
 
     if ($response && $response->is_success) {
 
         my $gff = $response->decoded_content;
 
         if ($gff =~ /EnsEMBL2GFF/) {
-            
-            # cache the result
-            open CACHE_FILE, ">$cache_file" or print STDERR "Failed to open cache file: $!\n";
-        
-            print CACHE_FILE $gff;
-
-            #close CACHE_FILE;
             
             print $gff;
             
@@ -113,7 +116,12 @@ else {
             # below and so zmap would not start drawing until after the 
             # child exits, which is not what we want at all!
 
-            #close STDOUT;
+            close STDOUT;
+            
+            # cache the result
+            open CACHE_FILE, ">$cache_file" or print STDERR "Failed to open cache file: $!\n";
+        
+            print CACHE_FILE $gff;
         }
         else {
             print STDERR "Got unexpected response from webserver: $gff\n";
