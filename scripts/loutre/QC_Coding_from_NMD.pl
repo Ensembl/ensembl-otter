9a779ab59e3d13aef32d754794034e2c84fec34e
ml6 <ml6>
2010-07-21 14:02:16 +0000

    set of QC scripts

---
+++ scripts/loutre/QC_Coding_from_NMD.pl
@@ -0,0 +1,147 @@
+#!/software/bin/perl -w
+
+=head1 NAME
+
+QC_Coding_from_NMD.pl
+
+=head1 SYNOPSIS
+
+QC_Coding_from_NMD.pl
+
+=head1 DESCRIPTION
+
+This script is part of a set of quality control scripts, it finds wrongly annotated 
+NMD transcripts in the specified loutre database
+
+here is an example commandline
+
+./QC_Coding_from_NMD.pl
+
+
+=head1 OPTIONS
+
+    -host (default:otterlive)   host name for the loutre database (gets put as phost= in locator)
+    -dbname (no default)  For RDBs, what name to connect to (pname= in locator)
+    -user (check the ~/.netrc file)  For RDBs, what username to connect as (puser= in locator)
+    -pass (check the ~/.netrc file)  For RDBs, what password to use (ppass= in locator)
+    -port (check the ~/.netrc file)   For RDBs, what port to use (pport= in locator)
+
+    -help|h	displays this documentation with PERLDOC
+
+=head1 CONTACT
+
+Mustapha Larbaoui B<email> ml6@sanger.ac.uk
+
+=cut
+
+use strict;
+use Getopt::Long;
+use Net::Netrc;
+use Bio::Vega::DBSQL::DBAdaptor;
+
+# loutre connexion parameters, default values.
+my $host = 'otterlive';
+my $port = '';
+my $name = '';
+my $user = '';
+my $pass = '';
+
+my $usage = sub { exec( 'perldoc', $0 ); };
+
+&GetOptions(
+    'host:s'                => \$host,
+    'port:n'                => \$port,
+    'dbname:s'              => \$name,
+    'user:s'                => \$user,
+    'pass:s'                => \$pass,
+    'h|help!'               => $usage
+  )
+  or $usage->();
+
+if ( !$user || !$pass || !$port ) {
+    my @param = &get_db_param($host);
+    $user = $param[0] unless $user;
+    $pass = $param[1] unless $pass;
+    $port = $param[2] unless $port;
+}
+
+my $tsi_sql = qq/SELECT s.name,  ga.value, gsi.stable_id, tsi.stable_id, t.seq_region_start, t.seq_region_end, t.seq_region_strand, t.biotype, t.status
+FROM transcript t, transcript_stable_id tsi, gene g, gene_attrib ga, gene_stable_id gsi, seq_region s
+WHERE t.biotype = 'Nonsense_mediated_decay'
+AND t.status = 'UNKNOWN'
+AND g.gene_id = t.gene_id
+AND g.gene_id = ga.gene_id
+AND ga.attrib_type_id = 4
+AND gsi.gene_id = g.gene_id
+AND g.is_current
+AND t.is_current
+AND g.source = 'havana'
+AND tsi.transcript_id = t.transcript_id
+AND s.seq_region_id = t.seq_region_id
+ORDER BY s.name, t.seq_region_start/;
+
+my $dba = Bio::Vega::DBSQL::DBAdaptor->new(
+        -user   => $user,
+        -dbname => $name,
+        -host   => $host,
+        -port   => $port,
+        -pass   => $pass);
+my $t_ad = $dba->get_TranscriptAdaptor;
+        
+my $tsi_sth = $dba->dbc->prepare($tsi_sql);
+$tsi_sth->execute;
+
+TRANSCRIPT: while(my ($sr_name, $locus, $gsi, $tsi, $start, $end, $strand, $biotype, $status) = $tsi_sth->fetchrow_array){
+	my $t = $t_ad->fetch_by_stable_id($tsi);
+	my $translation = $t->translation;
+	## Condition 1 : Should have a translation (skip old NMD annotation without translation)
+	next TRANSCRIPT unless $translation;
+	## Condition 2 : ORF > 35aa
+	my $orf_length = length($translation->seq);
+	my ($CDS_start_NF) = @{ $t->get_all_Attributes('cds_start_NF') };
+	## Condition 3 : Stop codon > 50bases from last splice junction
+	my $exons = $t->get_all_Exons();
+	my $last_coding_exon = $translation->end_Exon();
+	my $last_coding_exon_end = $translation->end();
+	pop @$exons; # remove last exon
+	my $stop_to_splice = 0;
+	my $flag;
+	EXON: foreach(@$exons){
+		next EXON unless ($_ eq $last_coding_exon || $flag);
+		if($_ eq $last_coding_exon) {
+		  $stop_to_splice = $_->seq->length - $last_coding_exon_end;
+		  $flag = 1;
+		  next EXON;	
+		}
+		$stop_to_splice += $_->seq->length;
+	}
+	## Condition 4 : CDS variant reference ?
+	
+	## Print transcript info if it reaches this point 
+	print STDOUT join(" ",$sr_name, $locus, $gsi, $tsi, $start, $end, $strand, $biotype, $status,$orf_length,$stop_to_splice)."\n" 
+	   if(( $orf_length <= 35 && !$CDS_start_NF) || $stop_to_splice <= 50);
+}        
+
+sub get_db_param {
+    my ( $dbhost ) = @_;
+    my ( $dbuser, $dbpass, $dbport );
+
+    my $ref = Net::Netrc->lookup($dbhost);
+    throw("$dbhost entry is missing from ~/.netrc") unless ($ref);
+    $dbuser = $ref->login;
+    $dbpass = $ref->password;
+    $dbport = $ref->account;
+    throw(
+        "Missing parameter in the ~/.netrc file:\n
+            machine " .  ( $dbhost || 'missing' ) . "\n
+            login " .    ( $dbuser || 'missing' ) . "\n
+            password " . ( $dbpass || 'missing' ) . "\n
+            account "
+          . ( $dbport || 'missing' )
+          . " (should be used to set the port number)"
+      )
+      unless ( $dbuser && $dbpass && $dbport );
+
+    return ( $dbuser, $dbpass, $dbport );
+}
+
---
+++ scripts/loutre/QC_chromosome_version.pl
@@ -0,0 +1,156 @@
+#!/software/bin/perl -w
+
+=head1 NAME
+
+QC_chromosome_version.pl
+
+=head1 SYNOPSIS
+
+QC_chromosome_version.pl
+
+=head1 DESCRIPTION
+
+This script is part of a set of quality control scripts, it checks the chromosome versions in the pipeline databases 
+and print a warning or change its version if more than one version of a chromosome is present. 
+The old chromosomes in the pipeline datbases should all be OtterArchive chromosome version to 
+prevent problem when fetching features on chromosome slice.
+
+here is an example commandline
+
+./QC_chromosome_version.pl
+
+
+=head1 OPTIONS
+
+    -host (default:otterpipe1,otterpipe2)   host name for the pipeline databases
+    -dbname (optional) pipeline database name
+    -user (check the ~/.netrc file)  what username to connect as
+    -pass (check the ~/.netrc file)  what password to use
+    -port (check the ~/.netrc file)  what port to use
+
+    -change	make the detected old chromosome OtterArchive (to use with extreme caution :-)
+    -help|h	displays this documentation with PERLDOC
+
+=head1 CONTACT
+
+Mustapha Larbaoui B<email> ml6@sanger.ac.uk
+
+=cut
+
+use strict;
+use warnings;
+use Getopt::Long;
+use DBI;
+use Net::Netrc;
+use Bio::EnsEMBL::Utils::Exception qw(throw warning);
+
+my @dbnames;
+my $hosts = ['otterpipe1','otterpipe2'];
+my $user;
+my $port;
+my $pass;
+my $change;
+my $help;
+my $usage = sub { exec( 'perldoc', $0 ); };
+
+GetOptions(
+			'host=s@'   => \$hosts,
+			'port=n'   => \$port,
+			'dbname=s' => \@dbnames,
+			'user=s'   => \$user,
+			'pass=s'   => \$pass,
+			'change!'  => \$change,
+			'h|help!'  => $usage,
+) or $usage->();
+
+@$hosts   = map( split( /,/, $_ ), @$hosts );
+@dbnames = map( split( /,/, $_ ), @dbnames );
+
+my $sql = qq/
+SELECT s.seq_region_id,
+       a1.value AS chromosome, 
+       s.name AS seq_set
+FROM attrib_type t1, coord_system c, seq_region s
+LEFT JOIN seq_region_attrib a1 ON (a1.seq_region_id =  s.seq_region_id AND t1.attrib_type_id = a1.attrib_type_id)
+WHERE c.coord_system_id = s.coord_system_id
+AND t1.code = 'chr'
+AND c.name = 'chromosome'
+AND c.version = 'Otter'
+AND s.name LIKE 'chr%'
+ORDER BY  chromosome, s.seq_region_id DESC
+/;
+
+foreach my $host (@$hosts) {
+	my @db;
+	if ( !@dbnames ) {
+		# get the list of pipeline here
+		my $sth = &get_sth($host,undef,"SHOW DATABASES LIKE 'pipe_%'");
+		$sth->execute();
+		while(my ($pipe) = $sth->fetchrow_array){
+			push @db, $pipe;
+		}
+	} else {
+		@db = @dbnames;
+	}
+	foreach(@db){
+		next if /pipe_cow/;
+		print STDOUT "QC_chr_version: Check $host:$_\n";
+		&process_database($host,$_);
+	}
+}
+
+sub process_database {
+	my ($host,$db_name) = @_;
+	my $hash;
+	my $sth = &get_sth($host,$db_name,$sql);
+	$sth->execute();
+	while(my ($srid,$chr,$sr_name) = $sth->fetchrow_array){
+		next unless $sr_name =~ /chr(\d+|X|Y|U|H)(-|_)(\d+|NCBIM37)/;
+		if($hash->{$chr}) {
+			# do something if more than one version
+			print STDOUT "QC_chr_version: ${srid}|${chr}|${sr_name} should be OtterArchive\n";
+			&change_version($host,$db_name,$srid) if $change;
+		} else {
+			$hash->{$chr} = [$srid,$sr_name];			
+		}		
+	}
+}
+
+sub change_version {
+	my ($host,$db_name,$srid) = @_;
+	print STDOUT "QC_chr_version: $srid changed to OtterArchive\n";
+	my $sql = qq{
+		UPDATE seq_region s, coord_system cs
+		SET s.coord_system_id = cs.coord_system_id
+		WHERE cs.version = 'OtterArchive'
+		AND s.seq_region_id = $srid
+	};	
+	my $sth = &get_sth($host,$db_name,$sql);
+	$sth->execute;
+}
+
+my %dbi_hash;
+sub get_sth {
+	my ($host,$db_name,$sql) = @_;
+	$db_name ||= 'no_db';
+	if(!$dbi_hash{$host}->{$db_name}){
+		my $ref = Net::Netrc->lookup($host);
+		thow("No entry found in ~/.netrc for host $host") unless $ref;
+		$user = $ref->login if $ref->login;
+		$pass = $ref->password if $ref->password;
+		$port = $ref->account if $ref->account;
+	
+		# Creating the DB connection
+		my $database = $db_name ne 'no_db' ? "database=$db_name":"";
+		my $dsn = "DBI:mysql:$database;host=$host;port=$port";
+		my $dbh = DBI->connect( $dsn, $user, $pass, { 'RaiseError' => 1 } );
+		$dbi_hash{$host}->{$db_name} = $dbh;		
+	}
+
+	my $sth = $dbi_hash{$host}->{$db_name}->prepare($sql);	
+	
+	return $sth;
+}
+
+
+
