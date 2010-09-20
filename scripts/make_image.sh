cf3544a2da9f8da340c9f29150972fc98f4f6cc3
jgrg <jgrg>
2010-09-20 13:56:09 +0000

    *** empty log message ***

---
+++ scripts/make_image.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+touch otterlace.app
+
+release="otterlace_mac_intel_52-15"
+sparse="$release.sparseimage"
+dmg="$release.dmg"
+
+if [ -e "$sparse" ]
+then
+  echo "Converting sparseimage to $dmg"
+  if [ -e "/Volumes/$release" ]
+  then
+    echo "Error: Disk $release is mounted"
+    exit 1
+  fi
+  rm "$release.dmg"
+  hdiutil convert "$sparse" -format UDBZ -o "$dmg"
+else
+  echo "Creating sparseimage $sparse"
+  hdiutil create -fs HFS+ -volname "$release" "$sparse"
+  open "$sparse"
+fi
