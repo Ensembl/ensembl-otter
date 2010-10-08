fb85212a87527105b2d780d5eb4d6956ab9814a2
jh13 <jh13>
2010-10-08 10:48:33 +0000

    Vega/PredictionTranscript.pm: revamp argument assignment (quietens "perlcritic --stern").

--- modules/Bio/Vega/PredictionTranscript.pm
+++ modules/Bio/Vega/PredictionTranscript.pm
@@ -8,14 +8,14 @@ use Bio::EnsEMBL::Utils::Argument qw( rearrange );
 use base 'Bio::EnsEMBL::PredictionTranscript';
 
 sub new {
-    my $class = shift;
+    my ($class, @args) = shift;
 
-    my $self = $class->SUPER::new(@_);
+    my $self = $class->SUPER::new(@args);
 
     my ($truncated_5_prime, $truncated_3_prime) = rearrange([
         'TRUNCATED_5_PRIME',
         'TRUNCATED_3_PRIME'
-    ], @_);
+    ], @args);
     
     $self->{truncated_5_prime} = $truncated_5_prime;
     $self->{truncated_3_prime} = $truncated_3_prime;
@@ -24,14 +24,14 @@ sub new {
 }
 
 sub truncated_5_prime {
-    my $self = shift;
-    $self->{truncated_5_prime} = shift if @_;
+    my ($self, @args) = @_;
+    $self->{truncated_5_prime} = shift @args if @args;
     return $self->{truncated_5_prime};
 }
 
 sub truncated_3_prime {
-    my $self = shift;
-    $self->{truncated_3_prime} = shift if @_;
+    my ($self, @args) = @_;
+    $self->{truncated_3_prime} = shift @args if @args;
     return $self->{truncated_3_prime};
 }
 
