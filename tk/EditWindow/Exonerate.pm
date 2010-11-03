3a1f424204f531b28a34730cdce37c026e7c24c5
jh13 <jh13>
2010-11-03 09:38:34 +0000

    Refactor EditWindow::Exonerate::launch_exonerate() internals into XaceSeqChooser::launch_exonerate().

--- tk/EditWindow/Exonerate.pm
+++ tk/EditWindow/Exonerate.pm
@@ -10,7 +10,6 @@ use Carp;
 use Hum::Pfetch;
 use Hum::FastaFileIO;
 use Hum::ClipboardUtils qw{ accessions_from_text };
-use Bio::Otter::Lace::Exonerate;
 use Bio::Otter::Lace::Client;
 use Hum::Sort qw{ ace_sort };
 use Tk::LabFrame;
@@ -20,9 +19,6 @@ use Data::Dumper;
 
 use base 'EditWindow';
 
-my $PROT_SCORE        = 100;
-my $DNA_SCORE         = 100;
-my $DNAHSP            = 120;
 my $BEST_N            = 1;
 my $MAX_INTRON_LENGTH = 200000;
 my $MAX_QUERY_LENGTH  = 10000;
@@ -425,39 +421,6 @@ sub launch_exonerate {
         return;
     }
 
-    my %seqs_by_type = ();
-
-    for my $seq (@$seqs) {
-        if (
-            $seq->type
-            && (   $seq->type eq 'EST'
-                || $seq->type eq 'ncRNA'
-                || $seq->type eq 'mRNA'
-                || $seq->type eq 'Protein')
-          )
-        {
-            push @{ $seqs_by_type{ $seq->type } }, $seq;
-        }
-        elsif ($seq->sequence_string =~ /^[AGCTNagctn\s]*$/) {
-            push @{ $seqs_by_type{'Unknown_DNA'} }, $seq;
-        }
-        else {
-            push @{ $seqs_by_type{'Unknown_Protein'} }, $seq;
-        }
-    }
-
-    # get marked region (if requested)
-
-    my ($genomic_start, $genomic_end) = (1, $self->XaceSeqChooser->Assembly->Sequence->sequence_length);
-
-    if ($self->{'_use_marked_region'}) {
-        my ($mark_start, $mark_end) = $self->XaceSeqChooser->zMapGetMark;
-        if ($mark_start && $mark_end) {
-            warn "Setting exonerate genomic start & end to marked region: $mark_start - $mark_end\n";
-            ($genomic_start, $genomic_end) = ($mark_start, $mark_end);
-        }
-    }
-
     $self->top->Busy;
 
     if ($self->{'_clear_existing'}) {
@@ -470,83 +433,13 @@ OTF_mRNA
 OTF_Protein });
     }
 
-    my $need_relaunch = 0;
-    my @method_names;
-    my $ace_text = '';
-    for my $type (keys %seqs_by_type) {
-
-        print STDERR "Running exonerate for sequence(s) of type: $type\n";
-
-        my $score    = $type =~ /Protein/  ? $PROT_SCORE : $DNA_SCORE;
-        my $ana_name = $type =~ /^Unknown/ ? $type       : "OTF_$type";
-        my $dnahsp   = $DNAHSP;
-        my $best_n   = $self->get_entry('bestn');
-        my $max_intron_length = $self->get_entry('max_intron_length');
-
-        my $exonerate = Bio::Otter::Lace::Exonerate->new;
-        $exonerate->AceDatabase($self->XaceSeqChooser->AceDatabase);
-        $exonerate->genomic_seq($self->XaceSeqChooser->Assembly->Sequence);
-        $exonerate->genomic_start($genomic_start);
-        $exonerate->genomic_end($genomic_end);
-        $exonerate->query_seq($seqs_by_type{$type});
-        $exonerate->sequence_fetcher($self->{'_name_seq'});
-        $exonerate->acedb_homol_tag($ana_name . '_homol');
-        $exonerate->query_type($type =~ /Protein/ ? 'protein' : 'dna');
-        $exonerate->score($score);
-        $exonerate->dnahsp($dnahsp);
-        $exonerate->bestn($best_n);
-        $exonerate->max_intron_length($max_intron_length);
-        $exonerate->method_tag($ana_name);
-        $exonerate->logic_name($ana_name);
-
-        my $seq_file = $exonerate->write_seq_file();
-
-        if ($seq_file) {
-            $exonerate->initialise($seq_file);
-            my $ace_output .= $exonerate->run;
-
-            # delete query file
-            unlink $seq_file;
-
-            next unless $ace_output;
-
-            # add hit sequences into ace text
-            my $names = $exonerate->delete_all_hit_names;
-
-            # only add the sequence to acedb if they are not pfetchable (i.e. they are unknown)
-            if ($type =~ /^Unknown/) {
-                foreach my $hit_name (@$names) {
-                    my $seq = $self->{'_name_seq'}->{$hit_name};
-
-                    if ($exonerate->query_type eq 'protein') {
-                        $ace_output .= $self->ace_PEPTIDE($hit_name, $seq);
-                    }
-                    else {
-                        $ace_output .= $self->ace_DNA($hit_name, $seq);
-                    }
-                }
-            }
-
-            $need_relaunch = 1;
-
-            # Need to add new method to collection if we don't have it already
-            my $coll      = $exonerate->AceDatabase->MethodCollection;
-            my $coll_zmap = $self->XaceSeqChooser->Assembly->MethodCollection;
-            my $method    = $exonerate->ace_Method;
-            push @method_names, $method->name;
-            unless ($coll->get_Method_by_name($method->name)
-                || $coll_zmap->get_Method_by_name($method->name))
-            {
-                $coll->add_Method($method);
-                $coll_zmap->add_Method($method);
-                $self->XaceSeqChooser->save_ace($coll->ace_string());
-            }
-            $ace_text .= $ace_output;
-        }
-    }
-
-    $self->XaceSeqChooser->save_ace($ace_text);
-    $self->XaceSeqChooser->zMapLoadFeatures(@method_names) if $need_relaunch;
+    my @exonerate_params = (
+        -use_marked_region => $self->{_use_marked_region},
+        -best_n            => $self->get_entry('bestn'),
+        -max_intron_length => $self->get_entry('max_intron_length'),
+        );
+    my $need_relaunch =
+        $self->XaceSeqChooser->launch_exonerate($seqs, @exonerate_params);
 
     $self->top->Unbusy;
 
@@ -702,47 +595,7 @@ sub get_query_seq {
         }
     }
 
-    @seqs = @confirmed_seqs;
-
-    # lower case query polyA/T tails to avoid spurious exons
-
-    foreach my $seq (@seqs) {
-        my $s = $seq->uppercase;
-        $s =~ s/(^T{6,}|A{6,}$)/lc($1)/ge;
-
-        $seq->sequence_string($s);
-
-        # save seq in hash
-        $self->{'_name_seq'}->{ $seq->name } = $seq;
-    }
-
-    return \@seqs;
-}
-
-sub ace_DNA {
-    my ($self, $name, $seq) = @_;
-
-    my $ace = qq{\nSequence "$name"\n\nDNA "$name"\n};
-
-    my $dna_string = $seq->sequence_string;
-
-    while ($dna_string =~ /(.{1,60})/g) {
-        $ace .= $1 . "\n";
-    }
-
-    return $ace;
-}
-
-sub ace_PEPTIDE {
-    my ($self, $name, $seq) = @_;
-
-    my $ace = qq{\nProtein "$name"\n\nPEPTIDE "$name"\n};
-
-    my $prot_string = $seq->sequence_string;
-    while ($prot_string =~ /(.{1,60})/g) {
-        $ace .= $1 . "\n";
-    }
-    return $ace;
+    return \@confirmed_seqs;
 }
 
 sub DESTROY {
