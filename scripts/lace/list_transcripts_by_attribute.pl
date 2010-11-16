72adb9d084b1b2d6931df1bed62d7ab2bd2af0f5
mg13 <mg13>
2010-11-16 17:12:12 +0000

    First version of list_transcripts_by_attribute.pl

---
+++ scripts/lace/list_transcripts_by_attribute.pl
@@ -0,0 +1,124 @@
+#!/usr/bin/env perl
+
+# In response to ticket #193917: List of NMD exceptions
+
+use warnings;
+use strict;
+
+use Carp;
+use Bio::Otter::Lace::Defaults;
+
+{
+    my $dataset_name = undef;
+    my $attrib_pattern = undef;
+    my $attrib_code = 'remark';
+
+    my $total = 0;
+    my $quiet = 0;
+
+    my $usage = sub { exec('perldoc', $0) };
+    # This do_getopt() call is needed to parse the otter config files
+    # even if you aren't giving any arguments on the command line.
+    Bio::Otter::Lace::Defaults::do_getopt(
+        'h|help!'       => $usage,
+        'dataset=s'     => \$dataset_name,
+	'attrib=s'      => \$attrib_pattern,
+	'code:s'        => \$attrib_code,
+        'quiet!'        => \$quiet,
+        'total!'        => \$total,
+        ) or $usage->();
+    $usage->() unless ($dataset_name and $attrib_pattern);
+
+    # Client communicates with otter HTTP server
+    my $cl = Bio::Otter::Lace::Defaults::make_Client();
+    
+    # DataSet interacts directly with an otter database
+    my $ds = $cl->get_DataSet_by_name($dataset_name);
+    my $otter_dba = $ds->get_cached_DBAdaptor;
+
+    my $list_transcripts = $otter_dba->dbc->prepare(q{
+        SELECT
+                g.gene_id,
+                gsi.stable_id,
+                ga.value,
+                t.transcript_id,
+                tsi.stable_id,
+                tan.value,
+                ta.value
+        FROM
+                transcript           t
+           JOIN transcript_attrib    ta  USING (transcript_id)
+           JOIN transcript_stable_id tsi USING (transcript_id)
+           JOIN gene                 g   ON t.gene_id = g.gene_id
+           JOIN gene_stable_id       gsi ON g.gene_id = gsi.gene_id
+           JOIN gene_attrib          ga  ON     g.gene_id = ga.gene_id 
+            			            AND ga.attrib_type_id = (
+            			                SELECT attrib_type_id
+            			                FROM   attrib_type
+            			                WHERE  code = 'name'
+            			               )
+           JOIN transcript_attrib    tan ON     t.transcript_id = tan.transcript_id
+            			            AND tan.attrib_type_id = (
+            			                SELECT attrib_type_id
+            			                FROM   attrib_type
+            			                WHERE  code = 'name'
+            			               )
+        WHERE
+                t.is_current = 1
+            AND ta.value LIKE ?
+            AND ta.attrib_type_id = (
+                                     SELECT attrib_type_id
+                                     FROM   attrib_type
+                                     WHERE  code = ?
+                                    )
+        ORDER BY gsi.stable_id, tsi.stable_id
+    });
+    $list_transcripts->execute($attrib_pattern, $attrib_code);
+
+    my $count = 0;
+    printf( "%-15s %18s : %-20s %18s [%s]\n",
+	    "Gene name", "Gene stable id",
+	    "Transcript name", "Transcript stable ID",
+	    "Attr. val." ) unless $quiet;
+    
+    while (my ($gid, $gene_sid, $gene_name, 
+	       $tid, $transcript_sid, $transcript_name,
+	       $attrib_value) = $list_transcripts->fetchrow()) {
+        ++$count;
+        printf( "%-15s %18s : %-20s %18s [%s]\n", 
+                $gene_name, $gene_sid,
+		$transcript_name, $transcript_sid,
+		$attrib_value,
+            ) unless $quiet;
+    }
+    printf "Total: %d\n", $count if $total;
+
+}
+
+
+
+__END__
+
+=head1 NAME - list_transcripts_by_attribute.pl
+
+=head1 SYNOPSIS
+
+list_transcripts_by_attribute -dataset <DATASET NAME> -attrib <ATTRIB PATTERN> 
+                              [-code <ATTRIB CODE>] [-quiet] [-total]
+
+=head1 DESCRIPTION
+
+Checks for current transcripts having the specified atttribute value 
+in an attribute of the specified code.
+
+The attribute value can contain SQL wildcards.
+The attribute code defaults to 'remark'.
+
+=head1 EXAMPLE
+
+  list_transcripts_by_attribute.pl --dataset=mouse --attrib='NMD__xception'
+
+=head1 AUTHOR
+
+Michael Gray B<email> mg13@sanger.ac.uk
+
