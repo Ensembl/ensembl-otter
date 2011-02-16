6ae07a8865dea02cd0fa11872522a93963454a21
jgrg <jgrg>
2011-02-16 15:45:35 +0000

    added process_gff_file attribute

--- modules/Bio/Otter/GFFFilter.pm
+++ modules/Bio/Otter/GFFFilter.pm
@@ -12,20 +12,38 @@ use base 'Bio::Otter::Filter';
 
 sub zmap_column {
     my ($self, $zmap_column) = @_;
-    $self->{_zmap_column} = $zmap_column if $zmap_column;
-    return $self->{_zmap_column};
+    
+    if ($zmap_column) {
+        $self->{'_zmap_column'} = $zmap_column;
+    }
+    return $self->{'_zmap_column'};
 }
 
 sub zmap_style {
     my ($self, $zmap_style) = @_;
-    $self->{_zmap_style} = $zmap_style if $zmap_style;
-    return $self->{_zmap_style};
+    
+    if ($zmap_style) {
+        $self->{'_zmap_style'} = $zmap_style;
+    }
+    return $self->{'_zmap_style'};
 }
 
 sub ditypes {
     my ($self, $ditypes) = @_;
-    $self->{_ditypes} = $ditypes if $ditypes;
-    return $self->{_ditypes};
+    
+    if ($ditypes) {
+        $self->{'_ditypes'} = $ditypes;
+    }
+    return $self->{'_ditypes'};
+}
+
+sub process_gff_file {
+    my($self, $flag) = @_;
+    
+    if (defined $flag) {
+        $self->{'_process_gff_file'} = $flag ? 1 : 0;
+    }
+    return $self->{'_process_gff_file'};
 }
 
 sub server_params {
