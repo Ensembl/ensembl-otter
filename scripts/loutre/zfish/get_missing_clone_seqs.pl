1c56fcc6d4371eb78b84a3cdd674ac72a4097673
mca <mca>
2011-03-30 11:03:00 +0000

    [2010-03-17 11:38, te3] copy of /software/noarch/linux-x86_64/zfish/agps/get_missing_clone_seqs.pl; that is all the history we have; Kerstin is handing the script over to us

--- scripts/loutre/zfish/get_missing_clone_seqs.pl
+++ scripts/loutre/zfish/get_missing_clone_seqs.pl
@@ -2,12 +2,17 @@
 
 use strict;
 
-use Bio::SeqIO;
-use Getopt::Long;
-use Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor;
-use Bio::EnsEMBL::DBSQL::DBAdaptor;
-use Bio::EnsEMBL::Pipeline::SeqFetcher::Finished_Pfetch;
-use LWP::UserAgent;
+#use Bio::SeqIO;
+#use Bio::Seq;
+#use Getopt::Long;
+#use Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor;
+#use Bio::EnsEMBL::DBSQL::DBAdaptor;
+#use Bio::EnsEMBL::Pipeline::SeqFetcher::Finished_Pfetch;
+#use LWP::UserAgent;
+#use Hum::SequenceInfo;
+use Hum::Pfetch 'get_Sequences';
+use Hum::SequenceInfo;
+use Hum::FastaFileIO;
 
 
 #automatically creates fa sequence files for clones not in the db
@@ -17,117 +22,101 @@ my @agp_files = <*.agp>;
 
 foreach my $agp_file (@agp_files) {
     
-    print "\n(get_missing_clone_seqs.pl) Processing $agp_file....\n";
-    open(my $fh, "$agp_file") or die "Can't read '$agp_file' : $!";
-    while (<$fh>) {
-	next if $_ =~ /^\#/; #skip comments
-	my @data = split('\t', $_);
-	if ($data[4] eq 'F') {
-	    
-	    my $out = &check_for_seq($data[5]);
-	    if ($out) {
-		if ($out == 1) {
-		    #print "$agp_file: $data[5] already in database\n";
+  print "\n(get_missing_clone_seqs.pl) Processing $agp_file....\n";
+  open(my $fh, "$agp_file") or die "Can't read '$agp_file' : $!";
+  while (<$fh>) {
+		next if $_ =~ /^\#/; #skip comments
+		my @data = split('\t', $_);
+		if ($data[4] eq 'F') {
+		    
+	  	my $out = &check_for_seq($data[5]);
+		  if ($out) {
+				if ($out == 1) {
+			    #print "$agp_file: $data[5] already in database\n";
+				} elsif ($out == 2) {
+		    	print "  $agp_file: $data[5] seq file already in current directory\n";
+				} elsif ($out == 3) {
+			    print "  $agp_file: $data[5] generated seq file in current directory\n";
+				}
+		  } else {
+				warn "$agp_file: Couldnt get sequence for clone $data[5]\n";
+	  	}
 		}
-		elsif ($out == 2) {
-		    print "$agp_file: $data[5] seq file already in current directory\n";
-		}
-		elsif ($out == 3) {
-		    print "$agp_file: $data[5] generated seq file in current directory\n";
-		}
-	    }
-	    else {
-		warn "$agp_file: Couldnt get sequence for clone $data[5]\n";
-	    }
 	}
-    }
-    print ".....$agp_file done.\n";
+  print ".....$agp_file done.\n\n";
 }
 
 
 
 sub check_for_seq {
-    my ($acc_ver) = @_;
-    my $seq_file = "$acc_ver.seq";
+  my ($acc_ver) = @_;
+  my $seq_file = "$acc_ver.seq";
 
-    my $pfetch ||= Bio::EnsEMBL::Pipeline::SeqFetcher::Finished_Pfetch->new;
-    my $seq = $pfetch->get_Seq_by_acc($acc_ver);
+  my $seq = get_Sequences($acc_ver);
+
+##  my $pfetch ||= Bio::EnsEMBL::Pipeline::SeqFetcher::Finished_Pfetch->new;
+##  my $seq = $pfetch->get_Seq_by_acc($acc_ver);
     
-    if ($seq) {
-	return(1);
-    }
-    else {
-
-	warn "Attempting to read fasta file <$acc_ver.seq> in current dir.\n";
-	my $in;
-	eval {
-	    $in = Bio::SeqIO->new(
-				  -file   => $seq_file,
-				  -format => 'FASTA',
-				  );
-	};
-	
-	if ($in) {
-	    return(2);
-	}
-	else {
+  if ($seq) {
+		# Sequence is available online via pfetch
+    return 1;
+  } else {
+	 
+	  print "Attempting to read fasta file <$acc_ver.seq> in current dir.\n";
+##		my $in;
+##		eval { $in = Bio::SeqIO->new(
+##				   -file   => $seq_file,
+##				   -format => 'FASTA',
+##	         );
+##				};
+##		if ($in) {
+##	    return(2);
 	    
-	    #no file present- got to generate it
-            my $acc = $acc_ver;
-	    $acc =~ s/^(\S+)\.(\d+)$/$1/;
-	    
-	    #get the url of the fasta file
-	    my $url = "http://intwebdev.sanger.ac.uk/cgi-bin/users/jgrg/submission_status?query_type=accession&query_name=$acc&Search=Search&.cgifields=query_type";
-	    foreach (my $i = 1; $i < 14; $i++) { #keeps trying three times if it fails 
-
-		my $ua = LWP::UserAgent->new;
-		my $req = HTTP::Request->new(GET => $url);
-
-		my $res = $ua->request($req);
-		my $cont = $res->content;
-		my $url2;
-
-		my $myfile;
-                if ($cont =~ m|.+Fasta file</th><td><a href=\"([^\"]+)|) {
-		
-                
-                # from here onwards this has to be replaced because the webside is not accessible via web anymore (still is from cbi4)
-                #    $url2 = "http://intwebdev.sanger.ac.uk/".$1;
-		#}
-		
-		#get the fasta file
-		#$ua = LWP::UserAgent->new;
-		#$req = HTTP::Request->new(GET => $url2);
-		#$res = $ua->request($req);
-		#$cont = $res->content;
-		
-		#$cont =~ s/^>\S+/>$acc_ver/; #change the name to acc_ver
-                    ($myfile) = ($cont =~ /(\/lustre\/cbi4\/work1\/humpub\/ftp_ghost\/zebrafish\/Chr_\d+\/\w+)/); 
-                    warn "opening $myfile\n";
-                    open(SEQ, $myfile) or die "Cannot open input file $myfile $1\n"; 
-                    open(FILE, ">$seq_file") or die "Cannot open output file $seq_file $1\n";
-                    while (<SEQ>) {
-                        s/^>\S+/>$acc_ver/;
-                        print FILE;
-                    }       
-                    close SEQ;
-                    close FILE;
-                    return(3)
-                }
-
-
-
-		#if ($cont =~ m/^>\S+\n[atgc\n]+$/) { #sequence is ok
-		    #create the sequence file
-		#    open (FILE,  ">$seq_file") or die "Can't open $seq_file\n";
-		#    print FILE $cont;
-		#    close FILE;
-		#    return(4);
-		#}
-		else {
-		    print "Failed to get sequence instead got $cont. Retrying....\n";
-		}
-	    }
-	}
+    if (-e $seq_file) {
+      $seq = Hum::FastaFileIO->new_DNA_IO($seq_file)->read_one_sequence;
     }
+    
+		if ($seq and $seq->name eq $acc_ver) {
+      return 2;
+    
+		} else {
+      # There is no file stored in the current directory, so we try to 
+      # fetch the current sequence from the Sanger submission system.
+      ### my $acc = $acc_ver;
+	    ### $acc =~ s/^(\S+)\.(\d+)$/$1/;
+      my ($acc, $sv) = ($acc_ver =~ /^(\S+)\.(\d+)$/);  
+
+      # this should be the only thing necessary now
+      my $inf = Hum::SequenceInfo->sanger_sequence_get($acc);
+      unless ($inf->sequence_version == $sv) {
+        printf STDERR "Current Sanger version of '%s' is '%d' not '%d'\n",
+                    $acc, $inf->sequence_version, $sv;
+        return;
+      }
+      $seq = $inf->Sequence;
+      Hum::FastaFileIO->new_DNA_IO("> $seq_file")->write_sequences($seq);
+
+      return 3;
+    }
+
+
+
+##      # this should be the only thing necessary now
+##      my $inf  = Hum::SequenceInfo->fetch_by_accession_sv($acc, $ver);
+##      my $embl = $inf->get_EMBL_entry;
+####      my $seq    = $embl->hum_sequence;
+##      my $seq    = $embl->bio_seq;
+####      my $header = $acc_ver;
+##
+##      my $outFile = Bio::SeqIO->new(-file => ">$seq_file" , '-format' => 'FASTA');
+##
+####      print $outFile $seq_obj;
+####    while ( my $seq = $in->next_seq() ) {
+##			my $success = $outFile->write_seq($seq);
+####    }
+##
+##      return(3, $success)
+##
+##		}
+  }
 }
