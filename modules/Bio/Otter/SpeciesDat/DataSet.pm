4c9c5f00a38ba24e6b8ce8bb46f3e04b6b18af21
jh13 <jh13>
2011-05-05 13:32:28 +0000

    MFetcher.pm: move the database adaptor methods to SpeciesDat/DataSet.pm.

--- modules/Bio/Otter/SpeciesDat/DataSet.pm
+++ modules/Bio/Otter/SpeciesDat/DataSet.pm
@@ -77,6 +77,106 @@ sub _otter_dba {
     return $odba;
 }
 
+sub default_assembly {
+    my ($self, $dba) = @_;
+
+    my ($asm_def) = @{ $dba->get_MetaContainer()->list_value_by_key('assembly.default') };
+
+    return $asm_def || 'UNKNOWN';
+}
+
+sub pipeline_dba {
+    my ($self) = @_;
+    return $self->satellite_dba('pipeline_db_head');
+}
+
+sub satellite_dba {
+    my ($self, $metakey) = @_;
+
+    # check for a cached dba
+    my $dba_cached = $self->{_sdba}{$metakey};
+    return $dba_cached if $dba_cached;
+
+    # get and check the options
+    my $options = $self->satellite_dba_options($metakey);
+    die "metakey '$metakey' is not defined" unless $options;
+
+    # create the adaptor
+    my $dba = $self->satellite_dba_make($metakey, "Bio::EnsEMBL::DBSQL::DBAdaptor", $options);
+
+    # re-bless if necessary
+    if (lc($self->default_assembly($dba)) eq 'otter') {
+        require Bio::Vega::DBSQL::DBAdaptor;
+        bless $dba,'Bio::Vega::DBSQL::DBAdaptor';
+    }
+
+    # create the variation database (if there is one)
+    my $vdba = $self->variation_satellite_dba_make("${metakey}_variation");
+    $vdba->dnadb($dba) if $vdba;
+
+    return $dba;
+}
+
+sub variation_satellite_dba_make {
+    my ($self, $metakey) = @_;
+
+    # check for a cached dba
+    my $dba = $self->{_sdba}{$metakey};
+    return $dba if $dba;
+
+    # get and check the options
+    my $options = $self->satellite_dba_options($metakey);
+    return unless $options;
+
+    # create the adaptor
+    $dba = $self->satellite_dba_make($metakey, "Bio::EnsEMBL::Variation::DBSQL::DBAdaptor", $options);
+
+    return $dba;
+}
+
+sub satellite_dba_make {
+    my ($self, $metakey, $adaptor_class, $options) = @_;
+
+    my @options;
+    {
+        ## no critic(BuiltinFunctions::ProhibitStringyEval)
+        @options = eval $options;
+    }
+    die "Error evaluating '$options' : $@" if $@;
+
+    my %anycase_options = (
+         -group     => $metakey,
+         -species   => $self->name,
+        @options,
+    );
+
+    my %uppercased_options = ();
+    while( my ($k,$v) = each %anycase_options) {
+        $uppercased_options{uc($k)} = $v;
+    }
+
+    eval "require $adaptor_class" ## no critic (BuiltinFunctions::ProhibitStringyEval)
+        or die "'require $adaptor_class' failed";
+    my $dba = $adaptor_class->new(%uppercased_options);
+    die "Couldn't connect to '$metakey' satellite db"
+        unless $dba;
+
+    warn "... with parameters: ".join(', ', map { "$_=".$uppercased_options{$_} } keys %uppercased_options )."\n";
+
+    $self->{_sdba}{$metakey} = $dba;
+
+    return $dba;
+}
+
+sub satellite_dba_options {
+    my ($self, $metakey) = @_;
+
+    my $meta_container = $self->otter_dba->get_MetaContainer;
+    my ($options) = @{ $meta_container->list_value_by_key($metakey) };
+
+    return $options;
+}
+
 1;
 
 __END__
