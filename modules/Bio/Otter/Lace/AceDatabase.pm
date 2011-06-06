27e87d996d7a24a2330aa26e6fbb072c8d6b04a1
Jeremy Henty <jh13@sanger.ac.uk>
2011-06-06 12:20:02 +0100

    Lace/Slice.pm: sub zmap_config: return a hash of config stanzas.

--- modules/Bio/Otter/Lace/AceDatabase.pm
+++ modules/Bio/Otter/Lace/AceDatabase.pm
@@ -331,14 +331,16 @@ sub zmap_config {
     my $pfetch_url = $self->Client->pfetch_url;
 
     my $hash = {
-        'sources'         => $sources,
-        'show-mainwindow' => ( $show_mainwindow ? 'true' : 'false' ),
-        'cookie-jar'      => $ENV{'OTTERLACE_COOKIE_JAR'},
-        'script-dir'      => $self->script_dir,
-        'pfetch-mode'     => ( $pfetch_www ? 'http' : 'pipe' ),
-        'pfetch'          => ( $pfetch_www ? $pfetch_url : 'pfetch' ),
-        'xremote-debug'   => $ZMAP_DEBUG ? 'true' : 'false',
-        %{$self->smart_slice->zmap_config},
+        'ZMap' => [
+            'sources'         => $sources,
+            'show-mainwindow' => ( $show_mainwindow ? 'true' : 'false' ),
+            'cookie-jar'      => $ENV{'OTTERLACE_COOKIE_JAR'},
+            'script-dir'      => $self->script_dir,
+            'pfetch-mode'     => ( $pfetch_www ? 'http' : 'pipe' ),
+            'pfetch'          => ( $pfetch_www ? $pfetch_url : 'pfetch' ),
+            'xremote-debug'   => $ZMAP_DEBUG ? 'true' : 'false',
+            %{$self->smart_slice->zmap_config},
+            ],
     };
 
     return $hash;
