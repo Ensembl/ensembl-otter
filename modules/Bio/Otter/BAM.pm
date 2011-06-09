c8a39f69a9775b712849c2df451a3083b1c39290
Jeremy Henty <jh13@sanger.ac.uk>
2011-06-09 09:41:16 +0100

    Bio/Otter/BAM.pm: add.

---
+++ modules/Bio/Otter/BAM.pm
@@ -0,0 +1,56 @@
+
+### Bio::Otter::BAM
+
+package Bio::Otter::BAM;
+
+use strict;
+use warnings;
+
+use Carp;
+
+my @keys = qw( description file csver );
+
+sub new {
+    my ($pkg, $name, $config) = @_;
+
+    for (@keys) {
+        confess "missing BAM configuration parameter: $_"
+            unless defined $config->{$_};
+    }
+
+    my $self = { name => $name, %{$config} };
+    bless $self, $pkg;
+
+    return $self;
+}
+
+sub name {
+    my ($self) = @_;
+    return $self->{name};
+}
+
+sub description {
+    my ($self) = @_;
+    return $self->{description};
+}
+
+sub file {
+    my ($self) = @_;
+    return $self->{file};
+}
+
+sub csver {
+    my ($self) = @_;
+    return $self->{csver};
+}
+
+1;
+
+__END__
+
+=head1 NAME - Bio::Otter::BAM
+
+=head1 AUTHOR
+
+Ana Code B<email> anacode@sanger.ac.uk
+
