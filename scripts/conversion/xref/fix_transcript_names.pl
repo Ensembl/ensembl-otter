d0f30995e8f579458eee196540c1f5350def3948
Stephen Trevanion <st3@sanger.ac.uk>
2011-06-16 11:02:52 +0100

    modified to support updating of transcript xrefs after gene based ones have already been added, ie a replacement for the confusing update_transcript_names.pl Also now works independently of modules in VegaCuration

--- scripts/conversion/xref/fix_transcript_names.pl
+++ scripts/conversion/xref/fix_transcript_names.pl
@@ -2,7 +2,7 @@
 
 =head1 NAME
 
-fix_transcript_names.pl - update transcript names to gene based one
+fix_transcript_names.pl - update transcript names to gene based ones, patch identical ones
 
 =head1 SYNOPSIS
 
@@ -21,27 +21,21 @@ General options:
     --logpath=PATH                      write logfile to PATH (default: .)
     --logappend, --log_append           append to logfile (default: truncate)
     --prune=0|1                         remove changes from previous runs of this script
-    -v, --verbose                       verbose logging (default: false)
-    -i, --interactive=0|1               run script interactively (default: true)
-    -n, --dry_run, --dry=0|1            don't write results to database
-    -h, --help, -?                      print help (this message)
+
+    --update                            update existing transcript names where gene names have changed
 
 =head1 DESCRIPTION
 
-This script updates vega transcript xrefs from the original clone-name
-based transcript names to gene-name based ones.
+This script updates vega transcript xrefs. When run with [update] it checks transcript
+names against gene names and alters any that are not in sync. It's use in this mode is where
+gene-based transcript names already exist but are now known to be wrong. This step will not
+be needed for each release.
 
-If the option to fix duplicated names is chosen, then where the above results
-in identical names for transcripts from the same gene, then the transcripts are
-numbered incrementaly after ordering from the longest coding to the shortest
-non-coding. If any of these identical names are from transcripts that have not
-been seen before then the ID is dumped to file (new_fragmented_gene_list.txt).
-These should be reported to Havana for double
-checking they are not meant to be fragmented (see below). Using the verbose option
-will also report on non-havana loci.
-
-See vega_data_curation.txt for further details on using this script and getting usefull
-reports from the log file
+The more common use of this is script is to update transcript names from the original clone-name
+based ones to gene-name based ones. This is done by running it without the [update] option. If
+the option to fix duplicated names is chosen, then where the above patching of names results
+in identical names for transcripts from the same gene, then the transcripts are numbered
+incrementaly after ordering from the longest coding to the shortest non-coding.
 
 The -prune option restores the data to the stage before this script was first run.
 
@@ -85,8 +79,13 @@ my $support = new Bio::EnsEMBL::Utils::VegaCuration::Transcript($SERVERROOT);
 
 # parse options
 $support->parse_common_options(@_);
-$support->parse_extra_options('prune=s');
-$support->allowed_params($support->get_common_params,'prune');
+$support->parse_extra_options(
+  'prune=s',
+  'update');
+$support->allowed_params(
+  $support->get_common_params,
+  'prune',
+  'update');
 
 if ($support->param('help') or $support->error) {
   warn $support->error if $support->error;
@@ -99,45 +98,41 @@ $support->confirm_params;
 # get log filehandle and print heading and parameters to logfile
 $support->init_log;
 
-#set filehandle for logging of gene IDs
-#new genes
-my $n_flist_fh = $support->filehandle('>', $support->param('logpath').'/new_fragmented_gene_list.txt');
-
-#get list of IDs that have previously been sent to annotators
-my $seen_genes = $support->get_havana_fragmented_loci_comments;
-
 # connect to database and get adaptors
 my $dba = $support->get_database('ensembl');
 my $sa  = $dba->get_SliceAdaptor;
 my $aa  = $dba->get_AttributeAdaptor;
 my $dbh = $dba->dbc->db_handle;
-
-# make a backup tables the first time the script is run
-my $dbname = $support->param('dbname');
-my @tables = qw(transcript xref transcript_attrib gene_attrib);
-my %tabs;
-map { $_ =~ s/`//g; $_ =~ s/$dbname.//g; $tabs{$_} += 1; } $dbh->tables;
-if (! exists ($tabs{'backup_ftn_transcript'})) {
-  foreach my $table (@tables) {
-    my $t = 'backup_ftn_'.$table;
-    $support->log("Creating backup of $table\n\n");
-    $dbh->do("CREATE table $t SELECT * FROM $table");
-  }
-}
-
-#are duplicate transcript names going to be fixed later on ?
 my $fix_names = 0;
-if ($support->user_proceed("\nDo you want to check and fix duplicated transcript names?") ) {
-  $fix_names = 1;
-}
 
-if ($support->param('prune')
-      && $support->user_proceed("\nDo you want to undo changes from previous runs of this script?")) {
-  $support->log("Undoing changes from previous runs of this script...\n\n");
-  foreach my $table (@tables) {
-    my $t = 'backup_ftn_'.$table;
-    $dbh->do("DELETE from $table");
-    $dbh->do("INSERT into $table SELECT * FROM $t");
+my $dbname = $support->param('dbname');
+my $n_flist_fh;
+if (! $support->param('update')) {
+
+  #are duplicate transcript names going to be fixed later on ?
+  if ($support->user_proceed("\nDo you want to check and fix duplicated transcript names?") ) {
+    $fix_names = 1;
+  }
+
+  # make a backup tables the first time the script is run
+  my @tables = qw(transcript xref transcript_attrib gene_attrib);
+  my %tabs;
+  map { $_ =~ s/`//g; $_ =~ s/$dbname.//g; $tabs{$_} += 1; } $dbh->tables;
+  if (! exists ($tabs{'backup_ftn_transcript'})) {
+    foreach my $table (@tables) {
+      my $t = 'backup_ftn_'.$table;
+      $support->log("Creating backup of $table\n\n");
+      $dbh->do("CREATE table $t SELECT * FROM $table");
+    }
+  }
+
+  if ($support->param('prune') && $support->user_proceed("\nDo you want to undo changes from previous runs of this script?")) {
+    $support->log("Undoing changes from previous runs of this script...\n\n");
+    foreach my $table (@tables) {
+      my $t = 'backup_ftn_'.$table;
+      $dbh->do("DELETE from $table");
+      $dbh->do("INSERT into $table SELECT * FROM $t");
+    }
   }
 }
 
@@ -147,27 +142,27 @@ foreach my $chr ($support->sort_chromosomes) {
   my $chrom = $sa->fetch_by_region('toplevel', $chr);
  GENE:
   foreach my $gene (@{$chrom->get_all_Genes()}) {
-    
     my $gsi    = $gene->stable_id;
     my $transnames;
     my %seen_names;
     my %transcripts;
-    my $g_name = $gene->display_xref->display_id;
+    my $g_name = $gene->get_all_Attributes('name')->[0]->value;#display_xref->display_id;
     my $source = $gene->source;
 
-    $support->log("\n$g_name ($gsi)\n");
+    $support->log_verbose("\n$g_name ($gsi)\n");
 
     #check for identical names in loutre
     foreach my $trans (@{$gene->get_all_Transcripts()}) {
       my $t_name = $trans->get_all_Attributes('name')->[0]->value;
- #     $t_name = $trans->display_xref->display_id;
-      #remove unexpected extensions but report them for fixing (should be caught now)
+      $t_name = $trans->display_xref->display_id if ($support->param('update'));
+
+      #remove unexpected extensions but report them for fixing (should be caught by QC now but leave it here just in case)
       my $base_name = $t_name;
       if ( ($base_name =~ s/-\d{1,2}$//)
-	     || ($base_name =~ s/__\d{1,2}$//)
-	       || ($base_name =~ s/__\d{1,2}$//)
-		 || ($base_name =~ s/_\d$//)
-	           || ($base_name =~ s/\.\d+$//)) {
+	|| ($base_name =~ s/__\d{1,2}$//)
+        || ($base_name =~ s/__\d{1,2}$//)
+        || ($base_name =~ s/_\d$//)
+	|| ($base_name =~ s/\.\d+$//)) {
 	unless ( ($support->param('dbname') =~ /rerio/) && ($chr eq 'AB')) {
 	  $support->log_warning("UNEXPECTED transcript name $t_name\n");
 	}
@@ -196,30 +191,31 @@ foreach my $chr ($support->sort_chromosomes) {
 	push @{$transnames->{$new_name}}, "$t_name|$tsi";
 	next if ($new_name eq $t_name);
 	
-	#store a transcript attrib for the old name as long as it's not just a case change
-	my $attrib = [
-	  Bio::EnsEMBL::Attribute->new(
-	    -CODE => 'synonym',
-	    -NAME => 'Synonym',
-	    -DESCRIPTION => 'Synonymous names',
-	    -VALUE => $t_name,
-	  )];
-	unless (lc($t_name) eq lc($new_name)) {
-	  if (! $support->param('dry_run')) {
-	    $aa->store_on_Transcript($t_dbID, $attrib);
-	  }
+        $c1++;
+        if (! $support->param('update')) {
+          #store a transcript attrib for the old name as long as it's not just a case change
+          my $attrib = [
+            Bio::EnsEMBL::Attribute->new(
+              -CODE => 'synonym',
+              -NAME => 'Synonym',
+              -DESCRIPTION => 'Synonymous names',
+              -VALUE => $t_name,
+            )];
+          unless (lc($t_name) eq lc($new_name)) {
+            if (! $support->param('dry_run')) {
+              $aa->store_on_Transcript($t_dbID, $attrib);
+            }
 	  $support->log_verbose("Stored synonym for old name ($t_name) for transcript $tsi\n",2);
+          }
 	}
-	$c1++;
-	
+
 	#update xref for new transcript name
 	if (! $support->param('dry_run')) {
-	  $dbh->do(qq(
-                        UPDATE  xref x, external_db ed
-                        SET     x.display_label = "$new_name"
-                        WHERE   dbprimary_acc = "$tsi"
-                        AND     x.external_db_id = ed.external_db_id
-                        AND     ed.db_name = "Vega_transcript"
+	  $dbh->do(qq(UPDATE xref x, external_db ed
+                         SET x.display_label = "$new_name"
+                       WHERE dbprimary_acc = "$tsi"
+                         AND x.external_db_id = ed.external_db_id
+                         AND ed.db_name = "Vega_transcript"
                     ));
 	}
 	$support->log(sprintf("%-20s%-3s%-20s", "$t_name", "--->", " $new_name")."\n", 1);
@@ -233,17 +229,95 @@ foreach my $chr ($support->sort_chromosomes) {
 
     #if there are duplicated names in Vega then patch, add proper remark if they are fragmented
     if ( (grep { scalar(@{$transnames->{$_}}) > 1 } keys %{$transnames}) && $fix_names) {
-      my $patched;
-      ($patched,$c2,$c2) = $support->check_remarks_and_update_names($gene,$c2,$c3);
-      if ($patched) {
-	unless ( $seen_genes->{$gsi} eq 'OK') {
-	  #distinguish between overlaping and non-overlapping genes for reporting
-	  $support->check_names_and_overlap($transnames,$gene,$n_flist_fh);
-	}
-      }
+      ($c2,$c3) = &check_remarks_and_update_names($gene,$c2,$c3);
     }
   }
 }
-$support->log("Done updating xrefs for $c1 transcripts\n");
-$support->log("Identified $c3 transcripts from $c2 genes as updatable.\n");
+
+$support->log("\nDone updating xrefs for $c1 transcripts\n");
+$support->log("\nIdentified $c3 transcripts from $c2 genes as updatable.\n") if $fix_names;
 $support->finish_log;
+
+
+
+=head2 check_remarks_and_update names
+
+   Arg[1]     : B::E::Gene (with potentially duplicated transcript names)
+   Arg[2]     : counter 1 (no. of patched genes)
+   Arg[3]     : counter 2 (no. of patched transcripts)
+   Example    : $support->update_names($gene,\$c1,\$c2)
+   Description: - checks remarks and patches transcripts with identical names according to
+                CDS and length
+   Returntype : true | false (depending on whether patched or not), counter1, counter2
+
+=cut
+
+sub check_remarks_and_update_names {
+  my ($gene,$gene_c,$trans_c) = @_;
+  my $action = ($support->param('dry_run')) ? 'Would add' : 'Added';
+  my $aa  = $gene->adaptor->db->get_AttributeAdaptor;
+  my $dbh = $gene->adaptor->db->dbc->db_handle;
+  my $gsi    = $gene->stable_id;
+  my $gid    = $gene->dbID;
+  my $g_name;
+
+  eval {
+    $g_name = $gene->display_xref->display_id;
+  };	
+  if ($@) {
+    $g_name = $gene->get_all_Attributes('name')->[0]->value;
+  }
+
+  #get existing gene remarks
+  my $remarks = [ map {$_->value} @{$gene->get_all_Attributes('remark')} ];
+
+  #shout if there is a remark to identify this as being fragmented locus
+  if ( ! grep {$_ eq 'fragmented locus' } @$remarks) {
+    $support->log_warning("Gene $g_name has duplicate transcript names but no fragmented_locus remark, this shold be added\n");
+  }
+
+  ##patch transcript names according to length and CDS
+  $gene_c++;
+
+  #separate coding and non_coding transcripts
+  my $coding_trans = [];
+  my $noncoding_trans = [];
+  foreach my $trans ( @{$gene->get_all_Transcripts()} ) {
+    if ($trans->translate) {
+      push @$coding_trans, $trans;
+    }
+    else {
+      push @$noncoding_trans, $trans;
+    }
+  }
+
+  #sort transcripts coding > non-coding, then on length
+  my $c = 0;
+  $support->log("\nPatching names according to CDS and length:\n",1);
+  foreach my $array_ref ($coding_trans,$noncoding_trans) {
+    foreach my $trans ( sort { $b->length <=> $a->length } @$array_ref ) {
+      $trans_c++;
+      my $tsi = $trans->stable_id;
+      my $t_name;
+      eval {
+	$t_name = $trans->display_xref->display_id;
+      };	
+      if ($@) {
+	$t_name = $trans->get_all_Attributes('name')->[0]->value;
+      }
+      $c++;
+      my $ext = sprintf("%03d", $c);
+      my $new_name = $g_name.'-'.$ext;
+      $support->log(sprintf("%-20s%-3s%-20s", "$t_name ", "-->", "$new_name")."\n",1);
+      if (! $support->param('dry_run')) {
+	# update transcript display xref
+	$dbh->do(qq(UPDATE xref x, external_db edb
+                       SET x.display_label  = "$new_name"
+                     WHERE x.external_db_id = edb.external_db_id
+                       AND x.dbprimary_acc  = "$tsi"
+                       AND edb.db_name      = "Vega_transcript"));
+      }
+    }
+  }
+  return ($gene_c,$trans_c);
+}
