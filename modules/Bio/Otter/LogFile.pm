d5dd84db88d31eb68eeaa2a223521c0a59358397
Matthew Astley <mca@sanger.ac.uk>
2011-09-08 16:51:13 +0100

    only ignore sigterm in logger child.
    bugfix ae71f5cfc2

--- modules/Bio/Otter/LogFile.pm
+++ modules/Bio/Otter/LogFile.pm
@@ -26,10 +26,6 @@ sub make_log {
     # Unbuffer STDOUT
     STDOUT->autoflush(1);
 
-    # Parent will try to kill us if it finishes cleanly, but we may
-    # need to wait and write out the final logs.
-    $SIG{'TERM'} = 'IGNORE';
-
     if (my $pid = open(STDOUT, "|-")) {
         # Send parent's STDERR to the same place as STDOUT.
         open STDERR, ">&STDOUT" or confess "Can't redirect STDERR to STDOUT";
@@ -40,6 +36,10 @@ sub make_log {
         
         # Unbuffer logfile
         $log->autoflush(1);
+
+        # Parent will try to kill us if it finishes cleanly, but we
+        # may need to wait and write out the final logs.
+        $SIG{'TERM'} = 'IGNORE';
 
         # Child filters output from parent
         while (<STDIN>) { ## no critic(InputOutput::ProhibitExplicitStdin)
