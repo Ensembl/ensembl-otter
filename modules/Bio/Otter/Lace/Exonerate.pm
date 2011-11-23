25b9b407bcef11a79214d5fac878213b9c0a0f5a
Jeremy Henty <jh13@sanger.ac.uk>
2011-11-23 09:51:33 +0000

    Lace/Exonerate.pm: quieten "perlcritic --harsh".

--- modules/Bio/Otter/Lace/Exonerate.pm
+++ modules/Bio/Otter/Lace/Exonerate.pm
@@ -19,7 +19,10 @@ use File::Basename;
 use Bio::Otter::Lace::Exonerate::Config::General;
 use Bio::Otter::Lace::Exonerate::Config::Blast;
 use Bio::EnsEMBL::Analysis::Tools::BlastDBTracking;
-Bio::EnsEMBL::Analysis::Tools::BlastDBTracking->_Fake_version_for_otf(1);
+{
+    ## no critic(Subroutines::ProtectPrivateSubs)
+    Bio::EnsEMBL::Analysis::Tools::BlastDBTracking->_Fake_version_for_otf(1);
+}
 
 use Bio::Seq;
 use Hum::Ace::Method;
