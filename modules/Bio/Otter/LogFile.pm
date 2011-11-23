48182f56ba680e0a35801070f842de6f69c4322a
Jeremy Henty <jh13@sanger.ac.uk>
2011-11-23 16:52:44 +0000

    Otter/LogFile.pm: sub set_prefix_sub(): remove and inline the default prefix subroutine.

--- modules/Bio/Otter/LogFile.pm
+++ modules/Bio/Otter/LogFile.pm
@@ -8,15 +8,6 @@ use warnings;
 use Carp;
 use IO::Handle;
 
-my $prefix_sub = sub {
-    return scalar(localtime) . "  ";
-    };
-
-sub set_prefix_sub {
-    $prefix_sub = shift;
-    return;
-}
-
 my $file;
 sub make_log {
     confess "Already logging to '$file'" if $file;
@@ -44,7 +35,7 @@ sub make_log {
         # Child filters output from parent
         while (<STDIN>) { ## no critic(InputOutput::ProhibitExplicitStdin)
             print STDERR $_;    # Still print to STDERR
-            print $log $prefix_sub->(), $_;
+            printf $log "%s  %s", scalar(localtime), $_;
         }
         close $log;
         exit;   # Child must exit here!
