226474ce2a445c5861ca2997e674938eb93cb58e
Michael Gray <mg13@sanger.ac.uk>
2011-12-08 14:13:27 +0000

    Factor out TargetSeq and provide role for FastaFile.

--- modules/Bio/Otter/Lace/OnTheFly/Aligner.pm
+++ modules/Bio/Otter/Lace/OnTheFly/Aligner.pm
@@ -3,9 +3,9 @@ package Bio::Otter::Lace::OnTheFly::Aligner;
 use namespace::autoclean;
 use Moose;
 
-has type         => ( is => 'ro', isa => 'Str',                     required => 1 );
-has seqs         => ( is => 'ro', isa => 'ArrayRef[Hum::Sequence]', required => 1 );
-has target_fasta => ( is => 'ro', isa => 'File::Temp',              required => 1 );
+has type   => ( is => 'ro', isa => 'Str',                                   required => 1 );
+has seqs   => ( is => 'ro', isa => 'ArrayRef[Hum::Sequence]',               required => 1 );
+has target => ( is => 'ro', isa => 'Bio::Otter::Lace::OnTheFly::TargetSeq', required => 1 );
 
 1;
 
---
+++ modules/Bio/Otter/Lace/OnTheFly/TargetSeq.pm
@@ -0,0 +1,47 @@
+package Bio::Otter::Lace::OnTheFly::TargetSeq;
+
+use namespace::autoclean;
+use Moose;
+
+has 'full_seq'     => ( is => 'ro', isa => 'Hum::Sequence', required => 1 );
+
+has 'start'        => ( is => 'ro', isa => 'Int', lazy => 1, builder => '_build_start' );
+has 'end'          => ( is => 'ro', isa => 'Int', lazy => 1, builder => '_build_end' );
+
+has 'target_seq'   => ( is => 'ro', isa => 'Hum::Sequence',
+                        lazy => 1, builder => '_build_target_seq', init_arg => undef );
+
+has fasta_description => ( is => 'ro', isa => 'Str', default => 'target' );
+
+sub fasta_sequences {
+    my $self = shift;
+    return $self->target_seq;
+}
+
+with 'Bio::Otter::Lace::OnTheFly::FastaFile';
+
+sub _build_start {              # could use 'default' for this, but for symmetry with _build_end:
+    return 1;
+}
+
+sub _build_end {
+    my $self = shift;
+    return $self->full_seq->sequence_length;
+}
+
+sub _build_target_seq {
+    my $self = shift;
+    my $target_seq = $self->full_seq->sub_sequence($self->start, $self->end);
+    $target_seq->name($self->full_seq->name);
+    return $target_seq;
+}
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Ana Code B<email> anacode@sanger.ac.uk
+
+# EOF
