dcf03b6db5dd2f7a9a417c0ce38231a8d815f988
Michael Gray <mg13@sanger.ac.uk>
2011-12-08 14:13:27 +0000

    Factor out TargetSeq and provide role for FastaFile.

--- modules/Bio/Otter/Lace/OnTheFly.pm
+++ modules/Bio/Otter/Lace/OnTheFly.pm
@@ -3,10 +3,9 @@ package Bio::Otter::Lace::OnTheFly;
 use namespace::autoclean;
 use Moose;
 
-use File::Temp;
-
 use Bio::Otter::Lace::OnTheFly::Aligner;
 use Bio::Otter::Lace::OnTheFly::QueryValidator;
+use Bio::Otter::Lace::OnTheFly::TargetSeq;
 
 has 'query_validator' => (
     is      => 'ro',
@@ -15,15 +14,29 @@ has 'query_validator' => (
     writer  => '_set_query_validator',
     );
 
-has target_seq           => ( is => 'ro', isa => 'Hum::Sequence', required => 1 );
-
-has target_fasta_file    => ( is => 'ro', isa => 'File::Temp',
-                              lazy => 1, builder => '_build_target_fasta_file', init_arg => undef );
+has 'target'          => (
+    is     => 'ro',
+    isa    => 'Bio::Otter::Lace::OnTheFly::TargetSeq',
+    handles => {
+        target_fasta_file => 'fasta_file',
+        target_start      => 'start',
+        target_end        => 'end',
+        target_seq        => 'target_seq',
+        },
+    writer => '_set_target',
+    );
 
 sub BUILD {
     my ( $self, $params ) = @_;
 
     $self->_set_query_validator(Bio::Otter::Lace::OnTheFly::QueryValidator->new($params));
+
+    my %target_params = ( full_seq => $params->{target_seq} );
+    $target_params{start} = $params->{target_start} if $params->{target_start};
+    $target_params{end}   = $params->{target_end} if $params->{target_end};
+
+    $self->_set_target( Bio::Otter::Lace::OnTheFly::TargetSeq->new(\%target_params) );
+
     return;
 }
 
@@ -33,30 +46,12 @@ sub aligners_for_each_type {
     my @aligners;
     foreach my $type ( $self->seq_types ) {
         push @aligners, Bio::Otter::Lace::OnTheFly::Aligner->new(
-            type         => $type,
-            seqs         => $self->seqs_for_type($type),
-            target_fasta => $self->target_fasta_file,
+            type   => $type,
+            seqs   => $self->seqs_for_type($type),
+            target => $self->target,
             );
     }
     return @aligners;
-}
-
-sub _build_target_fasta_file {
-    my $self = shift;
-
-    my $template = "otf_target_${$}_XXXXX";
-    my $file = File::Temp->new(
-        TEMPLATE => $template,
-        TMPDIR   => 1,
-        SUFFIX   => '.fa',
-        UNLINK   => 0,         # for now
-        );
-
-    my $ts_out  = Hum::FastaFileIO->new("> $file");
-    $ts_out->write_sequences( $self->target_seq );
-    $ts_out = undef;            # flush
-
-    return $file;
 }
 
 1;
