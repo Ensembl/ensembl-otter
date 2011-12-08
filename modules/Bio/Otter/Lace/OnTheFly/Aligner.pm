270f85c022e5635a63e32f0b23f4636e483c269c
Michael Gray <mg13@sanger.ac.uk>
2011-12-08 14:21:08 +0000

    Use FastFile role in B:O:L:OnTheFly::Aligner.

--- modules/Bio/Otter/Lace/OnTheFly/Aligner.pm
+++ modules/Bio/Otter/Lace/OnTheFly/Aligner.pm
@@ -7,6 +7,20 @@ has type   => ( is => 'ro', isa => 'Str',                                   requ
 has seqs   => ( is => 'ro', isa => 'ArrayRef[Hum::Sequence]',               required => 1 );
 has target => ( is => 'ro', isa => 'Bio::Otter::Lace::OnTheFly::TargetSeq', required => 1 );
 
+has fasta_description => ( is => 'ro', isa => 'Str', lazy => 1, builder => '_build_fasta_description' );
+
+sub _build_fasta_description {
+    my $self = shift;
+    return sprintf('query_%s', $self->type);
+}
+
+sub fasta_sequences {
+    my $self = shift;
+    return @{$self->seqs};
+}
+
+with 'Bio::Otter::Lace::OnTheFly::FastaFile';
+
 1;
 
 __END__
