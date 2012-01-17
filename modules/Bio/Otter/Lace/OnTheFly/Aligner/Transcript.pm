f3a7baf0bdc0072b27f92f2a159de9cefc5c1661
Michael Gray <mg13@sanger.ac.uk>
2012-01-17 13:39:22 +0000

    Comment out work-in-progress to silence perlcritic.

--- modules/Bio/Otter/Lace/OnTheFly/Aligner/Transcript.pm
+++ modules/Bio/Otter/Lace/OnTheFly/Aligner/Transcript.pm
@@ -20,34 +20,34 @@ around 'parse' => sub {
     return $basic->{raw};
 };
 
-sub _split_vulgar {
-    my ($self, $basic) = @_;
-
-    my $transcript = $self->transcript;
-    if ($transcript->strand == -1) {
-        warn "Can't handle reverse strand transcript yet";
-        return;
-    }
-
-    print "Considering transcript ", $transcript->start, " - ", $transcript->end, "\n";
-
-    my @exons = $transcript->get_all_Exons;
-    my @vulgar = ( @{$basic->{vulgar}} ); # make a copy we can consume
-
-    return unless @vulgar;
-
-    my @split;
-
-    my $vulgar_comp = shift @vulgar;
-    my $tsplice_curr = $basic->{t_start}; # current spliced target pos
-
-    EXON: foreach my $exon (@exons) {
-        my @exon_vulgar;
-        print "Considering exon ", $exon->start, " - ", $exon->end, "\n";
-    }
-
-    return;
-}
+# sub _split_vulgar {
+#     my ($self, $basic) = @_;
+# 
+#     my $transcript = $self->transcript;
+#     if ($transcript->strand == -1) {
+#         warn "Can't handle reverse strand transcript yet";
+#         return;
+#     }
+# 
+#     print "Considering transcript ", $transcript->start, " - ", $transcript->end, "\n";
+# 
+#     my @exons = $transcript->get_all_Exons;
+#     my @vulgar = ( @{$basic->{vulgar}} ); # make a copy we can consume
+# 
+#     return unless @vulgar;
+# 
+#     my @split;
+# 
+#     my $vulgar_comp = shift @vulgar;
+#     my $tsplice_curr = $basic->{t_start}; # current spliced target pos
+# 
+#     EXON: foreach my $exon (@exons) {
+#         my @exon_vulgar;
+#         print "Considering exon ", $exon->start, " - ", $exon->end, "\n";
+#     }
+# 
+#     return;
+# }
 
 1;
 
