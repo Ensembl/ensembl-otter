a5c73729edbee2e3447642ea247a956b366ea252
Stephen Trevanion <st3@sanger.ac.uk>
2012-02-03 12:10:54 +0000

    replacement for add_go_xrefs.pl (script renamed since it will work with Uniprot as well) This initial commit is for safety - script will parse input file but doesn't deal wiht duplicates correctly and doesn't add the xrefs

---
+++ scripts/conversion/xref/add_ebi_xrefs.pl
@@ -0,0 +1,402 @@
+#!/usr/bin/env perl
+
+=head1 NAME
+
+add_ebi_xrefs.pl - adds go and Uniprot xrefs to translations
+
+=head1 SYNOPSIS
+
+add_external_xrefs.pl [options]
+
+General options:
+    --conffile, --conf=FILE             read parameters from FILE
+                                        (default: conf/Conversion.ini)
+
+    --dbname, db_name=NAME              use database NAME
+    --host, --dbhost, --db_host=HOST    use database host HOST
+    --port, --dbport, --db_port=PORT    use database port PORT
+    --user, --dbuser, --db_user=USER    use database username USER
+    --pass, --dbpass, --db_pass=PASS    use database passwort PASS
+    --logfile, --log=FILE               log to FILE (default: *STDOUT)
+    --logpath=PATH                      write logfile to PATH (default: .)
+    --logappend, --log_append           append to logfile (default: truncate)
+    -v, --verbose                       verbose logging (default: false)
+    -i, --interactive                   run script interactively (default: true)
+    -n, --dry_run, --dry                don't write results to database
+    -h, --help, -?                      print help (this message)
+
+Specific options:
+
+    --gene_stable_id                    limit to stable IDs only
+    --chromosomes, --chr=LIST           only process LIST chromosomes
+    --gofile=FILE                       read GO input file
+    --prune                             reset to the state before running this
+                                        script
+
+=head1 DESCRIPTION
+
+This script parses a GOA input file to add GO and Uniprot xrefs to Vega translations
+
+Data comes from a file provided by Tony Sawford (tonys@ebi.ac.uk) from GOA:
+
+ftp://ftp.ebi.ac.uk/pub/contrib/goa/vega2goa.gz
+
+
+=head1 LICENCE
+
+This code is distributed under an Apache style licence:
+Please see http://www.ensembl.org/code_licence.html for details
+
+=head1 AUTHOR
+
+Steve Trevanion <st3@sanger.ac.uk>
+
+=head1 CONTACT
+
+Post questions to the EnsEMBL development list ensembl-dev@ebi.ac.uk
+
+=cut
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+use FindBin qw($Bin);
+use vars qw($SERVERROOT);
+use Storable;
+
+BEGIN {
+  $SERVERROOT = "$Bin/../../../..";
+  unshift(@INC, "$SERVERROOT/ensembl/modules");
+#  unshift(@INC, "$SERVERROOT/bioperl-live");
+}
+
+use Getopt::Long;
+use Pod::Usage;
+use Bio::EnsEMBL::Utils::ConversionSupport;
+use Bio::SeqIO::genbank;
+use Data::Dumper;
+
+$| = 1;
+
+our $support = new Bio::EnsEMBL::Utils::ConversionSupport($SERVERROOT);
+
+# parse options
+$support->parse_common_options(@_);
+$support->parse_extra_options(
+  'chromosomes|chr=s@',
+  'gene_stable_id|gsi=s@',
+  'ebifile=s',
+  'xrefformat=s',
+  'prune',
+);
+$support->allowed_params(
+  $support->get_common_params,
+  'chromosomes',
+  'gene_stable_id',
+  'ebifile',
+  'xrefformat',
+  'prune',
+);
+
+if ($support->param('help') or $support->error) {
+  warn $support->error if $support->error;
+  pod2usage(1);
+}
+
+$support->comma_to_list('chromosomes');
+$support->list_or_file('gene_stable_id');
+
+$support->check_required_params('ebifile','xrefformat');
+
+# ask user to confirm parameters to proceed
+$support->confirm_params;
+
+if ($support->param('xrefformat') !~ /uniprot|go/) {
+  $support->log_error("Please set xrefformat option to 'uniprot' or 'go'\n");
+}
+
+# get log filehandle and print heading and parameters to logfile
+$support->init_log;
+
+# connect to database and get adaptors
+my $dba = $support->get_database('ensembl');
+my $dbh = $dba->dbc->db_handle;
+my $sa  = $dba->get_SliceAdaptor();
+my $ga  = $dba->get_GeneAdaptor();
+my $tla = $dba->get_TranslationAdaptor();
+my $ea  = $dba->get_DBEntryAdaptor();
+
+my $xrefformat = $support->param('xrefformat');
+
+# delete previous xrefs if --prune option is used
+if ($support->param('prune') and $support->user_proceed('Would you really like to delete $xrefformat xrefs from previous runs of this script?')) {
+
+  $support->log("Deleting $xrefformat xrefs...\n");
+
+  my $num = $dba->dbc->do(qq(
+           DELETE x
+           FROM xref x, external_db ed
+           WHERE x.external_db_id = ed.external_db_id
+           AND ed.db_name = '$xrefformat'));
+  $support->log("Done deleting $num entries.\n");
+	
+  # object_xrefs
+  $support->log("Deleting orphan object_xrefs...\n");
+  $num = $dba->dbc->do(qq(
+           DELETE ox
+           FROM object_xref ox
+           LEFT JOIN xref x ON ox.xref_id = x.xref_id
+           WHERE x.xref_id IS NULL
+        ));
+  $support->log("Done deleting $num entries.\n");
+
+  if ($xrefformat eq 'go') {
+    # ontology_xrefs
+    $support->log("Deleting ontology_xrefs...\n");
+    $num = $dba->dbc->do(qq(DELETE FROM ontology_xref));
+    $support->log("Done deleting $num entries.\n");
+  }
+}
+
+my %gene_stable_ids = map { $_, 1 }  $support->param('gene_stable_id');
+my $chr_length = $support->get_chrlength($dba,'','',1);
+my @chr_sorted = $support->sort_chromosomes($chr_length);
+
+my $parsed_xrefs = {};
+my $xref_file    = $SERVERROOT.'/'.$support->param('dbname')."-EBI-parsed_records.file";
+
+# read input files... either retrieve from disc
+
+if (-e $xref_file) {
+  if ($support->user_proceed("Read xref records from a previously saved files - $xref_file ?\n")) {
+    $parsed_xrefs = retrieve($xref_file);
+  }
+}
+
+#or parse and store to disc...
+if (! %$parsed_xrefs ) {
+  $support->log_stamped("Reading xref input files...\n");
+  &parse_go;
+  $support->log_stamped("Finished parsing xrefs, storing to file...\n");
+  store($parsed_xrefs,$xref_file);
+}
+
+if ($support->param('verbose')) {
+  $support->log("Parsed xrefs are ".Dumper($parsed_xrefs)."\n");
+  exit;
+}
+
+$support->log_stamped("Done.\n\n");
+
+# loop over chromosomes
+$support->log("Looping over chromosomes: @chr_sorted\n\n");
+my $overall_c = 0;
+
+foreach my $chr (@chr_sorted) {
+  my $chr_c = 0;
+  $support->log_stamped("> Chromosome $chr (".$chr_length->{$chr}."bp).\n\n");
+
+  # fetch genes from db
+  $support->log("Fetching genes...\n");
+  my $slice = $sa->fetch_by_region('toplevel', $chr);
+  my $genes = $ga->fetch_all_by_Slice($slice);
+  $support->log_stamped("Done fetching ".scalar @$genes." genes.\n\n");
+
+  my $gnum = 0;
+ GENE:
+  foreach my $gene (@$genes) {
+    my $gsi = $gene->stable_id;	
+
+    # filter to user-specified gene_stable_ids
+    if (scalar(keys(%gene_stable_ids))){
+      next GENE unless $gene_stable_ids{$gsi};
+    }
+
+    my $gene_name = $gene->display_xref->display_id;
+    $support->log_verbose("Studying $gene_name ($gsi)...\n");
+    $gnum++;
+
+    foreach my $trans (@{$gene->get_all_Transcripts()}) {
+      if (my $trl = $trans->translation() ) {
+	my $trlsi = $trl->stable_id;
+	my $trlid = $trl->dbID;
+	
+	$support->log_verbose("Searching for name $trlsi...\n",1);
+	if (my $links = $parsed_xrefs->{$trlsi}) {
+	  $support->log_verbose("Match found for $trlsi.\n",1);
+
+	MATCH:
+	  foreach my $match (@{$links->{'GO'}}) {
+	    $chr_c++;
+	    $overall_c++;
+	    my ($xid,$ev_type) = split /\|\|/, $match;
+
+	    unless ($xid) {
+	      $support->log_warning("Parsed file not in the correct format, please check\n");
+	      next MATCH;
+	    }
+            unless ($ev_type) {
+              $support->log_warning("No evidence type associated with $xid ($trlsi), please check input file\n");
+              $ev_type = '';
+            }
+	    $support->log_verbose("Creating new GO xref for $trlsi--$match.\n", 2);
+	    my $dbentry = Bio::EnsEMBL::OntologyXref->new(
+	      -primary_id => $xid,
+	      -display_id => $xid,
+	      -version    => 1,
+	      -info_type  => 'DEPENDENT',
+	      -dbname     => 'GO',
+	    );
+	    $dbentry->add_linkage_type($ev_type);
+
+	    $trl->add_DBEntry($dbentry);
+	    if (! $support->param('dry_run')) {
+	      if (my $dbID = $ea->store($dbentry, $trlid, 'translation', 1)) {
+		$support->log_verbose("Stored GO xref (display_id = $xid, evidence = $ev_type) for $trlsi\n", 3);
+	      }
+	      else {
+		$support->log_warning("Failed to store GO xref for $trlsi\n");
+	      }
+	    }
+	  }
+	}								
+	else {
+	  $support->log_verbose("No match found for $trlsi.\n",1);					
+	}
+      }
+    }
+  }
+  $support->log("$chr_c GO xrefs added for chromosome $chr\n");
+}
+
+$support->log("$overall_c GO xrefs found\n");
+
+$support->finish_log;
+
+
+=head2 parse_go
+
+=cut
+
+sub parse_go {
+  $support->log_stamped("Reading EBI file...\n", 1);
+  # read input file from GO
+  open (EBI, '<', $support->param('ebifile')) or $support->throw(
+    "Couldn't open ".$support->param('ebifile')." for reading: $!\n");
+#  my %evi_code;
+  my %source_dbs;
+  my $prev_tlsi;
+  my $rec;
+  while (<EBI>) {
+    next if $_ =~ /^#/;
+    my @fields = split /\t/, $_;
+    my $tlsi         = $fields[0]; # add GO and Uniprot xrefs to this *translation*
+
+#    next unless $tlsi eq 'OTTHUMP00000080295';
+
+    my $uniprot      = $fields[1]; # used for Uniprot xrefs
+    my $go_id        = $fields[2]; # self explanatory [= xref.display_label and xref.dbprimary_acc]
+    my $go_name      = $fields[4]; # ie description of the GO term [= xref.description]
+    my $ev_code      = $fields[5]; # type of evidence, eg IEA [= ontology_xref.linkage]
+    my ($source_db,$source_acc) = split ':', $fields[7]; # source, ie the group / database that supplied the annotation - needs another xref linked to from ontology_xref.source_xref_id
+
+#    $evi_code{$ev_code}++;
+#    my $go_aspect    = $fields[3]; # F(unction), P(rocess) or C(omponent) (not used)
+#    my $ref          = $fields[6]; # identifier of the source cited as the authority for attributing a GO term to a gene product (not used)
+#    $source_dbs{$source_db}++ if $fields[7] && $source_acc;
+#    my $assigned_by  = $fields[8]; # who made the connection (not used)
+
+    #sanity checks
+    chomp $go_id;
+    if ( ($tlsi !~ /^OTT[A-Z]{3}P/) || ( $go_id !~ /^GO:/) ) {
+      $support->log_error("Check format of input file ($tlsi -- $go_id)\n");
+    }
+
+    #go back and remove any redundant records
+    if ($prev_tlsi && ($tlsi ne $prev_tlsi)) {
+      &cleanup_and_store($rec,$prev_tlsi);
+      $rec = {};
+    }
+
+    if (! $rec->{$tlsi}{'uniprot'}{$uniprot}) {
+      push @{$rec->{$tlsi}{'uniprot'}{$uniprot}}, {
+        'pid'           => $uniprot,
+        'display_label' => $uniprot,
+      };
+    }
+    push @{$rec->{$tlsi}{'go'}{$go_id}}, {
+        'pid'       => $go_id,
+        'display_label' => $go_id,
+        'go_name'   => $go_name,
+        'ev_code'   => $ev_code,
+        'source_db' => $source_db,
+        'source_acc'=> $source_acc,
+      };
+
+    $prev_tlsi  = $tlsi;
+  }
+
+  &cleanup_and_store($rec,$prev_tlsi);
+
+  #  warn Dumper(\%evi_code);
+  warn Dumper(\%source_dbs);
+}
+
+sub cleanup_and_store {
+  my ($rec,$tlsi) = @_;
+
+  #remove records without a source if there are also ones with a source
+  while ( my ($go_id,$go_rec) = each (%{$rec->{$tlsi}{'go'}}) ) {
+    if (scalar(@$go_rec) > 1) {
+      my $source_found = 0;
+      foreach my $cit (@$go_rec) {
+        $source_found = 1 if $cit->{'source_db'};
+      }
+      if ($source_found) {
+        for (my $i; $i < scalar(@{$go_rec}); $i++) {
+          my $cit = $go_rec->[$i];
+          if (! $cit->{'source_db'}) {
+            $support->log_verbose("Removing non sourced go record for $tlsi ($go_id).\n");
+            splice(@{$go_rec}, $i, 1);
+            next;
+          }
+        }
+      }
+
+      #remove duplicate records from the same external source
+      my %sources;
+      for (my $i; $i < scalar(@{$go_rec}); $i++) {
+        my $cit = $go_rec->[$i];
+        if ($sources{$cit->{'source_db'}}) {
+          $support->log_verbose("Removing ".$cit->{'source_db'}." sourced GO record for $tlsi ($go_id) since one has already been seen.\n");
+          splice(@{$go_rec}, $i, 1);
+          next;
+        }
+        $sources{$cit->{'source_db'}}++;
+      }
+      #finally remove all but the first remaining record
+      if (scalar(@$go_rec) > 1) {
+        for (my $i; $i < scalar(@{$go_rec}); $i++) {
+          if ($i) {
+            $support->log_verbose("Removing all but one sourced GO record for $tlsi ($go_id).\n");
+            splice(@{$go_rec}, $i, 1);
+          }
+        }
+      }
+    }
+  }
+
+  $parsed_xrefs->{$tlsi} = $rec->{$tlsi};
+# warn Dumper($parsed_xrefs); exit;
+ #store $rec on $xref
+
+}
+# my $sfs = $obj->get_all_supporting_features;
+#  for (my $i = 0; $i < scalar(@{$sfs}); $i++) {
+#    my $sf = $sfs->[$i];
+#    if ($sf->dbID == $id) {
+#      $support->log("Removing $type supporting_feature ".$sf->display_id." (".$sf->dbID.") from ".$obj->stable_id."\n",3);
+#      splice(@{$sfs}, $i, 1);
+#    }
+#  }
