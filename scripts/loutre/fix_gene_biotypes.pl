a3128791952ec87397194d80adad98ae36d58bd8
Jeremy Henty <jh13@sanger.ac.uk>
2012-02-07 15:37:04 +0000

    fix_gene_biotypes.pl: remove commented-out code.

--- scripts/loutre/fix_gene_biotypes.pl
+++ scripts/loutre/fix_gene_biotypes.pl
@@ -148,11 +148,6 @@ sub set_biotype_status_from_transcripts {
     my $biotype = 'processed_transcript';
     if (my @pseudo = grep { /pseudo/i } keys %tsct_biotype) {
         if (@pseudo > 1) {
-            # throw(
-            #     sprintf "More than one pseudogene type in gene %s (%s)",
-            #         $self->get_all_Attributes('name')->[0]->value,
-            #         join(', ', @pseudo)
-            #     );
             die "More than one pseudogene type in gene\n";
         }
         else {
