4cad9c70937d32b4d6ad840811049938cdfeef17
Michael Gray <mg13@sanger.ac.uk>
2012-05-01 16:34:04 +0100

    Provide safe logging to screen via copy of STDOUT.

---
+++ modules/Bio/Otter/Log/Appender/SafeScreen.pm
@@ -0,0 +1,43 @@
+package Bio::Otter::Log::Appender::SafeScreen;
+
+use warnings;
+use strict;
+
+use Data::Dumper;
+
+use Log::Log4perl;
+use base qw(Log::Log4perl::Appender);
+
+sub new {
+    my($class, @options) = @_;
+
+    my $self = {
+        name   => "unknown name",
+        @options,
+    };
+
+    open(my $stdout_copy, ">&STDOUT") or die "Can't dup STDOUT";
+    $self->{handle} = $stdout_copy;
+
+    bless $self, $class;
+    return $self;
+}
+    
+sub log {
+    my($self, %params) = @_;
+
+    $self->{handle}->print($params{message});
+    return;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bio::Otter::Log::Appender::SafeScreen - log to a copy of STDOUT
+
+=cut
+
+# EOF
