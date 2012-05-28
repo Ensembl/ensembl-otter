831c63e16e80dc0591ad0eaf005780ff4caa238c
Stephen Trevanion <st3@sanger.ac.uk>
2012-05-28 11:35:14 +0100

    Add Uniprot and GO xrefs in one go

--- scripts/conversion/xref/add_ebi_xrefs.pl
+++ scripts/conversion/xref/add_ebi_xrefs.pl
@@ -79,7 +79,32 @@ use Data::Dumper;
 
 $| = 1;
 
-our $support = new Bio::EnsEMBL::Utils::ConversionSupport($SERVERROOT);
+my $support = new Bio::EnsEMBL::Utils::ConversionSupport($SERVERROOT);
+
+# priorites from GOA
+my %evidence_priorities = (
+  IDA => '1',   # inferred from direct assay
+  IMP => '2',   # inferred from mutant phenotype
+  IGI => '3',   # inferred from genetic interaction
+  IPI => '4',   # inferred from physical interaction
+  IEP => '5',   # inferred from expression pattern
+  EXP => '6',   # inferred from experiment
+  ISO => '7',   # inferred from sequence orthology
+  ISA => '7',   # inferred from sequence alignment
+  ISS => '8',   # inferred from sequence or structural similarity
+  IBD => '8',   # Inferred from Biological aspect of Descendant
+  IKR => '8',   # Inferred from Key Residues
+  IRD => '8',   # Inferred from Rapid Divergence
+  IBA => '8',   # Inferred from Biological aspect of Ancestor
+  IGC => '9',   # Inferred from Genomic Context
+  NAS => '10',  # non-traceable author statement
+  TAS => '11',  # traceable author statement
+  IC  => '12',   # inferred by curator
+  RCA => '13',  # reviewed computational annotation
+  IEA => '14', # inferred from electronic annotation
+  ND  => '15',  # no data
+);
+
 
 # parse options
 $support->parse_common_options(@_);
@@ -87,7 +112,6 @@ $support->parse_extra_options(
   'chromosomes|chr=s@',
   'gene_stable_id|gsi=s@',
   'ebifile=s',
-  'xrefformat=s',
   'prune',
 );
 $support->allowed_params(
@@ -95,7 +119,6 @@ $support->allowed_params(
   'chromosomes',
   'gene_stable_id',
   'ebifile',
-  'xrefformat',
   'prune',
 );
 
@@ -107,18 +130,13 @@ if ($support->param('help') or $support->error) {
 $support->comma_to_list('chromosomes');
 $support->list_or_file('gene_stable_id');
 
-$support->check_required_params('ebifile','xrefformat');
+$support->check_required_params('ebifile');
 
 # ask user to confirm parameters to proceed
 $support->confirm_params;
 
 # get log filehandle and print heading and parameters to logfile
 $support->init_log;
-
-my $xrefformat = $support->param('xrefformat');
-if ($xrefformat !~ /Uniprot\/SWISSPROT|GO/) {
-  $support->log_error("Please set xrefformat option to 'Uniprot/SWISSPROT' or 'GO'\n");
-}
 
 # connect to database and get adaptors
 my $dba = $support->get_database('ensembl');
@@ -128,19 +146,19 @@ my $ga  = $dba->get_GeneAdaptor();
 my $tla = $dba->get_TranslationAdaptor();
 my $ea  = $dba->get_DBEntryAdaptor();
 
-
 # delete previous xrefs if --prune option is used
-if ($support->param('prune') and $support->user_proceed("Would you really like to delete $xrefformat xrefs from previous runs of this script?")) {
+if ($support->param('prune') and $support->user_proceed("Would you really like to delete xrefs from previous runs of this script?")) {
 
-  $support->log("Deleting $xrefformat xrefs...\n");
+  my $external_dbtypes = join("', '", qw(Uniprot/SWISSPROT Quick_Go_Evidence Quick_Go GO));
 
+  $support->log("Deleting xrefs...\n");
   my $num = $dba->dbc->do(qq(
            DELETE x
            FROM xref x, external_db ed
            WHERE x.external_db_id = ed.external_db_id
-           AND ed.db_name = '$xrefformat'));
+           AND ed.db_name in ('$external_dbtypes')));
   $support->log("Done deleting $num entries.\n");
-	
+
   # object_xrefs
   $support->log("Deleting orphan object_xrefs...\n");
   $num = $dba->dbc->do(qq(
@@ -150,36 +168,16 @@ if ($support->param('prune') and $support->user_proceed("Would you really like t
            WHERE x.xref_id IS NULL
         ));
   $support->log("Done deleting $num entries.\n");
-  
-  if ($xrefformat eq 'GO') {
 
-    #GOA Evidence xrefs
-    $support->log("Deleting GO source object_xrefs...\n");
-    $num = $dba->dbc->do(qq(
-           DELETE x
-           FROM xref x, external_db edb
-           WHERE x.external_db_id = edb.external_db_id
-           AND edb.db_name = 'Quick_Go_Evidence'));
-    $support->log("Done deleting $num entries.\n");
-
-    #GOA xrefs
-    $num = $dba->dbc->do(qq(
-           DELETE x
-           FROM xref x, external_db edb
-           WHERE x.external_db_id = edb.external_db_id
-           AND edb.db_name = 'Quick_Go'));
-    $support->log("Done deleting $num entries.\n");
-
-    # ontology_xrefs
-    $support->log("Deleting ontology_xrefs...\n");
-    $num = $dba->dbc->do(qq(DELETE FROM ontology_xref));
-    $support->log("Done deleting $num entries.\n");
-  }
+  # ontology_xrefs
+  $support->log("Deleting ontology_xrefs...\n");
+  $num = $dba->dbc->do(qq(DELETE FROM ontology_xref));
+  $support->log("Done deleting $num entries.\n");
 }
 
 my %gene_stable_ids = map { $_, 1 }  $support->param('gene_stable_id');
 my $parsed_xrefs = {};
-my $xref_file    = $SERVERROOT.'/'.$support->param('dbname')."-EBI-parsed_records.file";
+my $xref_file    = $support->param('logpath').'/'.$support->param('dbname')."-EBI-parsed_records.file";
 
 # read input files... either retrieve from disc
 if (-e $xref_file) {
@@ -225,106 +223,111 @@ foreach my $chr (@chr_sorted) {
     }
 
     my $gene_name = $gene->display_xref->display_id;
-    $support->log_verbose("Studying $gene_name ($gsi)...\n");
+    $support->log_verbose("Studying $gene_name ($gsi)...\n",1);
 
+  TRANS:
     foreach my $trans (@{$gene->get_all_Transcripts()}) {
       if (my $trl = $trans->translation() ) {
         my $tsi   = $trans->stable_id;
 	my $trlsi = $trl->stable_id;
 	my $trlid = $trl->dbID;
-	my ($pid,$rec);
-	while ( ($pid,$rec) = each (%{$parsed_xrefs->{$trlsi}{$xrefformat}})) {
-	  $support->log_verbose("Match found for $tsi.\n",1);
-          unless ($rec->{'pid'}) {
-            $support->log_warning("Parsed file not in the correct format, please check\n");
-          }
-          $chr_c++;
-          $overall_c++;
 
-          my $dbentry;
+        unless ($parsed_xrefs->{$trlsi}) {
+          $support->log_verbose("No match found for $trlsi.\n",2);
+          next TRANS;
+        }
 
-          if ($xrefformat eq 'Uniprot/SWISSPROT') {
-            #uniprot is easy, just add an xref to the tranlsation
-            $support->log_verbose("Creating new $xrefformat xref for $trlsi $pid.\n", 2);
-            $dbentry = Bio::EnsEMBL::DBEntry->new(
-              -primary_id => $pid,
-              -display_id => $rec->{'display_label'},
-              -version    => 1,
-              -info_type  => 'DEPENDENT',
-              -dbname     => $xrefformat,
-            );
+        unless ( $parsed_xrefs->{$trlsi}{'Uniprot/SWISSPROT'} && $parsed_xrefs->{$trlsi}{'GO'}) {
+          $support->log_error("Parsed file not in the correct format, please check\n");
+        }
+
+        $chr_c++;
+        $overall_c++;
+
+        #uniprot is easy, just add an xref to the translation
+        my ($uniprot_pid,$uniprot_rec) = %{$parsed_xrefs->{$trlsi}{'Uniprot/SWISSPROT'}};
+        $support->log_verbose("Creating new Uniprot xref for $trlsi $uniprot_pid.\n", 2);
+        my $dbentry = Bio::EnsEMBL::DBEntry->new(
+          -primary_id => $uniprot_pid,
+          -display_id => $uniprot_rec->{'display_label'},
+          -version    => 1,
+          -info_type  => 'DEPENDENT',
+          -dbname     => 'Uniprot/SWISSPROT',
+        );
+        $trl->add_DBEntry($dbentry);
+        if (! $support->param('dry_run')) {
+          if (my $dbID = $ea->store($dbentry, $trlid, 'translation', 1)) {
+            $support->log_verbose("Stored Uniprot xref (display_id = $uniprot_pid, dbID = $dbID) for $trlsi\n", 3);
           }
           else {
-            #go requires an ontology xref and an xref (for Annotation source)
-            my $ev_type = $rec->{'ev_code'};
-            unless ($ev_type) {
-              $support->log_warning("No evidence type associated with $pid ($trlsi), please check input file\n");
-              $ev_type = '';
-            }
+            $support->log_warning("Failed to store Uniprot xref for $trlsi\n");
+          }
+        }
 
-            #add link to GOA
-            $dbentry = Bio::EnsEMBL::DBEntry->new(
-              -primary_id => $rec->{'uniprot_acc'},
-              -display_id => $rec->{'uniprot_acc'},
-              -version    => 1,
-              -info_type  => 'DEPENDENT',
-              -dbname     => 'Quick_Go',
-            );
-            $trl->add_DBEntry($dbentry);
-            if (! $support->param('dry_run')) {
-              if (my $dbID = $ea->store($dbentry, $trlid, 'translation', 1)) {
-                $support->log_verbose("Stored Quick_GO xref (display_id = $pid, dbID = $dbID, source_db = ".$rec->{'assigned_by'}." for $trlsi\n", 3);
-              }
-              else {
-                $support->log_warning("Failed to store Quick_GO xref for $trlsi\n");
-              }
-            }
-
-            #add GO xrefs
-            $support->log_verbose("Creating new $xrefformat xref for $trlsi $pid.\n", 2);
-            $dbentry = Bio::EnsEMBL::OntologyXref->new(
-              -primary_id  => $pid,
-              -display_id  => $rec->{'display_label'},
-              -version     => 1,
-              -info_type   => 'DEPENDENT',
-              -dbname      => $xrefformat,
-              -description => $rec->{'go_name'},
-            );
-
-            my $source_xref = Bio::EnsEMBL::DBEntry->new(
-              -primary_id  => $rec->{'uniprot_acc'},
-              -display_id  => $rec->{'assigned_by'},
-              -info_type   => 'DEPENDENT',
-              -dbname      => 'Quick_Go_Evidence',
-              -version => 1,
-              -info_text   => 'Quick_Go:'.$rec->{'assigned_by'},
-            );
-            $dbentry->add_linkage_type($ev_type,$source_xref);
-
-            ##what's significance of info_type ?
-
+        #go requires an ontology xref and an xref (for Annotation source)
+        while ( my ($go_pid,$go_rec) = each %{$parsed_xrefs->{$trlsi}{'GO'}}) {
+          my $ev_type = $go_rec->{'ev_code'};
+          unless ($ev_type) {
+            $support->log_warning("No evidence type associated with $go_pid ($trlsi), please check input file\n");
+            $ev_type = '';
           }
 
+          #add link to GOA
+          $support->log_verbose("Creating new Quick_GO xref for $trlsi $go_pid.\n", 2);
+          $dbentry = Bio::EnsEMBL::DBEntry->new(
+            -primary_id => $go_rec->{'uniprot_acc'},
+            -display_id => $go_rec->{'uniprot_acc'},
+            -version    => 1,
+            -info_type  => 'DEPENDENT',
+            -dbname     => 'Quick_Go',
+          );
           $trl->add_DBEntry($dbentry);
           if (! $support->param('dry_run')) {
             if (my $dbID = $ea->store($dbentry, $trlid, 'translation', 1)) {
-              $support->log_verbose("Stored $xrefformat xref (display_id = $pid, dbID = $dbID, source_db = ".$rec->{'assigned_by'}." for $trlsi\n", 3);
+              $support->log_verbose("Stored Quick_GO xref (display_id = $go_pid, dbID = $dbID, source_db = ".$go_rec->{'assigned_by'}." for $trlsi\n", 3);
+            }
+            else {
+              $support->log_warning("Failed to store Quick_GO xref for $trlsi\n");
+            }
+          }
+
+          #add GO xrefs
+          $support->log_verbose("Creating new GO xref for $trlsi $go_pid.\n", 2);
+          $dbentry = Bio::EnsEMBL::OntologyXref->new(
+            -primary_id  => $go_pid,
+            -display_id  => $go_rec->{'display_label'},
+            -version     => 1,
+            -info_type   => 'DEPENDENT',
+            -dbname      => 'GO',
+            -description => $go_rec->{'go_name'},
+          );
+
+          my $source_xref = Bio::EnsEMBL::DBEntry->new(
+            -primary_id  => $go_rec->{'uniprot_acc'},
+            -display_id  => $go_rec->{'assigned_by'},
+            -info_type   => 'DEPENDENT',
+            -dbname      => 'Quick_Go_Evidence',
+            -version => 1,
+            -info_text   => 'Quick_Go:'.$go_rec->{'assigned_by'},
+          );
+          $dbentry->add_linkage_type($ev_type,$source_xref);
+          $trl->add_DBEntry($dbentry);
+          if (! $support->param('dry_run')) {
+            if (my $dbID = $ea->store($dbentry, $trlid, 'translation', 1)) {
+              $support->log_verbose("Stored GO xref (display_id = $go_pid, dbID = $dbID, source_db = ".$go_rec->{'assigned_by'}." for $trlsi\n", 3);
             }
             else {
               $support->log_warning("Failed to store GO xref for $trlsi\n");
             }
           }
-        }								
-        if (! $pid) {
-          $support->log_verbose("No match found for $trlsi.\n",1);					
         }
       }
     }
   }
-  $support->log("$chr_c $xrefformat xrefs added for chromosome $chr\n");
+  $support->log("$chr_c GO xrefs added for chromosome $chr\n");
 }
 
-$support->log("$overall_c $xrefformat xrefs found in total\n");
+$support->log("$overall_c GO xrefs found in total\n");
 
 $support->finish_log;
 
@@ -352,6 +355,11 @@ sub parse_go {
     my $go_name      = $fields[4]; # ie description of the GO term [= xref.description]
     my $ev_code      = $fields[5]; # type of evidence, eg IEA [= ontology_xref.linkage]# 
     my $assigned_by  = $fields[8]; # who made the connection, ie 'Annotation source' (also used for link to QuickGO
+
+    unless ($evidence_priorities{$ev_code}) {
+      $support->log_error("evidence code '$ev_code' is not in our priority list, contact EBI to find out where it should lie\n");
+    }
+
     chomp $assigned_by;
     $source_dbs{$assigned_by}++;
 
@@ -371,12 +379,6 @@ sub parse_go {
       $rec = {};
     }
 
-#    if (! $rec->{$tlsi}{'uniprot'}{$uniprot}) {
-#      push @{$rec->{$tlsi}{'uniprot'}{$uniprot}}, {
-#        'pid'           => $uniprot,
-#        'display_label' => $uniprot,
-#      };
-#    }
     $rec->{$tlsi}{'Uniprot/SWISSPROT'}{$uniprot} = {
       'pid'           => $uniprot,
       'display_label' => $uniprot,
@@ -400,30 +402,6 @@ sub parse_go {
 #select just one of the GO records (by evidence code priority) and then add to $parsed_xrefs
 sub cleanup_and_store {
   my ($rec,$tlsi) = @_;
-
-  # priorites from GOA
-  my %evidence_priorities = (
-    IDA => '1',   # inferred from direct assay
-    IMP => '2',   # inferred from mutant phenotype
-    IGI => '3',   # inferred from genetic interaction
-    IPI => '4',   # inferred from physical interaction
-    IEP => '5',   # inferred from expression pattern
-    EXP => '6',   # inferred from experiment
-    ISO => '7',   # inferred from sequence orthology
-    ISA => '7',   # inferred from sequence alignment
-    ISS => '8',   # inferred from sequence or structural similarity
-    IBD => '8',   # Inferred from Biological aspect of Descendant
-    IKR => '8',   # Inferred from Key Residues
-    IRD => '8',   # Inferred from Rapid Divergence
-    IBA => '8',   # Inferred from Biological aspect of Ancestor
-    IGC => '9',   # Inferred from Genomic Context
-    NAS => '10',  # non-traceable author statement
-    TAS => '11',  # traceable author statement
-    IC => '12',   # inferred by curator
-    RCA => '13',  # reviewed computational annotation
-    IEA  => '14', # inferred from electronic annotation
-    ND  => '15',  # no data
-  );
 
   #select just one record after ordering by priority
   my $wanted_rec;
