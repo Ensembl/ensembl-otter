ddbb1f2208d95b1789ee11b0dd656274a30bd954
Jeremy Henty <jh13@sanger.ac.uk>
2012-10-04 13:44:59 +0100

    Server/GFF/DAS.pm: sub Bio::EnsEMBL::Slice::get_all_features_via_DAS(): break some long lines.

--- modules/Bio/Otter/Server/GFF/DAS.pm
+++ modules/Bio/Otter/Server/GFF/DAS.pm
@@ -122,10 +122,14 @@ sub Bio::EnsEMBL::Slice::get_all_features_via_DAS {
     my $chr_end   = $slice->end();
     my $segment_name  = "$chr_name:$chr_start,$chr_end";
 
-    my ($sieve_field_path, $sieve_sign, $sieve_value) = split(/(--CONTAINS--|--LACKS--)/, $sieve);
+    my ($sieve_field_path, $sieve_sign, $sieve_value) =
+        split(/(--CONTAINS--|--LACKS--)/, $sieve);
     $sieve_field_path &&= [ split(/--THEN--/,$sieve_field_path) ];
 
-    my $grouplabel_field_path = $grouplabel ? [ map { [ split(/--THEN--/,$_) ] } split(/--JOIN--/,$grouplabel) ] : '';
+    my $grouplabel_field_path =
+        $grouplabel
+        ? [ map { [ split(/--THEN--/,$_) ] } split(/--JOIN--/,$grouplabel) ]
+        : '';
 
     # DAS servers will give all features if a "type=" argument is not present
     if ($analysis_name and $analysis_name eq 'all') {
