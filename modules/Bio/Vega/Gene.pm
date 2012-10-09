69304d4ad58bb97518e74a9106203b00f61c7613
Jeremy Henty <jh13@sanger.ac.uk>
2012-10-09 09:41:14 +0100

    Vega/Gene.pm: sub vega_hashkey(): break some long lines.

--- modules/Bio/Vega/Gene.pm
+++ modules/Bio/Vega/Gene.pm
@@ -47,13 +47,21 @@ sub all_Attributes_string {
 sub vega_hashkey {
     my ($self) = @_;
 
-    my $seq_region_name   = $self->seq_region_name    || throw(  'seq_region_name must be set to generate vega_hashkey');
-    my $start             = $self->seq_region_start   || throw( 'seq_region_start must be set to generate vega_hashkey');
-    my $end               = $self->seq_region_end     || throw(   'seq_region_end must be set to generate vega_hashkey');
-    my $strand            = $self->seq_region_strand  || throw('seq_region_strand must be set to generate vega_hashkey');
-    my $biotype           = $self->biotype            || throw(          'biotype must be set to generate vega_hashkey');
-    my $status            = $self->status             || throw(           'status must be set to generate vega_hashkey');
-    my $source            = $self->source             || throw(           'source must be set to generate vega_hashkey');
+    my $seq_region_name = $self->seq_region_name
+        || throw(  'seq_region_name must be set to generate vega_hashkey');
+    my $start           = $self->seq_region_start
+        || throw( 'seq_region_start must be set to generate vega_hashkey');
+    my $end             = $self->seq_region_end 
+        || throw(   'seq_region_end must be set to generate vega_hashkey');
+    my $strand          = $self->seq_region_strand
+        || throw('seq_region_strand must be set to generate vega_hashkey');
+    my $biotype         = $self->biotype        
+        || throw(          'biotype must be set to generate vega_hashkey');
+    my $status          = $self->status         
+        || throw(           'status must be set to generate vega_hashkey');
+    my $source          = $self->source         
+        || throw(           'source must be set to generate vega_hashkey');
+
     my $tran_count = scalar @{$self->get_all_Transcripts}
         || throw("there are no transcripts for this gene to generate correct vega_hashkey");
 
