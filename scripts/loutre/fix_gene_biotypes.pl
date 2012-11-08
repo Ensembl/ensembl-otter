1cb3fc4a2e2e666e55112efa7cc6cd1af853c923
James Gilbert <jgrg@sanger.ac.uk>
2012-11-08 16:33:58 +0000

    skip artifact genes

--- scripts/loutre/fix_gene_biotypes.pl
+++ scripts/loutre/fix_gene_biotypes.pl
@@ -73,7 +73,10 @@ use Bio::EnsEMBL::DBSQL::DBAdaptor;
     # my @args = qw( -dbname vega_homo_sapiens_20120813_68_GRCh37 -host ensdb-web-17 -port 5317 );
 
     # my @args = qw( -dbname vega_homo_sapiens_20120822_68_GRCh37 -host ensdb-web-17 -port 5317 );
-    my @args = qw( -dbname vega_mus_musculus_20120822_68_GRCm38 -host ensdb-web-17 -port 5317 );
+    # my @args = qw( -dbname vega_mus_musculus_20120822_68_GRCm38 -host ensdb-web-17 -port 5317 );
+    
+    # my @args = qw( -dbname vega_homo_sapiens_20120822_69_GRCh37 -host ensdb-web-17 -port 5317 );
+    my @args = qw( -dbname vega_mus_musculus_20120822_69_GRCm38 -host ensdb-web-17 -port 5317 );
 
     my $now = scalar localtime;
     print "$now fix_gene_biotypes.pl on (@args)\n";
@@ -160,6 +163,7 @@ sub fix_biotypes {
 
     my %gene_tsct_biotypes;
     while (my ($gene_biotype, $gene_status, $gene_id, $gsid, $tsct_biotype, $tsct_status) = $sth->fetchrow) {
+        next if $tsct_biotype eq 'artifact';
         $gene_status ||= '';
         $tsct_status ||= '';
         my $gene_data = $gene_tsct_biotypes{$gene_id} ||= {};
