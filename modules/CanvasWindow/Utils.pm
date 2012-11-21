8894ba40a97203422fc7b8b786f3a7c51e872f3e
Jeremy Henty <jh13@sanger.ac.uk>
2012-11-21 13:33:43 +0000

    CanvasWindow/Utils.pm: sub bbox_union(): remove, unused.

--- modules/CanvasWindow/Utils.pm
+++ modules/CanvasWindow/Utils.pm
@@ -10,20 +10,8 @@ use base qw( Exporter );
 use vars qw ( @EXPORT_OK );
 
 @EXPORT_OK = qw{
-    bbox_union
     expand_bbox
     };
-
-sub bbox_union {
-    my ($bb1, $bb2) = @_;
-
-    my @new = @$bb1;
-    $new[0] = $bb2->[0] if $bb2->[0] < $bb1->[0];
-    $new[1] = $bb2->[1] if $bb2->[1] < $bb1->[1];
-    $new[2] = $bb2->[2] if $bb2->[2] > $bb1->[2];
-    $new[3] = $bb2->[3] if $bb2->[3] > $bb1->[3];
-    return @new;
-}
 
 sub expand_bbox {
     my ($bbox, $pad) = @_;
