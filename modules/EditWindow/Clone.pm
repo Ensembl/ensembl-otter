Ensembl/ensembl-otter

commit 8b095ae70162e36272b1626ed2c50054fddc3836

EditWindow/Clone.pm: use "try()" instead of "eval()".

modules/EditWindow/Clone.pm
@@ -7,6 +7,8 @@ use strict;
 use warnings;
 use Carp;
 
+use Try::Tiny;
+
 use base 'EditWindow';
 
 =pod
@@ -216,9 +218,8 @@ sub generate_desc {
         return;
     }
 
-    # delete any existing text that is highlighted (we have to eval
-    # because if nothing is highlighted this errors)
-    eval{$self->description_text->delete('sel.first', 'sel.last')};
+    # delete any existing text that is highlighted (if nothing is highlighted this errors)
+    try {$self->description_text->delete('sel.first', 'sel.last')};
 
     # and insert the new text at the current cursor position
     $self->description_text->insert('insert', $desc);
@@ -378,7 +379,7 @@ sub fill_Entries {
         $entry->insert(0, $text);
 
         # Not all versions of Entry have the "readonly" state
-        eval { $entry->configure(-state => 'readonly'); };
+        try { $entry->configure(-state => 'readonly'); };
         $entry->configure(-state => 'disabled') if $@;
     }
 

commit fed1c0f062ef4a49cb2cafc4ca4beb872c6bae72

EditWindow/Clone.pm: sub fill_Entries(): properly handle entries without the "readonly" state.
This fixes a bug introduced in commit
8b095ae70162e36272b1626ed2c50054fddc3836 'EditWindow/Clone.pm: use
"try()" instead of "eval()".'.

modules/EditWindow/Clone.pm
@@ -379,8 +379,8 @@ sub fill_Entries {
         $entry->insert(0, $text);
 
         # Not all versions of Entry have the "readonly" state
-        try { $entry->configure(-state => 'readonly'); };
-        $entry->configure(-state => 'disabled') if $@;
+        try { $entry->configure(-state => 'readonly'); }
+        catch { $entry->configure(-state => 'disabled'); };
     }
 
     return;