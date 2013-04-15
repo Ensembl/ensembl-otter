f56684ed859a6e4a1f139a4617ff7b57d6815ec9
Michael Gray <mg13@sanger.ac.uk>
2013-04-15 11:31:03 +0100

    Table translation_stable_id no longer exists.

--- sql/otter_schema.sql
+++ sql/otter_schema.sql
@@ -1,5 +1,3 @@
-drop index stable_id on translation_stable_id;
-create index stable_id on translation_stable_id (stable_id,version);
 insert into meta (meta_key,meta_value) values('last_gene_old_dbid',0);
 insert into meta (meta_key,meta_value) values ('last_contig_info_old_dbid',0);
 
