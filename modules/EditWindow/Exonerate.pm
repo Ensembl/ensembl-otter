53fc8abbccd8331843598deea8f09e9416f8925c
Jeremy Henty <jh13@sanger.ac.uk>
2013-05-20 11:51:54 +0100

    SessionWindow.pm: &get_mark_in_slice_coords: create by refactoring.

--- modules/EditWindow/Exonerate.pm
+++ modules/EditWindow/Exonerate.pm
@@ -449,7 +449,7 @@ sub launch_exonerate {
 
     # get marked region (if requested)
     if ($self->{_use_marked_region}) {
-        my ($mark_start, $mark_end) = $SessionWindow->zmap->get_mark;
+        my ($mark_start, $mark_end) = $SessionWindow->get_mark_in_slice_coords;
         if ($mark_start && $mark_end) {
             $self->logger->warn("Setting exonerate genomic start & end to marked region: $mark_start - $mark_end");
             $otf->target_start($mark_start);
