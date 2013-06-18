601507c834c155418dc18123d590a22da0a4a445
Michael Gray <mg13@sanger.ac.uk>
2013-06-18 14:03:36 +0100

    rationalise_start_end_nf_attrs.pl: actually delete attributes.

--- scripts/loutre/rationalise_start_end_nf_attrs.pl
+++ scripts/loutre/rationalise_start_end_nf_attrs.pl
@@ -1,22 +1,30 @@
+#!/usr/bin/env perl
+
 package Bio::Otter::Script::RationaliseStartEndNFAttrs;
 
 use strict;
 use warnings;
 use 5.010;
 
+use Carp;
+
 use parent 'Bio::Otter::Utils::Script';
 
 sub ottscript_options {
     return (
-        dataset_mode => 'only_one', # for now
+        dataset_mode          => 'only_one', # for now
+        allow_iteration_limit => 1,
+        allow_modify_limit    => 1,
         );
 }
 
 sub process_dataset {
   my ($self, $dataset) = @_;
 
-  my $sth = $self->find_attrs_sth($dataset);
-  $dataset->callback_data('find_attrs_sth' => $sth);
+  my $find_sth = $self->find_attrs_sth($dataset);
+  $dataset->callback_data('find_attrs_sth' => $find_sth);
+  my $delete_sth = $self->delete_attr_sth($dataset);
+  $dataset->callback_data('delete_attr_sth' => $delete_sth);
 
   $dataset->iterate_transcripts(\&do_transcript);
 
@@ -26,20 +34,42 @@ sub process_dataset {
 sub do_transcript {
     my ($dataset, $ts) = @_;
 
-    my $sth = $dataset->callback_data('find_attrs_sth');
-    $sth->execute($ts->transcript_id);
-    my $rows = $sth->fetchall_arrayref({});
+    my $verbose = $dataset->verbose;
+
+    my $find_sth   = $dataset->callback_data('find_attrs_sth');
+    my $delete_sth = $dataset->callback_data('delete_attr_sth');
+
+    $find_sth->execute($ts->transcript_id);
+    my $rows = $find_sth->fetchall_arrayref({});
 
     my $msg;
-    my $verbose_msg = 'ok';
+    my $verbose_msg;
 
     if (@$rows) {
         my $n = scalar(@$rows);
-        $msg = "$n to delete";
-        if ($dataset->verbose) {
-            my $attrs    = join(',', map { $_->{code} . '=' . $_->{value} } @$rows);
-            $verbose_msg = "$n - $attrs";
+        my $d = 0;
+        foreach my $row ( @$rows ) {
+            my $v_id;
+            $v_id = sprintf(
+               "%s=%s (%d, %d)", $row->{code}, $row->{value}, $row->{transcript_id}, $row->{attrib_type_id}
+                ) if $verbose;
+            if ($dataset->may_modify) {
+                my $n_deleted = $delete_sth->execute($row->{transcript_id}, $row->{attrib_type_id});
+                if ($n_deleted == 1) {
+                    $d += $n_deleted;
+                    $verbose_msg .= "\n\t$v_id DELETED" if $verbose;
+                } else {
+                    my $errstr = $delete_sth->errstr;
+                    croak "Expected to delete 1 row, but deleted $n_deleted [$errstr]";
+                }
+            } else {
+                $verbose_msg .= "\n\t$v_id would be deleted" if $verbose;
+            }
         }
+        $msg = $dataset->may_modify ? "DELETED $d of $n attributes" : "would delete $n attributes";
+        $dataset->inc_modified_count if $d;
+    } else {
+        $verbose_msg = 'ok';
     }
     return ($msg, $verbose_msg);
 }
@@ -67,6 +97,20 @@ sub find_attrs_sth {
             'cds_start_NF',
             'cds_end_NF'
           )
+    });
+    return $sth;
+}
+
+sub delete_attr_sth {
+    my ($self, $dataset) = @_;
+
+    my $dbc = $dataset->otter_dba->dbc;
+    my $sth = $dbc->prepare(q{
+        DELETE FROM transcript_attrib
+        WHERE transcript_id  = ?
+        AND   attrib_type_id = ?
+        AND   value          = 0
+        LIMIT 1
     });
     return $sth;
 }
