eaccb7abbdef5648827c578809724822d85eda4d
Matthew Astley <mca@sanger.ac.uk>
2013-07-18 10:32:16 +0100

    B:O:Error - show in logs where window is closed

--- modules/Bio/Otter/Error.pm
+++ modules/Bio/Otter/Error.pm
@@ -17,14 +17,20 @@ BEGIN {
     no warnings qw( redefine ); ## no critic (TestingAndDebugging::ProhibitNoWarnings)
     sub Tk::Error {
         my ($w, $error, @messages) = @_;
+        # nb. @messages are a stacktrace, truncated by Tk where we
+        # emerged from innermost event handler
 
         my $message = $error =~ /web server/
             ? 'There seems to be a problem with the web server, please try again later.'
             : "Unidentified problem: $error\n\nI suggest you raise a helpdesk ticket!"
             ;
 
-        print STDERR $error, map { qq( $_\n) } @messages; # dump to the log
+        print STDERR "Tk::Error: $error", map { qq( $_\n) } @messages; # dump to the log
         $tk_error_dialog->($w, $message, @messages); # pop up the dialog
+
+        # show that we have left the nested event loop
+        print STDERR "(back from Tk::Error)\n";
+
         return;
     }
 }
