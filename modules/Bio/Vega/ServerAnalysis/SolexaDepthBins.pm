caf8ea9223213c128d22114af7ef0ee815deaa18
Jeremy Henty <jh13@sanger.ac.uk>
2013-08-07 16:20:21 +0100

    SolexaDepthBins.pm: complete rewrite for performance.
        This cut the running time of the solexa_coverage column from 40
        seconds to 20 seconds for human chr20-13, clones 27-30, running on
        a local Otter server on my deskpro.

--- modules/Bio/Vega/ServerAnalysis/SolexaDepthBins.pm
+++ modules/Bio/Vega/ServerAnalysis/SolexaDepthBins.pm
@@ -16,73 +16,74 @@ sub new {
     return bless {}, $class;
 }
 
+sub depth_feature {
+    my ($slice, $strand, $bin, $score) = @_;
+
+    my $start = $bin * $BIN_SIZE;
+    my $end   = $start + ($BIN_SIZE - 1);
+    my $display_label = sprintf("Average depth: %.2f", $score);
+
+    my $depth_feature =
+        Bio::EnsEMBL::SimpleFeature->new(
+            -start         => $start,
+            -end           => $end,
+            -strand        => $strand,
+            -slice         => $slice,
+            -score         => $score,
+            -display_label => $display_label,
+        );
+
+    return $depth_feature;
+}
+
+sub depth_features {
+    my ($slice, $strand, $depth_hash) = @_;
+    my @depth_features = map {
+        depth_feature($slice, $strand, $_, $depth_hash->{$_});
+    } keys %{$depth_hash};
+    return @depth_features;
+}
+
 sub run {
     my ($self, $features) = @_;
 
-    my @depth_features;
+    my @depth_features = ( );
 
-    my $depth_forward = {};
-    my $depth_reverse = {};
+    if (@{$features}) {
 
-    my $slice;
+        my $depth_forward = { };
+        my $depth_reverse = { };
 
-    for my $af (@$features) {
-        $slice ||= $af->slice;
-        my $depth = $af->strand == 1 ? $depth_forward : $depth_reverse; 
-        my @fs = $af->ungapped_features;
-        for my $f (@fs) {
-            for (my $i = $f->start; $i < $f->end; $i++) {
-                $depth->{$i}++;    
+        for my $feature (@{$features}) {
+            my $depth = $feature->strand == 1 ? $depth_forward : $depth_reverse; 
+            for my $ungapped_feature ($feature->ungapped_features) {
+
+                my $start = $ungapped_feature->start;
+                my $end   = $ungapped_feature->end + 1;
+                my $start_bin = int ($start / $BIN_SIZE);
+                my $end_bin   = int ($end   / $BIN_SIZE);
+
+                if ($start_bin == $end_bin) {
+                    $depth->{$start_bin} += ($end - $start) / $BIN_SIZE;
+                }
+                else {
+                    $depth->{$start_bin} += ($BIN_SIZE - $start % $BIN_SIZE) / $BIN_SIZE;
+                    for (my $bin = $start_bin + 1; $bin < $end_bin; $bin++) {
+                        $depth->{$bin}++;
+                    }
+                    $depth->{$end_bin} += ($end % $BIN_SIZE) / $BIN_SIZE;
+                }
             }
         }
-    }
 
-    for my $depth ($depth_forward, $depth_reverse) {
+        my $slice = $features->[0]->slice;
+        my @strand_depth_hash_list = (
+            [ +1, $depth_forward ],
+            [ -1, $depth_reverse ],
+            );
+        @depth_features =
+            map { depth_features($slice, @{$_}) } @strand_depth_hash_list;
 
-        my $strand = ($depth == $depth_forward ? 1 : -1);
-
-        my ($start, $end, $tot_depth);
-
-        my $bin_cnt = 0;
-
-        for my $b (sort { $a <=> $b } keys %$depth) {
-
-            my $depth = $depth->{$b};
-
-            $bin_cnt++;
-
-            if (!$start) {
-                $start = $b;
-                $end = $b;
-                $tot_depth = $depth;
-            }
-            elsif ( ($bin_cnt == $BIN_SIZE) || ($b != $end+1) ) {
-
-                # end this feature
-
-                my $score = $tot_depth / ($end - $start +1);
-
-                my $sf = Bio::EnsEMBL::SimpleFeature->new(
-                    -start         => $start,
-                    -end           => $end,
-                    -strand        => $strand,
-                    -slice         => $slice,
-                    -score         => $score,
-                    -display_label => sprintf("Average depth: %.2f", $score),
-                );
-
-                push @depth_features, $sf;
-
-                $start = $b;
-                $end = $b;
-                $tot_depth = $depth;
-                $bin_cnt = 0;
-            }
-            else {
-                $end++;
-                $tot_depth += $depth;
-            }
-        }
     }
 
     return @depth_features;
