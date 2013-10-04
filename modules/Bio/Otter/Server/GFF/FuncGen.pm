b359c5c059370bb801858d425bd700034996f0da
Michael Gray <mg13@sanger.ac.uk>
2013-10-04 13:31:39 +0100

    B:O:Server::GFF::FuncGen: filter on feature_type.

--- modules/Bio/Otter/Server/GFF/FuncGen.pm
+++ modules/Bio/Otter/Server/GFF/FuncGen.pm
@@ -9,10 +9,16 @@ use base qw( Bio::Otter::Server::GFF );
 # Inject the necessary get_all_ method into slice
 #
 sub Bio::EnsEMBL::Slice::get_all_SegmentationFeatures {
-    my ($slice, $featureset_name) = @_;
+    my ($slice, $featuretype_name, $featureset_name) = @_;
 
     my $funcgen_dba = $slice->adaptor->efgdb;
 
+    # Just a check for existence, whilst we are using grep below...
+    my $featuretype_adaptor = $funcgen_dba->get_adaptor('featuretype');
+    my $featuretype = $featuretype_adaptor->fetch_by_name($featuretype_name);
+    die "Cannot get featuretype for '$featuretype_name'" unless $featuretype;
+
+    # ...but we really use this one in the API fetch_all_by... call
     my $featureset_adaptor = $funcgen_dba->get_adaptor('featureset');
     my $featureset = $featureset_adaptor->fetch_by_name($featureset_name);
     die "Cannot get featureset for '$featureset_name'" unless $featureset;
@@ -20,7 +26,10 @@ sub Bio::EnsEMBL::Slice::get_all_SegmentationFeatures {
     my $segmentation_feature_adaptor = $funcgen_dba->get_adaptor('segmentationfeature');
     my $seg_features = $segmentation_feature_adaptor->fetch_all_by_Slice_FeatureSets($slice, [ $featureset ]);
 
-    return $seg_features;
+    # It would be better to do this in the fetch query, but that would mean extending / injecting into
+    # Bio::EnsEMBL::Funcgen::SegmentedFeatureAdaptor (probably via SetFeatureAdaptor).
+
+    return [ grep { $_->feature_type->name eq $featuretype_name } @$seg_features ];
 }
 
 sub ensembl_adaptor_class {
@@ -39,10 +48,13 @@ sub get_requested_features {
     my $feature_kind    = $self->require_argument('feature_kind');
     die "feature_kind '$feature_kind' not supported" unless $feature_kind eq 'SegmentationFeature';
 
-    my $metakey         = $self->require_argument('metakey'); # to find funcgen db
-    my $featureset_name = $self->require_argument('featureset');
+    my $metakey          = $self->require_argument('metakey'); # to find funcgen db
+    my $featuretype_name = $self->require_argument('feature_type');
+    my $featureset_name  = $self->require_argument('feature_set');
 
-    return $self->fetch_mapped_features_ensembl('get_all_SegmentationFeatures', [ $featureset_name ], $map, $metakey);
+    return $self->fetch_mapped_features_ensembl('get_all_SegmentationFeatures',
+                                                [ $featuretype_name, $featureset_name ],
+                                                $map, $metakey);
 }
 
 1;
