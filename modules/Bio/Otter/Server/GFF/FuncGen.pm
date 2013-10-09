54afc61ae0df5f6ba67e3428b8717677efc72fdd
Michael Gray <mg13@sanger.ac.uk>
2013-10-09 10:41:00 +0100

    B:O:S:GFF::FuncGen: make feature_type optional.

--- modules/Bio/Otter/Server/GFF/FuncGen.pm
+++ modules/Bio/Otter/Server/GFF/FuncGen.pm
@@ -13,10 +13,13 @@ sub Bio::EnsEMBL::Slice::get_all_SegmentationFeatures {
 
     my $funcgen_dba = $slice->adaptor->efgdb;
 
-    # Just a check for existence, whilst we are using grep below...
-    my $featuretype_adaptor = $funcgen_dba->get_adaptor('featuretype');
-    my $featuretype = $featuretype_adaptor->fetch_by_name($featuretype_name);
-    die "Cannot get featuretype for '$featuretype_name'" unless $featuretype;
+    my $featuretype;
+    if ($featuretype_name) {
+        # Just a check for existence, whilst we are using grep below...
+        my $featuretype_adaptor = $funcgen_dba->get_adaptor('featuretype');
+        $featuretype = $featuretype_adaptor->fetch_by_name($featuretype_name);
+        die "Cannot get featuretype for '$featuretype_name'" unless $featuretype;
+    }
 
     # ...but we really use this one in the API fetch_all_by... call
     my $featureset_adaptor = $funcgen_dba->get_adaptor('featureset');
@@ -25,6 +28,8 @@ sub Bio::EnsEMBL::Slice::get_all_SegmentationFeatures {
 
     my $segmentation_feature_adaptor = $funcgen_dba->get_adaptor('segmentationfeature');
     my $seg_features = $segmentation_feature_adaptor->fetch_all_by_Slice_FeatureSets($slice, [ $featureset ]);
+
+    return $seg_features unless $featuretype_name;
 
     # It would be better to do this in the fetch query, but that would mean extending / injecting into
     # Bio::EnsEMBL::Funcgen::SegmentedFeatureAdaptor (probably via SetFeatureAdaptor).
@@ -49,8 +54,8 @@ sub get_requested_features {
     die "feature_kind '$feature_kind' not supported" unless $feature_kind eq 'SegmentationFeature';
 
     my $metakey          = $self->require_argument('metakey'); # to find funcgen db
-    my $featuretype_name = $self->require_argument('feature_type');
     my $featureset_name  = $self->require_argument('feature_set');
+    my $featuretype_name = $self->param('feature_type');
 
     return $self->fetch_mapped_features_ensembl('get_all_SegmentationFeatures',
                                                 [ $featuretype_name, $featureset_name ],
