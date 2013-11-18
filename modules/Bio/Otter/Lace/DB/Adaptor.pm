eacd7091aea5dee69c476d65af28171b8ffa6ecf
Michael Gray <mg13@sanger.ac.uk>
2013-11-18 10:08:50 +0000

    B:O:Lace::DB::Adaptor base class.

---
+++ modules/Bio/Otter/Lace/DB/Adaptor.pm
@@ -0,0 +1,189 @@
+
+### Bio::Otter::Lace::DB::Adaptor
+
+package Bio::Otter::Lace::DB::Adaptor;
+
+use strict;
+use warnings;
+use Carp;
+
+sub new {
+    my ($pkg, $dbh) = @_;
+
+    confess "dbh must be supplied" unless $dbh;
+
+    my $self = bless {}, $pkg;
+    $self->dbh($dbh);
+
+    return $self;
+}
+
+sub dbh {
+    my ($self, $dbh) = @_;
+
+    if ($dbh) {
+        $self->{'_dbh'} = $dbh;
+    }
+    return $self->{'_dbh'};
+}
+
+## These methods must be overridden in the child adaptor:
+
+sub columns {
+    confess "columns() must be defined by subclass";
+}
+
+sub key_column_name {
+    confess "key_column_name() must be defined by subclass";
+}
+
+sub SQL {
+    confess "SQL() must be defined by subclass";
+}
+
+## These methods can be overridden in the child adaptor:
+
+sub key_is_auto_increment {
+    return;
+}
+
+#
+
+sub object_class {
+    return;
+}
+
+# or, if not, then override...
+
+sub new_object {
+    my ($self, %attribs) = @_;
+    my $obj_class = $self->object_class;
+    confess "object_class() must be provided by subclass to use new_object()" unless $obj_class;
+    return $obj_class->new(%attribs);
+}
+
+## ^^^ End of standard overrideable methods ^^^
+
+sub all_columns {
+    my ($self) = @_;
+    return join(', ', $self->columns);
+}
+
+sub check_object {
+    my ($self, $object) = @_;
+
+    confess "Object must be supplied" unless $object;
+
+    if (my $obj_class = $self->object_class) {
+        confess "'$object' is not a $obj_class" unless $object->isa($obj_class);
+    }
+
+    return $object;
+}
+
+sub store {
+    my ($self, $object) = @_;
+    $self->check_object($object);
+    confess "Object already stored" if $object->is_stored;
+
+    my $result = $self->_store_sth->execute(map { $object->$_() } $self->columns);
+    $object->is_stored(1) if $result;
+
+    if ($self->key_is_auto_increment) {
+        my $key_column_name = $self->key_column_name;
+        unless (defined $object->$key_column_name) {
+            $object->$key_column_name($self->dbh->last_insert_id("", "", "", ""));
+        }
+    }
+
+    return $result;
+}
+
+sub update {
+    my ($self, $object) = @_;
+    $self->check_object($object);
+    confess "Object not stored" unless $object->is_stored;
+
+    my $key_column_name = $self->key_column_name;
+    my $result = $self->_update_sth->execute((map { $object->$_() } $self->columns), $object->$key_column_name());
+    return $result;
+}
+
+sub delete {
+    my ($self, $object) = @_;
+    $self->check_object($object);
+    confess "Object not stored" unless $object->is_stored;
+
+    my $result = $self->_delete_sth->execute($object->name);
+    $object->is_stored(0) if $result;
+
+    return $result;
+}
+
+sub _fetch_by_key {
+    my ($self, $key) = @_;
+
+    my $sth = $self->_fetch_by_key_sth;
+    $sth->execute($key);
+    my $attribs = $sth->fetchrow_hashref;
+    return $attribs;
+}
+
+sub fetch_by_key {
+    my ($self, $key) = @_;
+
+    my $attribs = $self->_fetch_by_key($key);
+    return unless $attribs;
+
+    my $object = $self->new_object(%$attribs);
+    $object->is_stored(1);
+    return $object;
+}
+
+sub fetch_state {
+    my ($self, $object) = @_;
+    $self->check_object($object);
+
+    my $key_column_name = $self->key_column_name;
+    my $attribs = $self->_fetch_by_key($object->$key_column_name);
+    return unless $attribs;
+
+    while (my ($key, $value) = each %$attribs) {
+        $object->$key($value);
+    }
+    $object->is_stored(1);
+
+    return $object;
+}
+
+sub _store_sth         { return shift->_prepare_canned('store'); }
+sub _update_sth        { return shift->_prepare_canned('update'); }
+sub _delete_sth        { return shift->_prepare_canned('delete'); }
+sub _fetch_by_key_sth  { return shift->_prepare_canned('fetch_by_key'); }
+
+sub _prepare_canned {
+    my ($self, $name) = @_;
+    return $self->_prepare_cached("_${name}_sth", $self->SQL->{$name});
+}
+
+sub _prepare_cached {
+    my ($self, $name, $sql) = @_;
+    return $self->{$name} if $self->{$name};
+
+    return $self->{$name} = $self->dbh->prepare($sql);
+}
+
+sub begin_work { return shift->dbh->do('BEGIN IMMEDIATE TRANSACTION'); }
+sub commit     { return shift->dbh->commit;     }
+sub rollback   { return shift->dbh->rollback;   }
+
+1;
+
+__END__
+
+=head1 NAME - Bio::Otter::Lace::DB::Adaptor
+
+=head1 AUTHOR
+
+Michael Gray B<email> mg13@sanger.ac.uk
+
---
+++ t/OtterLaceDBAdaptor.t
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test::More;
+
+# FIXME: lots of boilerplate here.
+
+use Test::Otter qw( ^data_dir_or_skipall ); # also finds test libraries
+use OtterTest::DB;
+
+use lib "${ENV{ANACODE_TEAM_TOOLS}}/t/tlib";
+use Test::CriticModule;
+
+my $adaptor_module;
+BEGIN {
+      $adaptor_module = 'Bio::Otter::Lace::DB::Adaptor';
+      use_ok($adaptor_module);
+}
+
+critic_module_ok($adaptor_module);
+
+my $test_db = OtterTest::DB->new;
+my $ca = new_ok($adaptor_module => [ $test_db->dbh ]);
+
+done_testing;
+
+1;
+
+# Local Variables:
+# mode: perl
+# End:
+
+# EOF
