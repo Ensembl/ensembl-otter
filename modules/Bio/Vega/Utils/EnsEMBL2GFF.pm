e66d34477357a6d44774e57fc70e4420fa314630
Jeremy Henty <jh13@sanger.ac.uk>
2014-01-08 17:10:01 +0000

    EnsEMBL2GFF.pm: variation features: get the SO term.

--- modules/Bio/Vega/Utils/EnsEMBL2GFF.pm
+++ modules/Bio/Vega/Utils/EnsEMBL2GFF.pm
@@ -8,6 +8,7 @@ use warnings;
 use Try::Tiny;
 
 use Bio::EnsEMBL::Utils::Exception qw(verbose);
+use Bio::EnsEMBL::Variation::Utils::Sequence qw( SO_variation_class );
 
 # This module allows conversion of ensembl/otter objects to GFF by
 # inserting to_gff (and supporting _gff_hash) methods into the
@@ -479,7 +480,10 @@ my $_new_feature_id_sub = sub {
     }
 
     sub _gff_feature {
-        my $feature = 'sequence_alteration';
+        my ($self) = @_;
+        my $feature =
+            SO_variation_class($self->allele_string)
+            || 'sequence_alteration';
         return $feature;
     }
 }
