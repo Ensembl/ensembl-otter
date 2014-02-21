0317a0531831e0a9236db296ff6097f430ca7baa
Michael Gray <mg13@sanger.ac.uk>
2014-02-21 12:31:59 +0000

    B:O:Utils::AccessionInfo: an interface to MM to allow it to be swapped out when necessary.

---
+++ modules/Bio/Otter/Utils/AccessionInfo.pm
@@ -0,0 +1,46 @@
+
+### Bio::Otter::Utils::AccessionInfo
+
+package Bio::Otter::Utils::AccessionInfo;
+
+use strict;
+use warnings;
+
+use Readonly;
+
+=pod
+
+=head1 NAME - Bio::Otter::Utils::AccessionInfo
+
+A cover for MM to allow an (as-yet-to-be-written) alternative driver to be substituted,
+such as one which uses EBI dbfetch.
+
+=cut
+
+Readonly my $DEFAULT_DRIVER_CLASS => 'Bio::Otter::Utils::MM';
+
+sub new {
+    my ($class, @args) = @_;
+
+    my %options = ( driver_class => $DEFAULT_DRIVER_CLASS, @args );
+    my $driver_class = delete $options{driver_class};
+    ## no critic (BuiltinFunctions::ProhibitStringyEval,Anacode::ProhibitEval)
+    eval "require $driver_class" or die "Couldn't load '$driver_class': '$@'";
+
+    my $driver = $driver_class->new(%options);
+    return bless { _driver => $driver }, $class;
+}
+
+sub get_accession_info  { my ($self, @args) = @_; return $self->{_driver}->get_accession_info(@args);  }
+sub get_accession_types { my ($self, @args) = @_; return $self->{_driver}->get_accession_types(@args); }
+sub db_categories       { my ($self, @args) = @_; return $self->{_driver}->db_categories(@args);       }
+sub debug               { my ($self, @args) = @_; return $self->{_driver}->debug(@args);               }
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Ana Code B<email> anacode@sanger.ac.uk
+
