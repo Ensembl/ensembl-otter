fe0310c9e4f529f7e2b09102630e01349c287dfa
Matthew Astley <mca@sanger.ac.uk>
2014-03-04 17:13:15 +0000

    slice_locks design
    tests pass, not useful yet

--- modules/Bio/Vega/SliceLock.pm
+++ modules/Bio/Vega/SliceLock.pm
@@ -17,59 +17,134 @@ broker.
 
 =cut
 
+my @FIELD = qw( seq_region_id seq_region_start seq_region_end
+                author ts_begin ts_activity active freed freed_author
+                intent hostname ts_free );
+
 sub new {
     my ($class, @args) = @_;
 
     my $self = $class->SUPER::new(@args);
 
-    my ($slice_id, $author, $timestamp, $hostname) = rearrange(
-        [qw{ SLICE_ID AUTHOR TIMESTAMP HOSTNAME }], @args);
+    my %val;
+    @val{@FIELD} = rearrange([ map { uc($_) } @FIELD ], @args);
 
-    $self->slice_id($slice_id);
-    $self->author($author);
-    $self->timestamp($timestamp);
-    $self->hostname($hostname);
+    local $self->{_mutable} = 'new';
+    while (my ($field, $val) = each %val) {
+        $self->$field($val);
+    }
+
+    # When not loaded from DB, there are extra constraints
+    my $sla = $self->adaptor;
+    $self->_check_fresh unless $sla && $self->dbID && $self->is_stored($sla->db);
+
+    # We don't migrate these between databases.  (Store and stay)
+    throw("Cannot instantiate SliceLock with (SliceLockAdaptor xor dbID)")
+      if $sla xor $self->dbID;
 
     return $self;
 }
 
-sub slice_id {
-    my ($self, $slice_id) = @_;
-    if ($slice_id) {
-        $self->{'slice_id'} = $slice_id;
+sub _check_fresh {
+    my ($self) = @_;
+
+    # Freshly made lock must be a "pre" lock, not "held".
+    # The locking step is done by the adaptor.
+    my $active = $self->active;
+    throw("Fresh SliceLock must have active=pre, this is active=$active")
+      unless $active eq 'pre';
+
+    throw("Fresh SliceLock must not be freed")
+      if grep {defined} ($self->freed, $self->ts_free, $self->freed_author);
+
+    # Timestamps are set by the adaptor.
+    my @ts = ($self->ts_begin, $self->ts_activity, $self->ts_free);
+    throw("Fresh SliceLock must have null timestamps, this has [@ts]")
+      if grep {defined} @ts;
+
+    return;
+}
+
+
+sub dbID {
+    my $self = shift;
+    if (@_) {
+        # write
+        my $newId = shift;
+        my $oldId = $self->SUPER::dbID();
+        throw("dbID is immutable") if defined $oldId;
+        return $self->SUPER::dbID($newId);
+    } else {
+        # read
+        return $self->SUPER::dbID();
     }
-    return $self->{'slice_id'};
 }
 
-sub author{
-   my ($self, $value) = @_;
-   if( defined $value) {
-       if ($value->isa("Bio::Vega::Author")) {
-           $self->{'author'} = $value;
-       } else {
-           throw("Argument [$value] is not a Bio::Vega::Author");
-       }
-   }
-   return $self->{'author'};
-}
-
-sub timestamp{
-   my ($obj, $value) = @_;
-   if( defined $value) {
-      $obj->{'timestamp'} = $value;
+sub adaptor {
+    my $self = shift;
+    if (@_) {
+        # write
+        my $newAdap = shift;
+        my $oldAdap = $self->SUPER::adaptor();
+        throw("adaptor is immutable") if defined $oldAdap;
+        return $self->SUPER::adaptor($newAdap);
+    } else {
+        # read
+        return $self->SUPER::adaptor();
     }
-    return $obj->{'timestamp'};
 }
 
-sub hostname {
-    my ($self, $hostname) = @_;
-    if ($hostname) {
-        $self->{'hostname'} = $hostname;
+
+sub is_held {
+    my ($self) = @_;
+    return $self->active eq 'held' && !$self->freed;
+}
+
+sub _init {
+    my ($pkg) = @_;
+    my %new_method;
+    my %author = (author => 1, freed_author => 1);
+
+    # Simple r/w accessors
+    foreach my $field (qw( seq_region_id seq_region_start seq_region_end ),
+                       qw( ts_begin ts_activity ts_free ), # unixtimes
+                       qw( active freed ),        # enums.  database will check
+                       qw( author freed_author ), # objects
+                       qw( intent hostname )) {   # text
+        $new_method{$field} = sub {
+            my ($self, $newval) = @_;
+            if (@_ > 1) { # qw( freed freed_author ts_free ) are nullable
+                # some fields would be safe to mutate and save,
+                # but currently we only allow write during new
+                throw("$field is immutable") if !$self->{_mutable};
+                throw("Argument [$newval] is not a Bio::Vega::Author")
+                  if $author{$field} && defined $newval &&
+                    !eval { $newval->isa("Bio::Vega::Author") };
+                $self->{$field} = $newval;
+            }
+            return $self->{$field};
+        };
     }
-    return $self->{'hostname'};
+
+    # Times are in unixtime.  Add extra time accessors.
+    foreach my $field (qw( ts_begin ts_activity ts_free )) {
+        $new_method{"iso8601_$field"} = sub {
+            my ($self) = @_;
+            throw("$field is read-only") if @_ > 1;
+            my $unixtime = $self->$field;
+            return __iso8601($unixtime);
+        };
+    }
+
+    while (my ($field, $code) = each %new_method) {
+        no strict 'refs';
+        *{"$pkg\::$field"} = $code;
+    }
+
+    return;
 }
 
-
+__PACKAGE__->_init;
 
 1;
 
