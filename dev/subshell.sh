7f51fdced63e3e1d896b4257f25c960d5e2669b2
Matthew Astley <mca@sanger.ac.uk>
2014-03-07 15:20:02 +0000

    scripts/client/otterlace -S : put prompt-futz in another file
    tell (branch, commit, background OTTER_HOME) used to set environment
      use this as a more accurate prompt-prefix
      because it is no longer just "otterlace_dev", and
      reminding that the environment may have frozen in some aspect of HEAD
      and this may be invalidated by checkout of another branch
    
    still likely to be invisible to MacOS

---
+++ dev/subshell.sh
@@ -0,0 +1,8 @@
+# called from tail of "scripts/client/otterlace -S"
+# to make subshell with environment
+
+export debian_chroot="${ENSEMBL_OTTER_DEV:+$( cd $ENSEMBL_OTTER_DEV && git rev-parse --abbrev-ref HEAD )@$( cd $ENSEMBL_OTTER_DEV && git rev-parse --short HEAD)+}$(basename "$OTTER_HOME")"
+# Debian-centric hack because we can't safely override PS1 in
+# subshell without cooperation of ~/.bashrc
+
+exec bash -i
