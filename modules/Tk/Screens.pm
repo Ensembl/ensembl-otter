3892fd70e6fceca5ee32717c0c3922b510cd840d
Matthew Astley <mca@sanger.ac.uk>
2014-03-13 10:52:08 +0000

    Tk::Screens - utility for directing to "the next" screen
    when there is just one screen (single head or xinerama),
    gtk_arg returns nothing
    
    the plan is to put ZMap on "the next" screen in rotation

---
+++ modules/Tk/Screens.pm
@@ -0,0 +1,92 @@
+package Tk::Screens;
+
+use strict;
+use warnings;
+use Try::Tiny;
+
+
+sub nxt {
+    my ($class, $widget) = @_;
+
+    # Cache the "next" screen on MainWindow
+    my $mw = $widget->Widget(".");
+    my $dpys = $mw->{_Tk_Screens_all} ||= $class->_probe_screens($mw);
+
+    my $next = $dpys->[0];
+    return $next;
+}
+
+
+sub _new {
+    my ($class, $display_name, $screen_num) = @_;
+    my $self = bless { name => $display_name, i => $screen_num }, $class;
+    return $self;
+}
+
+# Like ":10.1" and always meaningful.
+sub name {
+    my ($self) = @_;
+    return $self->{name};
+}
+
+# Screen number (optional)
+sub i {
+    my ($self) = @_;
+    return $self->{i};
+}
+
+# Call in list context.  If we have a choice, returns "--screen=<num>"
+# else returns empty list.
+sub gtk_arg {
+    my ($self) = @_;
+    die "wantarray" unless wantarray;
+
+    my $i = $self->i;
+    if (defined $i) {
+        return ("--screen=$i");
+    } else {
+        return ();
+    }
+}
+
+
+# Tk advertises no method to discover other screens, but we can ask
+sub _probe_screens {
+    my ($class, $mw) = @_;
+    my $current = $mw->screen;
+    my ($display, $screen) = $current =~ m{^(:\d+)\.(\d+)$};
+
+    if (!defined $display) {
+        warn "Failed to parse screen name '$current', assuming one screen";
+        return [ $class->_new($current) ];
+    } else {
+        my @dpy;
+        for (my $i=0; $i <= 255; $i++) {
+            last unless
+              try {
+                  my $name = "$display.$i";
+                  my $tl = $mw->Toplevel(-screen => $name);
+                  # still here - success
+                  $dpy[$i] = $class->_new($name, $i);
+                  $tl->destroy;
+              } catch {
+                  warn "Unexpected error in _probe_screens: $_"
+                    unless m{couldn't connect to display|bad screen number};
+                  0;
+              };
+        }
+
+        if (1 == @dpy) {
+            # One screen, for which we understand the name.
+            # There is no point specifying what to use.
+            return [ $class->_new($current) ];
+        } else {
+            # Rotate them to start from 1+$screen,
+            # so that $dpy[0] is the "next" one
+            push @dpy, splice(@dpy, 0, 1+$screen);
+            return \@dpy;
+        }
+    }
+}
+
+1;
