4e1e47f142548ca22d8919e342c4ce1b1ee18c93
Matthew Astley <mca@sanger.ac.uk>
2014-04-02 12:48:46 +0100

    Tk::ScopedBusy - extra hooks to detect unexpected things
    trying to debug strangeness in CanvasWindow::SequenceNotes::History
    but nothing found this way
    
    found that session recovery was broken by bless(undef, 'Tk::ScopedBusy::BusyFellOff')
      and the resulting error message being cleared (by something trying to preserve $@ across DESTROY calls?)
    
      beware that Busy does not always set a ->{Busy} list
        there is no return value to tell
    
    always call the Unbusy if $widget remains defined

--- modules/Tk/ScopedBusy.pm
+++ modules/Tk/ScopedBusy.pm
@@ -1,6 +1,7 @@
 package Tk::ScopedBusy;
 use strict;
 use warnings;
+use Carp 'cluck';
 
 
 =head1 NAME
@@ -24,26 +25,57 @@ sub new {
                  caller => $caller };
     bless $self, $class;
 
-    warn "double-Busy on $widget from $caller" if $widget->{'Busy'};
+    cluck "[w] double-Busy on $widget from $caller in $self\n"
+      if $widget->{'Busy'};
+
     $widget->Busy(@busy_arg);
+    my $B = $widget->{'Busy'};
+
+    if ($B) {
+        bless $B, 'Tk::ScopedBusy::BusyFellOff'; # just for DESTROY
+        # May already have been blessed, for double-Busy
+    } else {
+        # Perhaps not $widget->viewable ?
+        warn "Busy on $widget from $caller had no effect in $self\n";
+        $self->{bad_Busy} = 1;
+    }
 
     return $self;
 }
 
+our $_during_DESTROY = 0;
 sub DESTROY {
     my ($self) = @_;
     my $widget = $self->{widget} || '(GONE)';
     my $caller = $self->{caller} || '(lost)';
-    if (Tk::Exists($widget)) {
-        warn "double-Unbusy on $widget from $caller" unless $widget->{'Busy'};
-        $widget->Unbusy;
-    } else {
+    warn "[w] ineffective Busy in $self is cancelled\n" if $self->{bad_Busy};
+    if (!Tk::Exists($widget)) {
         my $was_busy =
           ($widget && $widget->{'Busy'}
-           ? ', but it was still Busy' : '');
-        warn "$widget from $caller did not exist when I came to Unbusy it$was_busy";
+           ? ' (but it was still Busy)' : '');
+        cluck "[w] $widget from $caller did not exist$was_busy when I came to Unbusy it in $self\n";
+    }
+    if ($widget) {
+        warn "[w] double-Unbusy on $widget from $caller in $self\n"
+          unless $widget->{'Busy'};
+        local $_during_DESTROY = 1;
+
+        # call it even when !Tk::Exists, because there may be other
+        # actions attached
+        $widget->Unbusy;
     }
     return;
 };
+
+# Tk::ScopedBusy::BusyFellOff object is constructed indirectly, by
+# blessing what $widget->Busy puts in $widget->{Busy}
+#
+# This DESTROY informs you if something else does the $widget->Unbusy
+sub Tk::ScopedBusy::BusyFellOff::DESTROY {
+    my ($self) = @_;
+    cluck "Busy fell off not during our DESTROY"
+      unless $Tk::ScopedBusy::_during_DESTROY;
+    return;
+}
 
 1;
