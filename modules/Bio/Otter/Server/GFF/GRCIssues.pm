48be870a123cbb081387b89c07a25d508cb9d79e
Michael Gray <mg13@sanger.ac.uk>
2014-05-14 14:42:22 +0100

    B:O:Server::GFF::GRCIssues: put all details in Name field.
    As a temporary fix to get them shown in ZMap.

--- modules/Bio/Otter/Server/GFF/GRCIssues.pm
+++ modules/Bio/Otter/Server/GFF/GRCIssues.pm
@@ -32,10 +32,20 @@ sub Bio::EnsEMBL::Slice::get_all_GRC_issues {
         $fp->end(        $issue->{end}   - $chr_start + 1 );
         $fp->strand(     1 );
 
-        $fp->add_Attribute(_make_attrib('name', $issue->{jira_id}));
+        if (0) {
 
-        my $description = sprintf '%s: %s (%s)', @{$issue}{ qw( category description status ) };
-        $fp->add_Attribute(_make_attrib('description', $description));
+            # ZMap does not take the description from the GFF, so this isn't helpful...
+            my $description = sprintf '%s: %s (%s)', @{$issue}{ qw( category description status ) };
+            $fp->add_Attribute(_make_attrib('name', $issue->{jira_id}));
+            $fp->add_Attribute(_make_attrib('description', $description));
+
+        } else {
+
+            # ...instead we put it all in the name.
+            my $name = sprintf '%s: %s - %s (%s)', @{$issue}{ qw( jira_id category description status ) };
+            $fp->add_Attribute(_make_attrib('name', $name));
+
+        }
 
         push @feature_coll, $fp;
     }
