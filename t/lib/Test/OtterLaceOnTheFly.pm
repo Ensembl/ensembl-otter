fb0d43d571677d19aa02a3b0c801b8d33c497fef
Michael Gray <mg13@sanger.ac.uk>
2014-05-28 15:18:10 +0100

    B:O:L:OnTheFly::Transcript: protein.

--- t/lib/Test/OtterLaceOnTheFly.pm
+++ t/lib/Test/OtterLaceOnTheFly.pm
@@ -111,6 +111,18 @@ my @transcript_tests = (
             'BC018923.fwd' => 'BC018923.fwd 0 2538 + EMBOSS_001 55274 35000 - 12574 M 974 974 5 0 2 I 0 2242 3 0 2 M 33 33 G 0 1 M 159 159 5 0 2 I 0 1308 3 0 2 M 55 55 G 2 0 M 110 110 5 0 2 I 0 8897 3 0 2 M 230 230 5 0 2 I 0 3909 3 0 2 M 156 156 5 0 2 I 0 758 3 0 2 M 80 80 5 0 2 I 0 599 3 0 2 M 739 739',
         },
     },
+    {
+        name        => 'test_ts vs. protein Q96S55, fwd exons',
+        target_path => "${path}/test_ts_b.fa",
+        query_path  => "${path}/Q96S55.fasta",
+        query_ids   => [qw(sp|Q96S55|WRIP1_HUMAN)],
+        ts_spec     => "${path}/exons_b.fwd.txt",
+        ts_strand   => 1,
+        vulgar      => {
+            'sp|Q96S55|WRIP1_HUMAN' => 'sp|Q96S55|WRIP1_HUMAN 0 665 . EMBOSS_001 120540 140196 + 3329 M 274 822 5 0 2 I 0 2242 3 0 2 M 11 33 F 0 1 M 53 159 5 0 2 I 0 1233 3 0 2 M 43 129 F 0 1 G 1 0 M 36 108 S 0 2 5 0 2 I 0 8897 3 0 2 S 1 1 M 76 228 S 0 1 5 0 2 I 0 3909 3 0 2 S 1 2 M 51 153 S 0 1 5 0 2 I 0 758 3 0 2 S 1 2 M 26 78 5 0 2 I 0 599 3 0 2 M 91 273',
+        },
+        type        => 'Test_Protein',
+    },
     );
 
 sub fixed_transcript_tests {
