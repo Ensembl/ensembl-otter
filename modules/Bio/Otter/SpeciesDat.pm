Ensembl/ensembl-otter

commit d9c6e171b514d5048ce4c00aaa9b6387e5ce2fc7

switch some from B:O:Lace::DataSet to B:O:SpeciesData::DataSet
done 12 of about 126, starting with the most-used

more:
  git grep get_cached_DBAdaptor scripts/

scripts/lace/query_all_datasets
@@ -5,7 +5,7 @@
 use strict;
 use warnings;
 use Bio::Otter::Lace::Defaults;
-use Bio::Otter::Lace::PipelineDB;
+use Bio::Otter::Server::Config;
 use Try::Tiny;
 
 {
@@ -27,14 +27,13 @@ use Try::Tiny;
 
     print "$sql\n";
 
-    my $cl = Bio::Otter::Lace::Defaults::make_Client();
-    foreach my $ds ($cl->get_all_DataSets) {
+#    my $cl = Bio::Otter::Lace::Defaults::make_Client();
+    foreach my $ds (Bio::Otter::Server::Config->SpeciesDat->datasets) {
         printf "Querying dataset %s\n", $ds->name;
-        my $otter_dba = $ds->get_cached_DBAdaptor;
         my $query_dba =
           $pipe_flag
-          ? Bio::Otter::Lace::PipelineDB::get_pipeline_rw_DBAdaptor($otter_dba)
-          : $otter_dba;
+          ? $ds->pipeline_dba('rw')
+          : $ds->otter_dba;
 
         my $row_count;
         if ($do_flag) {

scripts/lace/repair_intl_clone_names
@@ -5,7 +5,7 @@ use warnings;
 use strict;
 use Carp;
 use Hum::Tracking;
-use Bio::Otter::Lace::PipelineDB;
+use Bio::Otter::Server::Config;
 use Bio::Otter::Lace::Defaults;
 use Hum::Submission;
 
@@ -21,14 +21,8 @@ use Hum::Submission;
 
     my $cl = Bio::Otter::Lace::Defaults::make_Client();
 
-    my @datasets;
-
-    if ($dataset_name) {
-        push @datasets, $cl->get_DataSet_by_name($dataset_name);
-    }
-    else {
-        @datasets = $cl->get_all_DataSets();
-    }
+    my $SD = Bio::Otter::Server::Config->SpeciesDat;
+    my @datasets = $dataset_name ? ($SD->dataset($dataset_name)) : $SD->datasets;
 
     get_international_clone_name('junk'); # ensure we can, before grabbing the lock
 
@@ -45,13 +39,13 @@ use Hum::Submission;
         print STDERR "\n=================== $dataset_name ===============\n";
 
         # Make a connection to the otter database
-        my $otter_db = $ds->get_cached_DBAdaptor;
+        my $otter_db = $ds->otter_dba;
 
         eval {
-            my $dbh = $otter_db->can('dbc') ? $otter_db->dbc : $otter_db;
+            my $dbh = $ds->otter_dba->dbc;
             repair_one_db($dbh, $rescan_flag, $change_flag, $dataset_name, 'otter');
 
-            my $pipe_db = Bio::Otter::Lace::SatelliteDB::get_DBAdaptor($otter_db, 'pipeline_db_rw_head',);
+            my $pipe_db = $ds->pipeline_dba('rw');
             repair_one_db($pipe_db->dbc, $rescan_flag, $change_flag, $dataset_name, 'pipe');
         };
         warn $@ if $@;

scripts/acme/add_patch_accession_attributes
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 
 use Bio::Otter::Lace::Defaults;
-use Bio::Otter::Lace::PipelineDB;
+use Bio::Otter::Server::Config;
 
 {
     my $dataset_name = 'human';
@@ -23,9 +23,9 @@ use Bio::Otter::Lace::PipelineDB;
     my $cl = Bio::Otter::Lace::Defaults::make_Client();
     
     # DataSet interacts directly with an otter database
-    my $ds = $cl->get_DataSet_by_name($dataset_name);
+    my $ds = Bio::Otter::Server::Config->SpeciesDat->dataset($dataset_name);
     
-    my $otter_dba = $ds->get_cached_DBAdaptor;
+    my $otter_dba = $ds->otter_dba;
     my $sth = $otter_dba->dbc->prepare(q{
         SELECT sr.seq_region_id
           , sr.name

scripts/acme/assembly_similarity_features
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 
 use Bio::Otter::Lace::Defaults;
-use Bio::Otter::Lace::PipelineDB;
+use Bio::Otter::Server::Config;
 
 {
     my $dataset_name = 'sheep';
@@ -26,11 +26,11 @@ use Bio::Otter::Lace::PipelineDB;
     my $cl = Bio::Otter::Lace::Defaults::make_Client();
     
     # DataSet interacts directly with an otter database
-    my $ds = $cl->get_DataSet_by_name($dataset_name);
+    my $ds = Bio::Otter::Server::Config->SpeciesDat->dataset($dataset_name);
     
-    my $otter_dba = $ds->get_cached_DBAdaptor;
-    my $pipe_dba = Bio::Otter::Lace::PipelineDB::get_pipeline_rw_DBAdaptor($otter_dba);
-    my $ens_dba = Bio::Otter::Lace::SatelliteDB::get_DBAdaptor($otter_dba, 'ensembl_core_db_head');
+#    my $otter_dba = $ds->otter_dba;
+    my $pipe_dba = $ds->pipeline_dba('rw');
+    my $ens_dba = $ds->satellite_dba('ensembl_core_db_head');
     
     my $ens_ctg_coord_sys_id  = $ens_dba->get_CoordSystemAdaptor->fetch_by_name('contig')->dbID;
     

scripts/acme/bed_to_intron_simple_features
@@ -5,7 +5,6 @@
 use strict;
 use warnings;
 use Bio::Otter::Lace::Defaults;
-use Bio::Otter::Lace::PipelineDB;
 
 {
     my $dataset_name = 'rat';
@@ -24,10 +23,10 @@ use Bio::Otter::Lace::PipelineDB;
     my $cl = Bio::Otter::Lace::Defaults::make_Client();
     
     # DataSet interacts directly with an otter database
-    my $ds = $cl->get_DataSet_by_name($dataset_name);
+    my $ds = Bio::Otter::Server::Config->SpeciesDat->dataset($dataset_name);
     
-    my $otter_dba = $ds->get_cached_DBAdaptor;
-    my $pipe_dba = Bio::Otter::Lace::PipelineDB::get_pipeline_rw_DBAdaptor($otter_dba);
+#    my $otter_dba = $ds->otter_dba;
+    my $pipe_dba = $ds->pipeline_dba('rw');
 
     my $write_similarity_feature = $pipe_dba->dbc->prepare(q{
         INSERT simple_feature (

scripts/acme/generate_solexa_config
@@ -6,7 +6,6 @@ use strict;
 use warnings;
 use Hum::Sort qw{ ace_sort };
 use Bio::Otter::Lace::Defaults;
-use Bio::Otter::Lace::SatelliteDB;
 
 {
 
@@ -18,12 +17,12 @@ use Bio::Otter::Lace::SatelliteDB;
         ) or $usage->();
     
     # Client communicates with otter HTTP server
-    my $cl = Bio::Otter::Lace::Defaults::make_Client();
+#    my $cl = Bio::Otter::Lace::Defaults::make_Client();
     
     # DataSet interacts directly with an otter database
-    my $ds = $cl->get_DataSet_by_name('zebrafish');
+    my $ds = Bio::Otter::Server::Config->SpeciesDat->dataset('zebrafish');
     
-    my $otter_dba = $ds->get_cached_DBAdaptor;
+#    my $otter_dba = $ds->otter_dba;
 
     my $head = "\n[zebrafish.use_filters]\n";
     my $body = '';
@@ -64,7 +63,7 @@ zmap_style=solexa_genes
 };
 
         my ($meta_key) = $template =~ /metakey=(\S+)/;
-        my $meta_db = Bio::Otter::Lace::SatelliteDB::get_DBAdaptor($otter_dba, $meta_key)->dbc;
+        my $meta_db = $ds->satellite_dba($meta_key)->dbc;
 
         my $field_count = $template =~ tr/%/%/;
         foreach my $stage (sort {ace_sort($a, $b)} keys %solex) {
@@ -106,7 +105,7 @@ zmap_style=solexa_introns
 };
 
         my ($meta_key) = $template =~ /metakey=(\S+)/;
-        my $meta_db = Bio::Otter::Lace::SatelliteDB::get_DBAdaptor($otter_dba, $meta_key)->dbc;
+        my $meta_db = $ds->satellite_dba($meta_key)->dbc;
 
         my $field_count = $template =~ tr/%/%/;
         foreach my $stage (sort {ace_sort($a, $b)} keys %solex) {
@@ -144,7 +143,7 @@ zmap_style=solexa_coverage
 };
 
         my ($meta_key) = $template =~ /metakey=(\S+)/;
-        my $meta_db = Bio::Otter::Lace::SatelliteDB::get_DBAdaptor($otter_dba, $meta_key)->dbc;
+        my $meta_db = $ds->satellite_dba($meta_key)->dbc;
 
         my $field_count = $template =~ tr/%/%/;
         foreach my $stage (sort {ace_sort($a, $b)} keys %solex) {

scripts/acme/list_gene_biotypes.pl
@@ -21,10 +21,10 @@ use Bio::Otter::Server::Config;
     $usage->() unless $dataset_name;
     
     # Client communicates with otter HTTP server
-    my $cl = Bio::Otter::Lace::Defaults::make_Client();
+#    my $cl = Bio::Otter::Lace::Defaults::make_Client();
     
     # DataSet interacts directly with an otter database
-    my $ds = $cl->get_DataSet_by_name($dataset_name);
+    my $ds = Bio::Otter::Server::Config->SpeciesDat->dataset($dataset_name);
 
     my $chr_list = join(', ', map { "'$_'" } (1..22, 'X', 'Y'));
 
@@ -62,7 +62,7 @@ use Bio::Otter::Server::Config;
           AND sra_chr.value in ($chr_list)
     };
     die $sql_query;
-    my $dba = $ds->get_cached_DBAdaptor;
+    my $dba = $ds->otter_dba;
     my $dbc = $dba->dbc;
     my $sth = $dbc->prepare($sql_query);
     $sth->execute;

scripts/loutre/change_set_attribs.pl
@@ -235,16 +235,10 @@ sub otter_client_dbhs {
     my ($dataset_name, $sel_loutre, $sel_pipe) = @_;
     die "expect list context" unless wantarray;
 
-    require Bio::Otter::Lace::Defaults;
-    require Bio::Otter::Lace::PipelineDB;
-    local @ARGV = (); Bio::Otter::Lace::Defaults::do_getopt();
-
-    # Client communicates with otter HTTP server
-    my $cl = Bio::Otter::Lace::Defaults::make_Client();
-    my $ds = $cl->get_DataSet_by_name($dataset_name);
-
-    my $otter_dba = $ds->get_cached_DBAdaptor;
-    my $pipe_dba = Bio::Otter::Lace::PipelineDB::get_rw_DBAdaptor($otter_dba);
+    require Bio::Otter::Server::Config;
+    my $ds = Bio::Otter::Server::Config->SpeciesDat->dataset($dataset_name);
+    my $otter_dba = $ds->otter_dba;
+    my $pipe_dba = $ds->pipeline_dba('rw');
 
     my @dbh;
     push @dbh, $otter_dba->dbc->db_handle if $sel_loutre;

commit 32a8d930fc8fb5c9e37aad9ecfa847e8d76f2e6f

B:O:SD->datasets - list vs. arrayref issue
mend scripts broken in d9c6e171

deprecate existing ->datasets method (arrayref)
replace existing callers with all_datasets method (list)

fixes RT#424647 more

modules/Bio/Otter/SpeciesDat.pm
@@ -60,7 +60,15 @@ sub dataset {
 
 sub datasets {
     my ($self) = @_;
+    carp 'B:O:SD->datasets deprecated';
+    # because it provides write access to internals,
+    # and (surprising within e-o) arrayref semantics instead of list
     return $self->{_datasets};
+}
+
+sub all_datasets {
+    my ($self) = @_;
+    return @{ $self->{_datasets} };
 }
 
 sub _dataset_hash {

modules/Bio/Otter/Utils/Script.pm
@@ -241,7 +241,7 @@ sub execute {
 
     my @ds_names = $self->_datasets;
     unless (@ds_names) {
-        @ds_names = map { $_->name } @{$species_dat->datasets};
+        @ds_names = map { $_->name } $species_dat->all_datasets;
     }
 
     foreach my $ds_name (sort @ds_names) {

scripts/lace/query_all_datasets
@@ -28,7 +28,7 @@ use Try::Tiny;
     print "$sql\n";
 
 #    my $cl = Bio::Otter::Lace::Defaults::make_Client();
-    foreach my $ds (Bio::Otter::Server::Config->SpeciesDat->datasets) {
+    foreach my $ds (Bio::Otter::Server::Config->SpeciesDat->all_datasets) {
         printf "Querying dataset %s\n", $ds->name;
         my $query_dba =
           $pipe_flag

scripts/lace/repair_intl_clone_names
@@ -22,7 +22,7 @@ use Hum::Submission;
     my $cl = Bio::Otter::Lace::Defaults::make_Client();
 
     my $SD = Bio::Otter::Server::Config->SpeciesDat;
-    my @datasets = $dataset_name ? ($SD->dataset($dataset_name)) : $SD->datasets;
+    my @datasets = $dataset_name ? ($SD->dataset($dataset_name)) : $SD->all_datasets;
 
     get_international_clone_name('junk'); # ensure we can, before grabbing the lock
 

scripts/lace/satellite_switch
@@ -49,8 +49,8 @@ $PORTS_RE = qr{\b($PORTS_RE)\b};
         ) or $usage->();
     $usage->() unless $switch;
 
-    my $ds_list = Bio::Otter::Server::Config->SpeciesDat->datasets;
-    foreach my $ds (sort { $a->name cmp $b->name } @$ds_list) {
+    foreach my $ds (sort { $a->name cmp $b->name }
+                    Bio::Otter::Server::Config->SpeciesDat->all_datasets) {
         my $name = $ds->name;
         my $dba = $ds->otter_dba;
         my $meta = $dba->get_MetaContainer;

modules/Test/Otter.pm
@@ -425,7 +425,7 @@ sub get_BOSDatasets {
     my $bosc_sd = Bio::Otter::Server::Config->SpeciesDat;
     warn "No datasets requested" unless @name;
     die "wantarray" unless wantarray;
-    return @{ $bosc_sd->datasets } if "@name" eq 'ALL';
+    return $bosc_sd->all_datasets if "@name" eq 'ALL';
     return map { $bosc_sd->dataset($_) or die "No such dataset '$_'" } @name;
 }
 

t/obtain-db.t
@@ -225,9 +225,9 @@ sub client_tt {
 }
 
 sub readonly_ds_tt {
-    my $dslist = SpeciesDat()->datasets;
-    plan tests => 10 * @$dslist;
-    foreach my $ds (@$dslist) {
+    my @all_ds = SpeciesDat()->all_datasets;
+    plan tests => 10 * @all_ds;
+    foreach my $ds (@all_ds) {
         my $name = $ds->name;
         my $ds_ro = $ds->clone_readonly;
         my $ok = 1;
@@ -483,7 +483,8 @@ sub _dba2subslice {
 sub equivs_tt { # transient
     my $fn = Bio::Otter::Server::Config->data_filename('species.dat');
     my $new = Bio::Otter::SpeciesDat->new($fn);
-    plan tests => 1 + @{ $new->datasets };
+    my @new_all = $new->all_datasets;
+    plan tests => 1 + @new_all;
 
     my $old = Bio::Otter::SpeciesDat->new__old($fn);
     # When this method is gone or fails for want of legacy fields, we
@@ -491,7 +492,7 @@ sub equivs_tt { # transient
 
 #    # Paper over some CNAME vs. canonical hostname differences
 #    my %munge = qw( lutra7 otterpipe2 lutra5 otterpipe1 );
-#    foreach my $db (@{ $old->datasets }) {
+#    foreach my $db ($old->all_datasets) {
 #        foreach my $k (qw( HOST DNA_HOST )) {
 #            my $p = $db->ds_all_params;
 #            my $replace = $munge{ $p->{$k} };
@@ -504,7 +505,7 @@ sub equivs_tt { # transient
 #
 ### Not required, it was a data bug.  Left in case it becomes...  a feature.
 
-    foreach my $ds_name (sort map { $_->name } @{ $new->datasets }) {
+    foreach my $ds_name (sort map { $_->name } @new_all) {
         is_deeply($old->dataset($ds_name),
                   $new->dataset($ds_name),
                   "Dataset $ds_name");