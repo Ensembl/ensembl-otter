a19b0995451c9cc17e231ec96d9aa182c209a3b3
Michael Gray <mg13@sanger.ac.uk>
2014-11-05 15:57:43 +0000

    OnTheFly: put FASTA files in a subdir of /tmp.

--- modules/Bio/Otter/Lace/OnTheFly/FastaFile.pm
+++ modules/Bio/Otter/Lace/OnTheFly/FastaFile.pm
@@ -6,19 +6,24 @@ use Moose::Role;                # THIS IS A ROLE, not a class
 requires 'seqs_for_fasta';
 requires 'description_for_fasta';
 
+use File::Path;
+use File::Spec;
 use File::Temp;
+
 use Hum::FastaFileIO;
 
 has 'fasta_file'   => ( is => 'ro', isa => 'File::Temp',
                         lazy => 1, builder => '_build_fasta_file', init_arg => undef );
 
+has 'fasta_dir'    => ( is => 'ro', isa => 'Str',
+                        lazy => 1, builder => '_build_fasta_dir',   init_arg => undef );
+
 sub _build_fasta_file {         ## no critic (Subroutines::ProhibitUnusedPrivateSubroutines)
     my $self = shift;
 
-    my $template = sprintf("otf_%s_%d_XXXXX", $self->description_for_fasta, $$);
+    my $template = sprintf("%s/otf_%s_%d_XXXXX", $self->fasta_dir, $self->description_for_fasta, $$);
     my $file = File::Temp->new(
         TEMPLATE => $template,
-        TMPDIR   => 1,
         SUFFIX   => '.fa',
         UNLINK   => 0,
         );
@@ -28,6 +33,15 @@ sub _build_fasta_file {         ## no critic (Subroutines::ProhibitUnusedPrivate
     $ts_out = undef;            # flush
 
     return $file;
+}
+
+sub _build_fasta_dir {
+    my $self = shift;
+    my $tmp  = File::Spec->tmpdir;
+    my $user = getpwuid($<);
+    my $path = sprintf('%s/otter_otf_%s', $tmp, $user);
+    File::Path::make_path($path);
+    return $path;
 }
 
 1;
