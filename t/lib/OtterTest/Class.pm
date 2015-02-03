68bbd91fca6964201f996d22b39a5a167140e1dd
Michael Gray <mg13@sanger.ac.uk>
2015-02-03 12:29:29 +0000

    Refactor common code into OtterTest::Class parent.

---
+++ t/lib/OtterTest/Class.pm
@@ -0,0 +1,124 @@
+package OtterTest::Class;
+
+use Test::Class::Most           # autmogically becomes our parent
+    is_abstract => 1,
+    attributes  => our_object;
+
+use parent 'Class::Data::Inheritable';
+
+use lib "${ENV{ANACODE_TEAM_TOOLS}}/t/tlib";
+use Test::CriticModule;
+
+BEGIN {
+    __PACKAGE__->mk_classdata('class');
+}
+
+sub is_abstract {
+    my $test = shift;
+    return Test::Class::Most->is_abstract($test);
+}
+
+sub startup : Tests(startup => 1) {
+    my $test  = shift;
+    return 'abstract base class' if $test->is_abstract;
+
+    ( my $class = ref $test ) =~ s/^Test:://;
+
+    use_ok $class or die;
+    $test->class($class);
+
+    return;
+}
+
+sub setup : Tests(setup) {
+    my $test = shift;
+    return if $test->is_abstract;
+
+    my $class = $test->class;
+    $test->our_object($class->new);
+    return;
+}
+
+sub attributes { return undef }
+
+sub _critic : Test(1) {
+    my $test = shift;
+    return 'abstract base class' if $test->is_abstract;
+
+    my $class = $test->class;
+    critic_module_ok($class);
+    return;
+}
+
+sub constructor : Test(3) {
+    my $test = shift;
+    return 'abstract base class' if $test->is_abstract;
+
+    my $class = $test->class;
+    can_ok $class, 'new';
+    ok my  $cs = $class->new, '... and the constructor should succeed';
+    isa_ok $cs,  $class,      '... and the object it returns';
+    return;
+}
+
+sub test_attributes : Tests {
+    my $test = shift;
+    return 'abstract base class' if $test->is_abstract;
+
+    my $attributes = $test->attributes;
+    return 'no attributes' unless $attributes;
+
+    $test->num_tests((scalar keys %$attributes)*3);
+
+    foreach my $a ( keys %$attributes ) {
+        $test->_attribute($a, $attributes->{$a});
+    }
+    return;
+}
+
+sub _attribute {
+    my ($test, $attribute, $expected) = @_;
+
+    $test->setup;
+    my $obj = $test->our_object;
+
+    can_ok $obj, $attribute;
+    ok ! defined $obj->$attribute, "...and '$attribute' should start out undefined";
+    $test->set_attributes;
+    is $obj->$attribute, $expected,'...and setting its value should succeed';
+
+    return;
+}
+
+sub set_attributes {
+    my $test = shift;
+    my $obj = $test->our_object;
+    my $attributes = $test->attributes;
+    foreach my $a ( keys %$attributes ) {
+        $obj->$a($attributes->{$a});
+    }
+    return;
+}
+
+sub attributes {
+    my $test = shift;
+
+    my $attributes = $test->{attributes};
+    return $attributes if $attributes;
+
+    $attributes = { %{$test->build_attributes} }; # make a copy we can manipulate
+    foreach my $a ( keys %$attributes ) {
+        my $val_or_sub = $attributes->{$a};
+        my $ref = ref $val_or_sub;
+        if ($ref and $ref eq 'CODE') {
+            $val_or_sub = &$val_or_sub($test);
+            $attributes->{$a} = $val_or_sub;
+        }
+    }
+
+    return $test->{attributes} = $attributes;
+}
+
+sub build_attributes { die 'build_attributes() must be provided by child class.' }
+
+1;
