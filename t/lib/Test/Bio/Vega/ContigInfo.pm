79165da25b7c8e4de4caf77b0ff9b7da3ddea24c
Michael Gray <mg13@sanger.ac.uk>
2015-02-04 16:02:02 +0000

    Test::Bio::Vega::ContigInfo->attributes()

--- t/lib/Test/Bio/Vega/ContigInfo.pm
+++ t/lib/Test/Bio/Vega/ContigInfo.pm
@@ -3,6 +3,8 @@ package Test::Bio::Vega::ContigInfo;
 use Test::Class::Most
     parent     => 'OtterTest::Class';
 
+use Bio::EnsEMBL::Attribute;
+
 sub build_attributes {
     my $test = shift;
     return {
@@ -12,6 +14,26 @@ sub build_attributes {
         author  => sub { return bless {}, 'Bio::Vega::Author' },
         created_date => '2015-02-03 04:05:06',
     };
+}
+
+sub attributes : Test(6) {
+    my $test = shift;
+    my $ci = $test->our_object;
+
+    can_ok $ci, 'get_all_Attributes';
+    is_deeply $ci->get_all_Attributes, [], '...and list starts empty';
+
+    can_ok $ci, 'add_Attributes';
+    $ci->add_Attributes( bless { code => 'test' }, 'Bio::EnsEMBL::Attribute' );
+    my $attr = $ci->get_all_Attributes;
+    is scalar @$attr, 1, '...and we can add an attribute';
+    $ci->add_Attributes(
+        bless({ code => 'test_x' }, 'Bio::EnsEMBL::Attribute'),
+        bless({ code => 'test' },   'Bio::EnsEMBL::Attribute'),
+        );
+    is scalar @{$ci->get_all_Attributes},         3, '...and we can add multiple attributes';
+    is scalar @{$ci->get_all_Attributes('test')}, 2, '...and we can get them by attrib code';
+    return;
 }
 
 1;
