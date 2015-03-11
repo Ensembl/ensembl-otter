79293eebda18dbe8d055897f6ace10a8504ca87d
Michael Gray <mg13@sanger.ac.uk>
2015-03-11 14:30:47 +0000

    Bio::Vega::Tiler: produce assembly tile features.

---
+++ modules/Bio/Vega/Tiler.pm
@@ -0,0 +1,50 @@
+package Bio::Vega::Tiler;
+
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::FeaturePair;
+
+sub new {
+    my ($pkg, $slice) = @_;
+    return bless { slice => $slice }, $pkg;
+}
+
+sub slice {
+    my ($self, @args) = @_;
+    ($self->{'slice'}) = @args if @args;
+    my $slice = $self->{'slice'};
+    return $slice;
+}
+
+sub tile_features {
+    my ($self) = @_;
+    my $slice = $self->slice;
+
+    my @features;
+    foreach my $tile (@{ $slice->project('seqlevel') }) {
+        my $tile_slice = $tile->to_Slice;
+        my $sf = Bio::EnsEMBL::FeaturePair->new(
+            -start         => $tile->from_start,
+            -end           => $tile->from_end,
+            -strand        => 1,
+            -slice         => $slice,
+            -hseqname      => $tile_slice->seq_region_name,
+            -hstart        => $tile_slice->start,
+            -hend          => $tile_slice->end,
+            -hstrand       => $tile_slice->strand,
+            -score         => $tile_slice->seq_region_Slice->length, # abused to pass clone length
+            );
+        push @features, $sf;
+    }
+    return @features;
+}
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Ana Code B<email> anacode@sanger.ac.uk
+
