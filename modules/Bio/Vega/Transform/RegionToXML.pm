e8d0de446bb68e9f8fcabddf41852293bfdfc2bd
Michael Gray <mg13@sanger.ac.uk>
2015-03-13 14:37:10 +0000

    Bio::Vega::Transform::XML => Bio::Vega::Transform::RegionToXML

--- modules/Bio/Vega/Transform/RegionToXML.pm
+++ modules/Bio/Vega/Transform/RegionToXML.pm
@@ -1,8 +1,5 @@
 
-# This module is badly named.  It is nothing to do with Bio::Vega::Transform
-# It does not parse XML, it creates XML.
-
-package Bio::Vega::Transform::XML;
+package Bio::Vega::Transform::RegionToXML;
 
 use strict;
 use warnings;
@@ -401,7 +398,7 @@ sub generate_FeatureSet {
 
 __END__
 
-=head1 NAME - Bio::Vega::Transform::XML
+=head1 NAME - Bio::Vega::Transform::RegionToXML
 
 =head1 AUTHOR
 
