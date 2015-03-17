9deae809158afaf6efaf5651de8fded7dd6eaef8
Michael Gray <mg13@sanger.ac.uk>
2015-03-17 11:49:17 +0000

    Test::Bio::Vega::{Gene,Transcript}: quash 'undefined' warnings from Test::Region.

--- t/lib/Test/Bio/Vega/Transcript.pm
+++ t/lib/Test/Bio/Vega/Transcript.pm
@@ -49,7 +49,7 @@ sub evidence_list : Test(4) {
 sub matches_parsed_xml {
     my ($test, $parsed_xml, $description) = @_;
     my $ts = $test->our_object;
-    my $ts_info = OtterTest::TestRegion->new->transcript_info_lookup($parsed_xml->{stable_id});
+    my $ts_info = OtterTest::TestRegion->new(0)->transcript_info_lookup($parsed_xml->{stable_id});
     note "stable_id '$parsed_xml->{stable_id}'";
     $test->attributes_are($ts,
                           {
