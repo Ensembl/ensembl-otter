df8889ca46a5ac90d91031c5b36a5bf7b4d1affd
Michael Gray <mg13@sanger.ac.uk>
2015-04-14 13:47:23 +0100

    Test::Bio::Vega::Region::Ace: actually test make_ace_string()

--- t/lib/Test/Bio/Vega/Region/Ace.pm
+++ t/lib/Test/Bio/Vega/Region/Ace.pm
@@ -1,8 +1,22 @@
 package Test::Bio::Vega::Region::Ace;
 
 use Test::Class::Most
-    parent     => 'OtterTest::Class';
+    parent     => 'Test::Bio::Vega';
 
-sub build_attributes { return; } # no test_attributes tests required
+sub test_bio_vega_features { return { test_region => 1, parsed_region => 1 }; }
+sub build_attributes       { return; } # no test_attributes tests required
+
+sub make_ace_string : Tests {
+    my $test = shift;
+
+    my $bvra = $test->our_object;
+    can_ok $bvra, 'make_ace_string';
+
+    my $ace = $bvra->make_ace_string($test->parsed_region);
+    ok ($ace, '... produces output');
+    note ("ace_string (first 200 chrs):\n", substr($ace, 0, 200));
+
+    return;
+}
 
 1;
