4a9d28df791f3d7ea4933b75c52f42af7cd0fbef
Michael Gray <mg13@sanger.ac.uk>
2015-04-29 11:18:20 +0100

    Test::Bio::Vega::Region::Ace: load expected assembly via AceDatabase, compare ace_strings.

--- t/lib/Test/Bio/Vega/Region/Ace.pm
+++ t/lib/Test/Bio/Vega/Region/Ace.pm
@@ -3,6 +3,10 @@ package Test::Bio::Vega::Region::Ace;
 use Test::Class::Most
     parent     => 'Test::Bio::Vega';
 
+use File::Temp qw( tempdir );
+
+use OtterTest::AceDatabase;
+
 sub test_bio_vega_features { return { test_region => 1, parsed_region => 1 }; }
 sub build_attributes       { return; } # no test_attributes tests required
 
@@ -25,15 +29,25 @@ sub make_assembly : Tests {
     my $bvra = $test->our_object;
     can_ok $bvra, 'make_assembly';
 
+    my $tmpdir = tempdir('B:V:R:Ace.make_assembly.XXXXXX', TMPDIR => 1, CLEANUP => 1);
+
+    my $adb = OtterTest::AceDatabase->new_from_region(
+        "$tmpdir/acedb",
+        'B:V:R:Ace.make_assembly',
+        $test->parsed_region,
+        );
+    my $ea = $adb->fetch_assembly;
+
     my $ha = $bvra->make_assembly(
         $test->parsed_region,
         {
             name             => $test->test_region->xml_parsed->{'sequence_set'}->{'assembly_type'}, # FIXME
-            MethodCollection => bless {}, 'Hum::Ace::MethodCollection', # FIXME
+            MethodCollection => $adb->MethodCollection,
         },
         );
     isa_ok($ha, 'Hum::Ace::Assembly', '...and result of make_assembly()');
-    note("ace_string:\n", $ha->ace_string);
+
+    eq_or_diff($ha->ace_string, $ea->ace_string, '...and ace_string matches');
 
     return;
 }
