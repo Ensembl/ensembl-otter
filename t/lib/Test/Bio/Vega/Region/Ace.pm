2721963b2b01bc01a13978c375764d5e07d19ccd
Michael Gray <mg13@sanger.ac.uk>
2015-04-30 16:11:46 +0100

    B:V:Region::Ace: make_assembly() now matches express_data_fetch from AceDB.
    Test does deep compare of subseqs and clones.

--- t/lib/Test/Bio/Vega/Region/Ace.pm
+++ t/lib/Test/Bio/Vega/Region/Ace.pm
@@ -3,7 +3,7 @@ package Test::Bio::Vega::Region::Ace;
 use Test::Class::Most
     parent     => 'Test::Bio::Vega';
 
-use File::Temp qw( tempdir );
+use File::Temp qw( tempdir tempfile );
 
 use OtterTest::AceDatabase;
 
@@ -22,7 +22,8 @@ sub make_ace_string : Tests {
 
     my $ace = $bvra->make_ace_string($test->parsed_region);
     ok ($ace, '... produces output');
-    note ("ace_string (first 2000 chrs):\n", substr($ace, 0, 2000));
+
+    $test->_save_ace('make_ace_string', $ace);
 
     return;
 }
@@ -42,6 +43,10 @@ sub make_assembly : Tests {
         );
     my $ea = $adb->fetch_assembly;
 
+    # $adb has already fetched DNA, so we pluck it from there for our test region.
+    # YUCK: playing with EnsEMBL internals:
+    $test->parsed_region->slice->{'seq'} = $ea->Sequence->sequence_string;
+
     my $ha = $bvra->make_assembly(
         $test->parsed_region,
         {
@@ -51,7 +56,8 @@ sub make_assembly : Tests {
         );
     isa_ok($ha, 'Hum::Ace::Assembly', '...and result of make_assembly()');
 
-    eq_or_diff($ha->ace_string, $ea->ace_string, '...and ace_string matches');
+    my $ace_string = $ha->ace_string;
+    eq_or_diff($ace_string, $ea->ace_string, '...and ace_string matches');
 
     my @e_clones = $ea->get_all_Clones;
     my @h_clones = $ha->get_all_Clones;
@@ -63,7 +69,10 @@ sub make_assembly : Tests {
                 fail "clone[$i] missing";
                 next;
             }
-            eq_or_diff($h_clones[$i]->ace_string, $e_clones[$i]->ace_string, "clone[$i] ace_string");
+            my $clone_ace_string = $h_clones[$i]->ace_string;
+            $ace_string .= $clone_ace_string;
+            eq_or_diff($clone_ace_string, $e_clones[$i]->ace_string, "clone[$i] ace_string");
+            cmp_deeply($h_clones[$i], $e_clones[$i], "clone[$i] deeply");
         }
     };
 
@@ -77,10 +86,26 @@ sub make_assembly : Tests {
                 fail "SubSeq[$i] missing";
                 next;
             }
-            eq_or_diff($h_subseqs[$i]->ace_string, $e_subseqs[$i]->ace_string, "SubSeq[$i] ace_string");
+            my $substr_ace_string = $h_subseqs[$i]->ace_string;
+            $ace_string .= $substr_ace_string;
+            eq_or_diff($substr_ace_string, $e_subseqs[$i]->ace_string, "SubSeq[$i] ace_string");
+            cmp_deeply($h_subseqs[$i], $e_subseqs[$i], "SubSeq[$i] deeply");
         }
     };
 
+    $test->_save_ace('make_assembly', $ace_string);
+
+    return;
+}
+
+sub _save_ace {
+    my ($test, $desc, $ace_string) = @_;
+
+    my ($fh, $filename) = tempfile("B:V:R:Ace.${desc}.XXXXXX", TMPDIR => 1, CLEANUP => 0, SUFFIX => '.ace');
+    print $fh $ace_string;
+    close $fh;
+
+    diag("Ace for '$desc' saved to: $filename");
     return;
 }
 
