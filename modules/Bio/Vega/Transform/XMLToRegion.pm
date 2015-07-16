c2a5813d9b5ffad9b5d86e18af9a98e7bd71bcdf
Michael Gray <mg13@sanger.ac.uk>
2015-07-16 17:18:22 +0100

    Add gene source/prefix to analysis name.

--- modules/Bio/Vega/Transform/XMLToRegion.pm
+++ modules/Bio/Vega/Transform/XMLToRegion.pm
@@ -632,10 +632,16 @@ sub _build_Locus {              ## no critic (Subroutines::ProhibitUnusedPrivate
     foreach my $transcript (@$transcripts){
         $transcript->start($transcript->start - $slice_offset);
         $transcript->end(  $transcript->end   - $slice_offset);
+
+        my $logic_name = $transcript->analysis->logic_name;
+        if ($self->analysis_from_transcript_class and $source ne 'havana' and $logic_name ne 'Otter') {
+            $logic_name = sprintf('%s:%s', $source, $logic_name);
+        }
         if ($truncated) {
             # update analysis to _trunc version
-            my $logic_name = $transcript->analysis->logic_name;
             $logic_name .= '_trunc';
+        }
+        if ($logic_name ne $transcript->analysis->logic_name) {
             $transcript->analysis($self->_get_Analysis($logic_name));
         }
     }
