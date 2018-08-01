2f217d2140c77d38cac389e8f50cc658193ae7c6
Shamika Mohanan <smm@ebi.ac.uk>
2018-08-01 12:02:32 +0100

    Modifications based on review feedback

--- modules/Bio/Otter/Server/UserSpecies.pm
+++ modules/Bio/Otter/Server/UserSpecies.pm
@@ -3,20 +3,23 @@ package Bio::Otter::Server::UserSpecies;
 use strict;
 use warnings;
 use DBI;
-use Data::Dumper;
+
 sub species_group{
 
  # MySQL database configurations
  my $dsn = "DBI:mysql:otter_registration";
- my $username = 'root';
- my $password = 'sorciers';
+ my $username = '****';
+ my $password = '****';
+
+ my %attr = (PrintError=>0, RaiseError=>1);
 
  # Connect to MySQL database
- my $dbh = DBI->connect($dsn,$username,$password);
+ my $dbh = DBI->connect($dsn, $username, $password, \%attr);
  
  # Set up hash of species for each user
  my %final_species_groups = query_links($dbh);
-
+ 
+ # Setting up 'dev','main', 'restricted' and 'mouse_strain' species groups
  $final_species_groups{'species_groups'}{'dev'} = ['human_dev', 'human_test'];
  $final_species_groups{'species_groups'}{'main'} = ['c_elegans', 'cat', 'chicken', 'chimp', 'cow', 'dog', 'drosophila', 'gibbon', 'gorilla', 'human', 'lemur', 'marmoset', 'medicago', 'mouse', 'mus_spretus', 'opossum', 'pig', 'platypus', 'rat', 'sheep', 'sordaria', 'tas_devil', 'tomato', 'tropicalis', 'wallaby', 'zebrafish']; 
  $final_species_groups{'species_groups'}{'mouse_strains'} = ['mouse-SPRET-EiJ', 'mouse-PWK-PhJ', 'mouse-CAST-EiJ', 'mouse-WSB-EiJ', 'mouse-NZO-HlLtJ', 'mouse-C57BL-6NJ', 'mouse-NOD-ShiLtJ', 'mouse-FVB-NJ', 'mouse-DBA-2J', 'mouse-CBA-J', 'mouse-C3H-HeJ', 'mouse-AKR-J', 'mouse-BALB-cJ', 'mouse-A-J', 'mouse-LP-J', 'mouse-129S1-SvImJ', 'mouse-C57BL-6NJ_v1_test'];
@@ -31,15 +34,15 @@ sub query_links{
  my ($dbh) = @_;
  my %data_group;
  
-  #Query the database
+  # Query the database
   my $sql_user_group = "SELECT username, species_write, species_read FROM otter_user";
-  my $sth_user_group = $dbh->prepare($sql_user_group);
-  $sth_user_group->execute();
-  while(my $array_ref = $sth_user_group->fetchrow_arrayref()){
-        my @temp_array_write = ();
-        my @temp_array_read = ();
-
-        #Setting up read and write species for each user
+  my $sth_user_group = $dbh->prepare($sql_user_group)
+                       or die "Could not prepare statement: " . $dbh->errstr;
+  $sth_user_group->execute()
+                       or die "Could not execute statement: " . $dbh->errstr;
+  if(my $array_ref = $sth_user_group->fetchrow_arrayref()){
+        
+        # Setting up read and write species for each user
         my $username = $array_ref->[0];
         my $species_write_string = $array_ref->[1];  
         my @species_write_array = split ',', $species_write_string; 
@@ -48,6 +51,9 @@ sub query_links{
         $data_group{'user_groups'}{$username.'.data'}{'users'} = $username; 
         $data_group{'user_groups'}{$username.'.data'}{'write'} = \@species_write_array; 
 #        $data_group{'user_groups'}{$username.'.data'}{'read'} = \@species_read_array; #Uncomment this line when READONLY datasets are available
+  }
+  else{
+     die "Database fetch statement failed";
   }
 
   $sth_user_group->finish();            
