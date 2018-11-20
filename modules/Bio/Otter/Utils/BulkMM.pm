0d1dbe92049334057d31dccbefa335a879b0ed9b
Mira <mira@ebi.ac.uk>
2018-11-20 14:15:05 +0000

    MM db setings changed

--- modules/Bio/Otter/Utils/BulkMM.pm
+++ modules/Bio/Otter/Utils/BulkMM.pm
@@ -59,9 +59,9 @@ Readonly my %CLASS_TO_SOURCE => (
     );
 
 Readonly my %DEFAULT_OPTIONS => (
-    host => '192.168.0.21',
+    host => '192.168.0.13',
     port => 3306,
-    user => 'mmreadonly',
+    user => 'mm_readonly',
     name => 'mm_ini',
     db_categories => [ @SEQ_DB_CATEGORIES ],
     );
@@ -615,4 +615,3 @@ __END__
 =head1 AUTHOR
 
 Ana Code B<email> anacode@sanger.ac.uk
-
