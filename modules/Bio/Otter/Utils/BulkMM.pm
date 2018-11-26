238bfd5defc6071f6b858051e952672f33d1ece3
s-mm <smm@ebi.ac.uk>
2018-11-26 12:52:23 +0000

    Changes made to access MM db and fix BAM script authorization issue

--- modules/Bio/Otter/Utils/BulkMM.pm
+++ modules/Bio/Otter/Utils/BulkMM.pm
@@ -37,11 +37,11 @@ All cells in the entry.accession columns match
 # NB: databases will be searched in the order in which they appear in this list
 
 Readonly my @SEQ_DB_CATEGORIES => qw(
-    emblnew
-    emblrelease
-    uniprot
-    uniprot_archive
-    refseq
+    emnew_20160426
+    embl_132
+    uniprot_2018_01
+    uniprot_2018_01
+    refseq_80
 );
 
 Readonly my $UNIPARC => 'uniprot_archive';
@@ -59,9 +59,9 @@ Readonly my %CLASS_TO_SOURCE => (
     );
 
 Readonly my %DEFAULT_OPTIONS => (
-    host => '192.168.0.21',
-    port => 3306,
-    user => 'mmreadonly',
+    host => '193.62.52.185',
+    port => 3310,
+    user => 'mm_readonly',
     name => 'mm_ini',
     db_categories => [ @SEQ_DB_CATEGORIES ],
     );
