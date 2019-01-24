734c07b3dc51ec934f9f6fb90dd8b67475c6f9a5
mira <mira@ebi.ac.uk>
2019-01-24 13:41:46 +0000

    Merge with fetch db

--- modules/Bio/Otter/Lace/Client.pm
+++ modules/Bio/Otter/Lace/Client.pm
@@ -1139,7 +1139,7 @@ sub fetch_fasta_seqence {
     my ($self, $acc) = @_;
     use Data::Dumper;
     my $datasets_hash = $self->otter_response_content
-        ('GET', 'get_sequence', {'id'=>$acc, 'author' => $self->author});
+        ('GET', 'get_fasta_sequence', {'id'=>$acc, 'author' => $self->author});
 
     return $datasets_hash;
 }
