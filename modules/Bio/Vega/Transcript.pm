a5038b80f3076915697eb0b92a31f4c08ae5fcbd
Thibaut Hourlier <thibaut@ebi.ac.uk>
2019-04-10 12:53:24 +0100

    Add status method to Bio::Vega::Gene and Bio::Vega::Transcript
    The status column has been removed in the Ensembl schema 90 but
    Otter/Loutre is still using the information. To be able to use the
    latest Ensembl code we need to have this method in these two objects

--- modules/Bio/Vega/Transcript.pm
+++ modules/Bio/Vega/Transcript.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use Bio::EnsEMBL::Utils::Argument qw ( rearrange );
 use Bio::EnsEMBL::Utils::Exception qw(throw warning);
+use Bio::EnsEMBL::Attribute;
 use Bio::Vega::Utils::AttributesMixin;
 use Bio::Vega::Evidence;
 use Bio::Vega::Translation;
@@ -292,6 +293,28 @@ sub last_db_version {
         $self->{_last_db_version} = shift @args;
     }
     return $self->{_last_db_version};
+}
+
+sub status {
+  my ($self, $status) = @_;
+
+  my $attributes = $self->get_all_Attributes('status');
+  if ($status) {
+    $self->{status} = $status;
+    if (@$attributes) {
+      $attributes->[0]->value($status);
+    }
+    else {
+      $self->add_Attributes(Bio::EnsEMBL::Attribute->new(-code => 'status', -value => $status));
+    }
+  }
+  elsif (!$self->{status} and @$attributes) {
+    if (@$attributes > 1) {
+      warning('You have multiple status attributes, using the first one '.$attributes->[0]);
+    }
+    $self->{status} = $attributes->[0]->value;
+  }
+  return $self->{status};
 }
 
 
