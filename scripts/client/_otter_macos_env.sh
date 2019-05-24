a6e8e66e2e8eb1bc9a9f8d1b7a5d8695a23b25e8
Thibaut Hourlier <thibaut@ebi.ac.uk>
2019-05-24 12:00:23 +0100

    Remove call to pango-querymodules
    pango-querymodules has been removed since version 1.38 and stuff has
    been statically compiled into the library. So there is no need for this
    code anymore

--- scripts/client/_otter_macos_env.sh
+++ scripts/client/_otter_macos_env.sh
@@ -48,20 +48,6 @@ export GDK_PIXBUF_MODULE_FILE="$dot_otter_etc/gdk-pixbuf.loaders"
 GDK_PIXBUF_MODULEDIR="${lib_path}/gdk-pixbuf-2.0/2.10.0/loaders" \
 gdk-pixbuf-query-loaders > "$GDK_PIXBUF_MODULE_FILE"
 
-# Create config files for pango font handling library
-pango_module_file="$dot_otter_etc/pango.modules"
-export PANGO_RC_FILE="$dot_otter_etc/pangorc"
-
-cat > $PANGO_RC_FILE << PANGO_RC
-
-[Pango]
-ModuleFiles = $pango_module_file
-
-PANGO_RC
-
-find "${lib_path}" -name 'libpango*.dylib' -print0 \
-| xargs -0 pango-querymodules > "$pango_module_file"
-
 # Need the X11 locale directory
 export XLOCALEDIR="${OTTER_SWAC}/share/X11/locale"
 
