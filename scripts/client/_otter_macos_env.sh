46c2975fad9ba52b6ccd9c90b058ed28af6812c9
Thibaut Hourlier <thibaut@ebi.ac.uk>
2019-05-28 15:35:46 +0100

    Do not use pangorc and set lib_path
    This is similar to a6e8e66e2e8eb1bc9a9f8d1b7a5d8695a23b25e8 and
    bb4493682b20eda6883f7d0b45e4e3e165b8fa8e

--- scripts/client/_otter_macos_env.sh
+++ scripts/client/_otter_macos_env.sh
@@ -1,3 +1,19 @@
+#!/usr/bin/env perl
+# Copyright [2018-2019] EMBL-European Bioinformatics Institute
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 # The guts of setting up MacOS specifics,
 # usually called from _otter_env_core.sh.
 #
@@ -15,7 +31,7 @@ fi
 bin_dir="${OTTER_SWAC}/bin"
 export PATH="$bin_dir:$PATH"
 
-export FONTCONFIG_PATH="${OTTER_SWAC}/etc/fonts"
+export FONTCONFIG_PATH="${OTTER_SWAC}/share/fonts"
 
 # Make etc directory inside ~/.otter to store
 # the gdk-pixbuf and pango config files needed
@@ -23,7 +39,11 @@ dot_otter=~/.otter
 dot_otter_etc="$dot_otter/etc"
 mkdir -p "$dot_otter_etc"
 
-lib_path="${OTTER_SWAC}/lib"
+if [ -d "${OTTER_SWAC}/lib/gdk-pixbuf-2.0" ]; then
+  lib_path="${OTTER_SWAC}/lib"
+else
+  lib_path="/opt/local/lib"
+fi
 
 # Create config file so that gdk-pixbuf can load
 # its bitmap image format loaders
@@ -31,20 +51,6 @@ export GDK_PIXBUF_MODULE_FILE="$dot_otter_etc/gdk-pixbuf.loaders"
 
 GDK_PIXBUF_MODULEDIR="${lib_path}/gdk-pixbuf-2.0/2.10.0/loaders" \
 gdk-pixbuf-query-loaders > "$GDK_PIXBUF_MODULE_FILE"
-
-# Create config files for pango font handling library
-pango_module_file="$dot_otter_etc/pango.modules"
-export PANGO_RC_FILE="$dot_otter_etc/pangorc"
-
-cat > $PANGO_RC_FILE << PANGO_RC
-
-[Pango]
-ModuleFiles = $pango_module_file
-
-PANGO_RC
-
-find "${lib_path}/pango" -name '*.so' -print0 \
-| xargs -0 pango-querymodules > "$pango_module_file"
 
 # Need the X11 locale directory
 export XLOCALEDIR="${OTTER_SWAC}/share/X11/locale"
