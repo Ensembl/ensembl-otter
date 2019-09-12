eb4514829d5f64ee02ade1eaf96925d6325b876b
s-mm <smm@ebi.ac.uk>
2019-09-12 11:54:23 +0100

    this change allows new transcripts to be saved in the local db and remote db without any errors

--- modules/Bio/Vega/Transform/FromHumAce.pm
+++ modules/Bio/Vega/Transform/FromHumAce.pm
@@ -305,6 +305,7 @@ sub store_Gene {
     # Throw it away
     my $transcripts = $gene->get_all_Transcripts;
     @$transcripts = ();
+    $gene->flush_Transcripts;
 
     $gene->slice($self->whole_slice) unless $gene->slice;
 
@@ -312,7 +313,6 @@ sub store_Gene {
     $gene_adaptor->store_only($gene);
 
     # We want get_all_Transcripts to load from database on next call.
-    $gene->flush_Transcripts;
     delete $gene->{'_transcript_array'};
 
     $locus->ensembl_dbID($gene->dbID);
