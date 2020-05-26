a5280f62ce2da8531d17c08554049e951e6545d2
mira <mira@ebi.ac.uk>
2020-05-26 02:34:58 -0700

    Dafault values insert replaced with new syntax

--- modules/Bio/Vega/DBSQL/StableIdAdaptor.pm
+++ modules/Bio/Vega/DBSQL/StableIdAdaptor.pm
@@ -58,7 +58,7 @@ sub _fetch_new_by_type {
     my $poolid = $type . "_pool_id";
     my $table  = $type . "_stable_id_pool";
 
-    my $sql = "insert into $table () values()";
+    my $sql = "insert into $table DEFAULT VALUES";
     my $sth = $self->prepare($sql);
     $sth->execute;
     my $num = $self->last_insert_id($poolid, undef, $table) or throw("Failed to get autoincremented '$poolid'");
