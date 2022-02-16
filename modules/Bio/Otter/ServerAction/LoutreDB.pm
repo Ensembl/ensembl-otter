dff30a63feae2e9da1ed81a62d90bda6e3f003f9
Shashank B R <shabr@ebi.ac.uk>
2022-02-16 14:10:31 +0530

    sql changes for mysql 8

--- modules/Bio/Otter/ServerAction/LoutreDB.pm
+++ modules/Bio/Otter/ServerAction/LoutreDB.pm
@@ -93,13 +93,13 @@ sub get_meta {
 =cut
 
 my $select_cs_sql = <<'SQL';
-    SELECT 'coord_system_id', 'species_id', 'name', 'version', 'rank', 'attrib'
+    SELECT coord_system_id, species_id, name, version, `rank`, attrib
       FROM coord_system
-     WHERE 'name' = 'chromosome' AND 'version' = 'Otter'
+     WHERE name = 'chromosome' AND version = 'Otter'
 SQL
 
 my $select_at_sql = <<'SQL';
-    SELECT 'attrib_type_id', 'code', 'name', 'description'
+    SELECT attrib_type_id, code, name, description
       FROM attrib_type
 SQL
 
@@ -109,14 +109,14 @@ sub get_db_info {
     my (%results, $select_cs_sql);
 
     if ($coord_system_name and $coord_system_version) {
-        $select_cs_sql = "SELECT 'coord_system_id', 'species_id', 'name', 'version', 'rank', 'attrib'".
+        $select_cs_sql = "SELECT coord_system_id, species_id, name, version, `rank`, attrib".
         ' FROM coord_system'.
-        " WHERE 'name' = '$coord_system_name' AND 'version' = '$coord_system_version'";
+        " WHERE name = '$coord_system_name' AND version = '$coord_system_version'";
     }
     else {
-         $select_cs_sql = "SELECT 'coord_system_id', 'species_id', 'name', 'version', 'rank', 'attrib'".
+         $select_cs_sql = "SELECT coord_system_id, species_id, name, version, `rank`, attrib".
         ' FROM coord_system'.
-        " WHERE 'attrib' = 'default_version' AND 'rank' = 1";
+        " WHERE attrib = 'default_version' AND `rank` = 1";
     }
 
     my $dbc = $self->server->otter_dba()->dbc();
